1b2ae821bf9cbf5f38a9f7a008029bb191f555bb
Peter <peathal@yahoo.de>
2013-05-29 00:23:04 +0200

    license header change to reflect more contributors

--- core/src/main/java/com/graphhopper/storage/GraphBuilder.java
+++ core/src/main/java/com/graphhopper/storage/GraphBuilder.java
@@ -1,9 +1,9 @@
 /*
- *  Licensed to Peter Karich under one or more contributor license
+ *  Licensed to GraphHopper and Peter Karich under one or more contributor license
  *  agreements. See the NOTICE file distributed with this work for
  *  additional information regarding copyright ownership.
  *
- *  Peter Karich licenses this file to you under the Apache License,
+ *  GraphHopper licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the
  *  License at
