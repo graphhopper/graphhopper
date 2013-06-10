e8f5f7b02acc63189f9d81bfc388f21dea8e53c8
Peter <peathal@yahoo.de>
2013-06-10 13:28:15 +0200

    make importing bzip2 files possible via reflection

---
+++ tools/src/main/java/com/graphhopper/tools/Import.java
@@ -0,0 +1,13 @@
+package com.graphhopper.tools;
+
+import com.graphhopper.GraphHopper;
+
+/**
+ * @author Peter Karich
+ */
+public class Import {
+
+    public static void main(String[] args) throws Exception {
+        GraphHopper.main(args);
+    }
+}
