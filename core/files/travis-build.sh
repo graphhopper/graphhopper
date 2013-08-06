9dc2c16d6189b2eb46207e5d578ed48f3bdd78b6
Peter <peathal@yahoo.de>
2013-08-06 20:03:20 +0200

    install also in custom travis script

--- core/files/travis-build.sh
+++ core/files/travis-build.sh
@@ -5,6 +5,13 @@ modules="core web tools"
 for module in $modules; do
   echo "====== $module ====="
   cd $module
+  # INSTALL    
+  mvn install -DskipTests=true
+  if [[ "x$EXIT_VAL" != "x0" ]]; then
+    exit $EXIT_VAL
+  fi 
+  
+  # TEST
   mvn test
   EXIT_VAL="$?"
   if [[ "x$EXIT_VAL" != "x0" ]]; then
