8a9d629240ec8a31eb7be274e99d1343c700e258
Peter <peathal@yahoo.de>
2013-11-23 12:07:39 +0100

    puh, finally fix the maven and mapsforge hassle see #122 and #121

--- android/scripts/maven-install-mapsforge.sh
+++ android/scripts/maven-install-mapsforge.sh
@@ -1,7 +1,16 @@
+# do the following
+# git clone https://code.google.com/p/mapsforge/
+# cd mapsforge; and fix http://code.google.com/p/mapsforge/issues/detail?id=461
+# mvn clean install
+# cp mapsforge-map/target/mapsforge-map-0.3.1-SNAPSHOT-jar-with-dependencies.jar graphhopper/android/libs/mapsforge-0.3.1-SNAPSHOT.jar
+
+# if we would do it via normal maven dependency management we run into strange things which I was not able to fix
+# http://stackoverflow.com/a/8315600/194609
+
 MAPSFORGE=`ls ./libs/mapsforge*.jar`
 echo "installing file: $MAPSFORGE"
 $MAVEN_HOME/bin/mvn install:install-file -DgroupId=org.mapsforge \
- -DartifactId=mapsforge \
+ -DartifactId=mapsforge-gh \
  -Dversion=0.3.1-SNAPSHOT \
  -Dpackaging=jar \
  -Dfile=$MAPSFORGE
