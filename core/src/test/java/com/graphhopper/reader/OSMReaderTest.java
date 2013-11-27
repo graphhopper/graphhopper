74d90b51e4db8471c9332236cc86d74bd4f8ae7e
Peter <peathal@yahoo.de>
2013-11-27 23:17:56 +0100

    fixed failing test

--- core/src/test/java/com/graphhopper/reader/OSMReaderTest.java
+++ core/src/test/java/com/graphhopper/reader/OSMReaderTest.java
@@ -296,7 +296,7 @@ public class OSMReaderTest
         int n80 = AbstractGraphStorageTester.getIdOf(graph, 54.1);
         EdgeIterator iter = carOutExplorer.setBaseNode(n80);
         iter.next();
-        assertEquals(10, carEncoder.getSpeed(iter.getFlags()));
+        assertEquals(5, carEncoder.getSpeed(iter.getFlags()));
 
         // more precise speed calculation! ~150km (from 54.0,10.1 to 55.0,10.1) in duration=70 minutes -> wow ;)
         // => 130km/h => / 1.4 => 92km/h        
