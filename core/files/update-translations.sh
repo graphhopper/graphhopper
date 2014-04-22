a078d4bc8c589b21fee6144fa88b10283ca010a7
Peter <peathal@yahoo.de>
2014-04-22 18:25:16 +0200

    i18n: fixed csv ugliness and replaced with tsv

--- core/files/update-translations.sh
+++ core/files/update-translations.sh
@@ -5,9 +5,10 @@ destination=src/main/resources/com/graphhopper/util/
 
 translations="en_US SKIP de_DE ro pt_PT pt_BR bg es ru ja fr si tr nl it fil SKIP el"
 file=$1
-#file=/tmp/gh.csv
-#rm $file
-#wget -O $file "https://docs.google.com/spreadsheet/ccc?key=0AmukcXek0JP6dGM4R1VTV2d3TkRSUFVQakhVeVBQRHc&pli=1&output=csv"
+
+# TODO we need tsv but Google throws Internal Error when doing (works for csv):
+#wget -O $file "https://docs.google.com/spreadsheet/ccc?key=0AmukcXek0JP6dGM4R1VTV2d3TkRSUFVQakhVeVBQRHc&pli=1&output=tsv"
+#so you need to manually grab it
 
 INDEX=1
 for tr in $translations; do
@@ -16,5 +17,5 @@ for tr in $translations; do
     continue
   fi
   echo -e '# do not edit manually, instead use spreadsheet https://t.co/f086oJXAEI and script ./core/files/update-translations.sh\n' > $destination/$tr.txt
-  tail -n+6 "$file" | cut -d',' -s -f1,$INDEX --output-delimiter='=' >> $destination/$tr.txt
+  tail -n+5 "$file" | cut -s -f1,$INDEX --output-delimiter='=' >> $destination/$tr.txt
 done
