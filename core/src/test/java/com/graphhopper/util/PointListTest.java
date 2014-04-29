1a25de95cf508b1e80e21de214f645cd90c545a3
Peter <peathal@yahoo.de>
2014-04-29 12:09:12 +0200

    pointlist: test fix

--- core/src/test/java/com/graphhopper/util/PointListTest.java
+++ core/src/test/java/com/graphhopper/util/PointListTest.java
@@ -80,7 +80,7 @@ public class PointListTest
         instance.add(toAdd);
 
         assertEquals(12, instance.getSize());
-        assertEquals(20, instance.getCapacity());
+        assertEquals(24, instance.getCapacity());
 
         for (int i = 0; i < toAdd.size(); i++)
         {
