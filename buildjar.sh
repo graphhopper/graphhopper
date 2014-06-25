d3c5718496e66d4e8ad8b5da423c86ed8ddb1fe0
Ken Southerland <southerland@samsix.com>
2014-06-24 23:21:19 -0700

    script to rebuild the jar file

---
+++ buildjar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mvn clean install
+
+mkdir ~/tmp/repack
+cd ~/tmp/repack
+jar xvf ~/dev/samsix/graphhopper/core/target/graphhopper-0.4-SNAPSHOT.jar
+rm log4j.properties
+mv META-INF/MANIFEST.MF ../manifest
+
+cd ..
+jar cvfm graphhopper-0.4-SNAPSHOT.jar ~/tmp/manifest -C ~/tmp/repack/ .
+rm -rf ~/tmp/repack ~/tmp/manifest
+
+mv graphhopper-0.4-SNAPSHOT.jar ~/dev/samsix/nrg/lib/graphhopper
+
+. godev
+cd nrg
+ant -Djarfile=lib/graphhopper/graphhopper-0.4-SNAPSHOT.jar s6signjar
