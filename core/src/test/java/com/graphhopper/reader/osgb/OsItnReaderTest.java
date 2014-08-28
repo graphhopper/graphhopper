144afc58dd53df7e54394b28ecad18190be21edd
engaric <stuartadam@Stuart-Adams-MacBook-Pro.local>
2014-08-29 00:01:17 +0100

    Move test to correct package

--- core/src/test/java/com/graphhopper/reader/osgb/OsItnReaderTest.java
+++ core/src/test/java/com/graphhopper/reader/osgb/OsItnReaderTest.java
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.graphhopper.reader;
+package com.graphhopper.reader.osgb;
 
 import static com.graphhopper.util.GHUtility.count;
 import static org.junit.Assert.assertEquals;
