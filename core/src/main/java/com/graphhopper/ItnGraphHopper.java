a9f599d5b853e5b3af714da3d88c9a8974c44875
engaric <stuartadam@Stuart-Adams-MacBook-Pro.local>
2014-10-21 00:42:44 +0100

    ITN Loader

--- core/src/main/java/com/graphhopper/ItnGraphHopper.java
+++ core/src/main/java/com/graphhopper/ItnGraphHopper.java
@@ -7,9 +7,25 @@ import org.slf4j.LoggerFactory;
 
 import com.graphhopper.reader.DataReader;
 import com.graphhopper.reader.osgb.OsItnReader;
+import com.graphhopper.routing.util.RoutingAlgorithmSpecialAreaTests;
 import com.graphhopper.storage.GraphStorage;
+import com.graphhopper.util.CmdArgs;
 
 public class ItnGraphHopper extends GraphHopper {
+	
+	 public static void main( String[] strs ) throws Exception
+	    {
+	        CmdArgs args = CmdArgs.read(strs);
+	        GraphHopper hopper = new ItnGraphHopper().init(args);
+	        hopper.importOrLoad();
+	        if (args.getBool("graph.testIT", false))
+	        {
+	            // important: use osmreader.wayPointMaxDistance=0
+	            RoutingAlgorithmSpecialAreaTests tests = new RoutingAlgorithmSpecialAreaTests(hopper);
+	            tests.start();
+	        }
+	        hopper.close();
+	    }
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -21,7 +37,7 @@ public class ItnGraphHopper extends GraphHopper {
 	protected OsItnReader initOSMReader(OsItnReader reader) {
 		if (osmFile == null)
 			throw new IllegalArgumentException("No OSM file specified");
-
+		forDesktop();
 		logger.info("start creating graph from " + osmFile);
 		File osmTmpFile = new File(osmFile);
 		return reader.setOSMFile(osmTmpFile).setElevationProvider(eleProvider)
