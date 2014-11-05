ed6748258f6a455f389fde4361741ea17ca1c154
Stuart <engaric@hotmail.com>
2014-11-05 23:35:50 +0000

    Simpler import process. Use configurable reader
    Reader configured via reader.implementation setting

---
+++ core/itngen.sh
@@ -0,0 +1,3 @@
+java -Xmx4096m -Xms2048m -XX:+UseParallelGC -XX:+UseParallelOldGC -cp ../tools/target/classes:target/classes:${HOME}/.m2/repository/uk/co/ordnancesurvey/api/OSGBCoordinateConvertor/1.0.3/OSGBCoordinateConvertor-1.0.3.jar:${HOME}/.m2/repository/org/slf4j/slf4j-api/1.7.7/slf4j-api-1.7.7.jar:${HOME}/.m2/repository/net/sf/trove4j/trove4j/3.0.3/trove4j-3.0.3.jar com.graphhopper.tools.Import osmreader.osm=./ITNun/ prepare.chShortcuts=false reader.implementation=com.graphhopper.reader.osgb.OsItnReader
+
+#:${HOME}/.m2/repository/org/slf4j/slf4j-log4j12/1.7.7/slf4j-log4j12-1.7.7.jar:${HOME}/.m2/repository/log4j/log4j/1.2.17/log4j-1.2.17.jar
