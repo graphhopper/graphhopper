61a6dab7527a935950892598a6999a5c7dd03a78
Peter <peathal@yahoo.de>
2015-03-04 10:41:41 +0100

    fixed map-matching start script to use arbitrary version

--- map-matching.sh
+++ map-matching.sh
@@ -1,6 +1,4 @@
-VERSION=0.4-SNAPSHOT
-JAR=target/map-matching-0.4-SNAPSHOT-jar-with-dependencies.jar
-#target/graphhopper-map-matching-$VERSION-jar-with-dependencies.jar
+JAR=$(ls target/map-matching-*-dependencies.jar)
 
 if [ "$JAVA" = "" ]; then
  JAVA=java
