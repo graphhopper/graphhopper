e829cf09c3eb49eb7d5cc35b054a18c1e59becfe
Peter <peathal@yahoo.de>
2015-04-28 22:24:02 +0200

    use https for omniscale in gh maps

--- web/src/main/webapp/js/main.js
+++ web/src/main/webapp/js/main.js
@@ -319,7 +319,7 @@ function initMap(selectLayer) {
         subdomains: ['a', 'b', 'c']
     });
         
-    var omniscale = L.tileLayer.wms('http://{s}.maps.omniscale.net/v1/peter-d693a656/tile', {
+    var omniscale = L.tileLayer.wms('https://maps.omniscale.net/v1/peter-d693a656/tile', {
             layers: 'osm',
         attribution: osmAttr + ', &copy; <a href="http://maps.omniscale.com/">Omniscale</a>'
     });
