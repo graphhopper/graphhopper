4f4803f2a70910ce4bbb35529328c044688d99d7
crowmagnumb <crowmagnumb@gmail.com>
2015-06-01 12:14:53 -0700

    commenting out full america for now

--- us-download.sh
+++ us-download.sh
@@ -2,6 +2,6 @@ wget http://download.geofabrik.de/north-america/us-northeast-latest.osm.pbf
 wget http://download.geofabrik.de/north-america/us-midwest-latest.osm.pbf
 wget http://download.geofabrik.de/north-america/us-south-latest.osm.pbf
 wget http://download.geofabrik.de/north-america/us-west-latest.osm.pbf
-wget http://download.geofabrik.de/north-america-latest.osm.pbf
+#wget http://download.geofabrik.de/north-america-latest.osm.pbf
 
 
