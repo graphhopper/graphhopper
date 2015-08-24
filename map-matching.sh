9ec16d2290191bbd1048d58b180ffa73be131179
Peter <peathal@yahoo.de>
2015-08-24 16:46:48 +0200

    fix jar path problem if jar not yet build

--- map-matching.sh
+++ map-matching.sh
@@ -1,4 +1,8 @@
-JAR=$(ls target/map-matching-*-dependencies.jar)
+set_jar_path
+
+function set_jar_path {
+  JAR=$(ls target/map-matching-*-dependencies.jar)
+}
 
 if [ "$JAVA" = "" ]; then
  JAVA=java
@@ -6,6 +10,7 @@ fi
 
 if [ ! -f "$JAR" ]; then
   mvn -DskipTests=true install assembly:single
+  set_jar_path
 fi
 
 exec "$JAVA" $JAVA_OPTS -jar $JAR "$@"
