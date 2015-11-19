1452863e810ddd7ba48745cabb20b9e4ec824020
Peter <peathal@yahoo.de>
2015-11-19 21:01:49 +0100

    move android downloads, see #565

--- android/app/src/main/java/com/graphhopper/android/MainActivity.java
+++ android/app/src/main/java/com/graphhopper/android/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends Activity
     private volatile boolean prepareInProgress = false;
     private volatile boolean shortestPathRunning = false;
     private String currentArea = "berlin";
-    private String fileListURL = "https://graphhopper.com/public/maps/0.6/";
+    private String fileListURL = "http://download2.graphhopper.com/public/maps/0.6/";
     private String prefixURL = fileListURL;
     private String downloadURL;
     private File mapsFolder;
@@ -265,7 +265,7 @@ public class MainActivity extends Activity
             @Override
             protected void onPostExecute( List<String> nameList )
             {
-                if (nameList.isEmpty())
+                if (nameList == null || nameList.isEmpty())
                 {
                     logUser("No maps created for your version!? " + fileListURL);
                     return;
@@ -409,13 +409,13 @@ public class MainActivity extends Activity
 
         tileRendererLayer = new TileRendererLayer(tileCache, mapDataStore,
                 mapView.getModel().mapViewPosition, false, true, AndroidGraphicFactory.INSTANCE)
-        {
-            @Override
-            public boolean onLongPress( LatLong tapLatLong, Point layerXY, Point tapXY )
-            {
-                return onMapTap(tapLatLong, layerXY, tapXY);
-            }
-        };
+                {
+                    @Override
+                    public boolean onLongPress( LatLong tapLatLong, Point layerXY, Point tapXY )
+                    {
+                        return onMapTap(tapLatLong, layerXY, tapXY);
+                    }
+                };
         tileRendererLayer.setTextScale(1.5f);
         tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
         mapView.getModel().mapViewPosition.setMapPosition(new MapPosition(mapDataStore.boundingBox().getCenterPoint(), (byte) 15));
@@ -466,9 +466,9 @@ public class MainActivity extends Activity
         paintStroke.setStyle(Style.STROKE);
         paintStroke.setColor(Color.argb(128, 0, 0xCC, 0x33));
         paintStroke.setDashPathEffect(new float[]
-                {
-                        25, 15
-                });
+        {
+            25, 15
+        });
         paintStroke.setStrokeWidth(8);
 
         Polyline line = new Polyline((org.mapsforge.core.graphics.Paint) paintStroke, AndroidGraphicFactory.INSTANCE);
