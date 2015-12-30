418deb7e4d5ce7be5f155651f6f79e8a4bbdcacd
Robin <boldtrn@gmail.com>
2015-12-30 09:48:55 +0100

    Fixed Typo

--- core/src/main/java/com/graphhopper/reader/osm/ConditionalTagWayAcceptor.java
+++ core/src/main/java/com/graphhopper/reader/osm/ConditionalTagWayAcceptor.java
@@ -25,25 +25,25 @@ public class ConditionalTagWayAcceptor implements WayAcceptor
     private final ConditionalParser parser;
 
     // If true, we match allowing restriction-values (e.g. yes or permissive)
-    private final boolean alllowanceCheck;
+    private final boolean allowanceCheck;
 
     /**
      * Create with todays date
      */
-    public ConditionalTagWayAcceptor( List<String> tagsToCheck, Set<String> restricedValues, boolean alllowanceCheck )
+    public ConditionalTagWayAcceptor( List<String> tagsToCheck, Set<String> restricedValues, boolean allowanceCheck )
     {
-        this(Calendar.getInstance(), tagsToCheck, restricedValues, alllowanceCheck);
+        this(Calendar.getInstance(), tagsToCheck, restricedValues, allowanceCheck);
     }
 
     /**
      * Create with given date
      */
-    public ConditionalTagWayAcceptor( Calendar date, List<String> tagsToCheck, Set<String> restricedValues, boolean alllowanceCheck )
+    public ConditionalTagWayAcceptor( Calendar date, List<String> tagsToCheck, Set<String> restricedValues, boolean allowanceCheck )
     {
         this.calendar = date;
         this.tagsToCheck = tagsToCheck;
         this.parser = new ConditionalParser(restricedValues);
-        this.alllowanceCheck = alllowanceCheck;
+        this.allowanceCheck = allowanceCheck;
     }
 
     @Override
@@ -59,7 +59,7 @@ public class ConditionalTagWayAcceptor implements WayAcceptor
                 {
                     DateRange dateRange = parser.getRestrictiveDateRange(val);
                     if (dateRange != null && dateRange.isInRange(calendar))
-                        return alllowanceCheck;
+                        return allowanceCheck;
                 } catch (Exception e)
                 {
                     logger.info("Could not parse the value:" + val + " of tag:" + tagToCheck + ". The Exception Message is:" + e.getMessage());
@@ -67,6 +67,6 @@ public class ConditionalTagWayAcceptor implements WayAcceptor
             }
         }
 
-        return !alllowanceCheck;
+        return !allowanceCheck;
     }
 }
