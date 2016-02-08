a423bd73d4ff2d38806ebee3c0ebd52b6808aac0
Peter <peathal@yahoo.de>
2016-02-08 11:24:33 +0100

    fixed GraphHopperWeb client for custom parameters, fixes #658

--- web/src/main/java/com/graphhopper/http/GraphHopperWeb.java
+++ web/src/main/java/com/graphhopper/http/GraphHopperWeb.java
@@ -24,8 +24,11 @@ import com.graphhopper.GraphHopperAPI;
 import com.graphhopper.util.*;
 import com.graphhopper.util.shapes.GHPoint;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -46,9 +49,28 @@ public class GraphHopperWeb implements GraphHopperAPI
     private boolean instructions = true;
     private boolean calcPoints = true;
     private boolean elevation = false;
+    private final Set<String> ignoreSet;
 
     public GraphHopperWeb()
     {
+        // some parameters are supported directly via Java API so ignore them when writing the getHints map
+        ignoreSet = new HashSet<String>();
+        ignoreSet.add("calc_points");
+        ignoreSet.add("calcpoints");
+        ignoreSet.add("instructions");
+        ignoreSet.add("elevation");
+        ignoreSet.add("key");
+
+        // some parameters are in the request:
+        ignoreSet.add("algorithm");
+        ignoreSet.add("locale");
+        ignoreSet.add("point");
+        ignoreSet.add("vehicle");
+
+        // some are special and need to be avoided
+        ignoreSet.add("points_encoded");
+        ignoreSet.add("pointsencoded");
+        ignoreSet.add("type");
     }
 
     public void setDownloader( Downloader downloader )
@@ -109,7 +131,6 @@ public class GraphHopperWeb implements GraphHopperAPI
                         + "Use calcPoints=false and instructions=false to disable point and instruction calculation");
 
             boolean tmpElevation = request.getHints().getBool("elevation", elevation);
-            String tmpKey = request.getHints().get("key", key);
 
             String url = routeServiceUrl
                     + "?"
@@ -125,12 +146,20 @@ public class GraphHopperWeb implements GraphHopperAPI
             if (!request.getVehicle().isEmpty())
                 url += "&vehicle=" + request.getVehicle();
 
-            if (!tmpKey.isEmpty())
-                url += "&key=" + tmpKey;
-            int altMax = request.getHints().getInt("alternative_route.max_num", 0);
-            if (altMax != 0)
+            if (!key.isEmpty())
+                url += "&key=" + key;
+
+            for (Entry<String, String> entry : request.getHints().toMap().entrySet())
             {
-                url += "&alternative_route.max_num=" + altMax;
+                String urlKey = entry.getKey();
+                String urlValue = entry.getValue();
+
+                // use lower case conversion for check only!
+                if (ignoreSet.contains(urlKey.toLowerCase()))
+                    continue;
+
+                if (urlValue != null && !urlValue.isEmpty())
+                    url += "&" + WebHelper.encodeURL(urlKey) + "=" + WebHelper.encodeURL(urlValue);
             }
 
             String str = downloader.downloadAsString(url, true);
