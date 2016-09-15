98337436ec88ccb9d8b82d611db698c2366e8542
ammagamma <contactammagamma@gmail.com>
2016-09-15 14:32:50 +0200

    inserts intermediate points in web UI correctly for round trips

--- web/src/main/webapp/js/routeManipulation.js
+++ web/src/main/webapp/js/routeManipulation.js
@@ -1,40 +1,31 @@
 var L = require('leaflet');
 
 module.exports.getIntermediatePointIndex = function(routeSegments, clickedLocation) {
-    // finds the point of a routeSegment that is closest to the clicked location
-    var getClosestPoint = function(routeSegment) {
-        return routeSegment.coordinates.map(function(c, i) {
-            return {
-                index: i,
-                distance: L.latLng(c).distanceTo(clickedLocation)
-            };
-        }).reduce(function(prev, curr) {
-            return curr.distance < prev.distance ? curr : prev;
-        });
-    };
-
-    // find the part of the route that contains the closest point to the clicked location
-    // there can be more than one such parts if alternative routes are used
-    var closestRouteSegment = routeSegments.map(function(rs) {
-       rs.closestPoint = getClosestPoint(rs);
-       return rs;
-    }).reduce(function(prev, curr) {
-        return curr.closestPoint.distance < prev.closestPoint.distance ? curr : prev;
-    });
-
-    // start at the closest point and follow the route to find the index of the next waypoint
-    var routeCoords = closestRouteSegment.coordinates;
-    var wayPoints = closestRouteSegment.wayPoints;
-    var index = 1; // if no waypoint is found the index will be one
-    for (var i=closestRouteSegment.closestPoint.index; i<routeCoords.length; ++i) {
-        var wpIndex = wayPoints.findIndex(function(wp) {
-            return routeCoords[i].equals(wp, 1.e-3);
-        });
-        if (wpIndex !== -1) {
-            index = wpIndex === 0 ? 1 : wpIndex;
-            break;
+    // for each point of the route determine its distance to the clicked location and the index
+    // of the waypoint that comes next if we keep following the route
+    var nextWayPointIndex = 0;
+    var wayPoints = routeSegments[0].wayPoints;
+    var distancesAndNextWayPointIndices = [];
+    for(var i=0; i<routeSegments.length; ++i) {
+        var routeCoords = routeSegments[i].coordinates;
+        for(var j=0; j<routeCoords.length; ++j) {
+            // whenever we hit a waypoint we start looking for the next one
+            if (routeCoords[j].equals(wayPoints[nextWayPointIndex], 1.e-5)) {
+                nextWayPointIndex++;
+            }
+            distancesAndNextWayPointIndices.push({
+                distance: routeCoords[j].distanceTo(clickedLocation),
+                nextWayPointIndex: nextWayPointIndex === wayPoints.length ? nextWayPointIndex-1 : nextWayPointIndex
+            });
         }
     }
-    return index;
-};
 
+    // get the waypoint index for the point closest to the clicked location, if two distances are
+    // equal prefer the point further down the route for a slightly nicer user experience
+    var result = distancesAndNextWayPointIndices.reduce(function(prev, curr) {
+       return (curr.distance - prev.distance) < 1.e-6 ? curr : prev;
+    }).nextWayPointIndex;
+
+    result = (result > 0 && result < wayPoints.length) ? result : 1; //just for safety
+    return result;
+};
