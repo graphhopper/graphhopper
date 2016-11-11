1baa34293f0a0f7230ae1ff273bfd4831e68229c
ratrun <ratrun@gmx.at>
2016-11-11 16:21:00 +0100

    Improve access restriction handling for bicycle routing Give highway=cycleway higher preference compared to access restrictions. The change was triggered by the discussion in issue#863 about allowing a combination of highway=cycleway with "vehicle=no". Also BikeFlagEncoder access restriction testcases are improved for similar cases.

--- core/src/main/java/com/graphhopper/routing/util/BikeCommonFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/BikeCommonFlagEncoder.java
@@ -248,8 +248,19 @@ public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         if (!highwaySpeeds.containsKey(highwayValue))
             return 0;
 
+        String sacScale = way.getTag("sac_scale");
+        if (sacScale != null) {
+            if ((way.hasTag("highway", "cycleway"))
+                    && (way.hasTag("sac_scale", "hiking")))
+                return acceptBit;
+            if (!isSacScaleAllowed(sacScale))
+                return 0;
+        }
+
         // use the way if it is tagged for bikes
-        if (way.hasTag("bicycle", intendedValues) || way.hasTag("bicycle", "dismount"))
+        if (way.hasTag("bicycle", intendedValues) || 
+            way.hasTag("bicycle", "dismount") || 
+            way.hasTag("highway", "cycleway"))
             return acceptBit;
 
         // accept only if explicitly tagged for bike usage
@@ -266,15 +277,6 @@ public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         // check access restrictions
         if (way.hasTag(restrictions, restrictedValues) && !getConditionalTagInspector().isRestrictedWayConditionallyPermitted(way))
             return 0;
-
-        String sacScale = way.getTag("sac_scale");
-        if (sacScale != null) {
-            if ((way.hasTag("highway", "cycleway"))
-                    && (way.hasTag("sac_scale", "hiking")))
-                return acceptBit;
-            if (!isSacScaleAllowed(sacScale))
-                return 0;
-        }
 
         if (getConditionalTagInspector().isPermittedWayConditionallyRestricted(way))
             return 0;
