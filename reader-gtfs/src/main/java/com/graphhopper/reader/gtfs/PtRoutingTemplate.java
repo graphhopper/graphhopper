074d36a40cb4f0583bb6515fc46f8b95357dd992
Peter <graphhopper@gmx.de>
2016-11-21 09:58:47 +0100

    print no route found error message instead of exception

--- reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/PtRoutingTemplate.java
+++ reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/PtRoutingTemplate.java
@@ -22,59 +22,59 @@ import java.util.List;
 
 class PtRoutingTemplate implements RoutingTemplate {
 
-	private final GtfsStorage gtfsStorage;
-	private final GHRequest ghRequest;
-	private final GHResponse ghResponse;
-	private final LocationIndex locationIndex;
-	private final GraphHopperStorage graphHopperStorage;
-	private List<QueryResult> snappedWaypoints = new ArrayList<>(2);
-	private List<Path> paths;
+    private final GtfsStorage gtfsStorage;
+    private final GHRequest ghRequest;
+    private final GHResponse ghResponse;
+    private final LocationIndex locationIndex;
+    private final GraphHopperStorage graphHopperStorage;
+    private List<QueryResult> snappedWaypoints = new ArrayList<>(2);
+    private List<Path> paths;
     private List<Integer> toNodes;
     private int startNode = -1;
     private long initialTime;
     private FlagEncoder flagEncoder;
 
     PtRoutingTemplate(GHRequest ghRequest, GHResponse ghRsp, LocationIndex locationIndex, GtfsStorage gtfsStorage, GraphHopperStorage graphHopperStorage) {
-		this.gtfsStorage = gtfsStorage;
-		this.ghRequest = ghRequest;
-		this.ghResponse = ghRsp;
-		this.locationIndex = locationIndex;
-		this.graphHopperStorage = graphHopperStorage;
-	}
+        this.gtfsStorage = gtfsStorage;
+        this.ghRequest = ghRequest;
+        this.ghResponse = ghRsp;
+        this.locationIndex = locationIndex;
+        this.graphHopperStorage = graphHopperStorage;
+    }
 
-	@Override
-	public List<QueryResult> lookup(List<GHPoint> points, FlagEncoder encoder) {
+    @Override
+    public List<QueryResult> lookup(List<GHPoint> points, FlagEncoder encoder) {
         this.flagEncoder = encoder;
-		if (points.size() != 2)
-			throw new IllegalArgumentException("Exactly 2 points have to be specified, but was:" + points.size());
+        if (points.size() != 2)
+            throw new IllegalArgumentException("Exactly 2 points have to be specified, but was:" + points.size());
 
-		EdgeFilter enterFilter = new PtEnterPositionLookupEdgeFilter(gtfsStorage);
-		EdgeFilter exitFilter = new PtExitPositionLookupEdgeFilter(gtfsStorage);
+        EdgeFilter enterFilter = new PtEnterPositionLookupEdgeFilter(gtfsStorage);
+        EdgeFilter exitFilter = new PtExitPositionLookupEdgeFilter(gtfsStorage);
 //		EdgeFilter enterFilter = new EverythingButPt(gtfsStorage);
 //		EdgeFilter exitFilter = new EverythingButPt(gtfsStorage);
 
-		GHPoint enter = points.get(0);
-		QueryResult source = locationIndex.findClosest(enter.lat, enter.lon, enterFilter);
-		if (!source.isValid()) {
-			ghResponse.addError(new PointNotFoundException("Cannot find entry point: " + enter, 0));
-		} else {
+        GHPoint enter = points.get(0);
+        QueryResult source = locationIndex.findClosest(enter.lat, enter.lon, enterFilter);
+        if (!source.isValid()) {
+            ghResponse.addError(new PointNotFoundException("Cannot find entry point: " + enter, 0));
+        } else {
 //            ForwardInTime forwardInTime = new ForwardInTime(source);
             long requestedTimeOfDay = ghRequest.getHints().getInt(GraphHopperGtfs.EARLIEST_DEPARTURE_TIME_HINT, 0) % (24 * 60 * 60);
             long requestedDay = ghRequest.getHints().getInt(GraphHopperGtfs.EARLIEST_DEPARTURE_TIME_HINT, 0) / (24 * 60 * 60);
 //            startNode = forwardInTime.find(requestedTimeOfDay);
 //            initialTime = forwardInTime.getTime() + requestedDay * (24 * 60 * 60);
 
-			startNode = source.getClosestNode();
-			initialTime = requestedTimeOfDay + requestedDay * (24 * 60 * 60);
+            startNode = source.getClosestNode();
+            initialTime = requestedTimeOfDay + requestedDay * (24 * 60 * 60);
         }
-		snappedWaypoints.add(source);
-		GHPoint exit = points.get(1);
+        snappedWaypoints.add(source);
+        GHPoint exit = points.get(1);
         QueryResult dest = locationIndex.findClosest(exit.lat, exit.lon, exitFilter);
-		toNodes = new ArrayList<>();
-		if (!dest.isValid()) {
-			ghResponse.addError(new PointNotFoundException("Cannot find exit point: " + exit, 0));
-		} else {
-			toNodes.add(dest.getClosestNode());
+        toNodes = new ArrayList<>();
+        if (!dest.isValid()) {
+            ghResponse.addError(new PointNotFoundException("Cannot find exit point: " + exit, 0));
+        } else {
+            toNodes.add(dest.getClosestNode());
 //			new DepthFirstSearch() {
 //				@Override
 //				protected boolean goFurther(int nodeId) {
@@ -87,107 +87,110 @@ class PtRoutingTemplate implements RoutingTemplate {
 //					return gtfsStorage.getEdges().get(edgeState.getEdge()) instanceof ArrivalNodeMarkerEdge;
 //				}
 //			}), dest.getClosestNode());
-		}
-		snappedWaypoints.add(dest);
-		return snappedWaypoints;
-	}
+        }
+        snappedWaypoints.add(dest);
+        return snappedWaypoints;
+    }
 
-	@Override
-	public List<Path> calcPaths(QueryGraph queryGraph, RoutingAlgorithmFactory algoFactory, AlgorithmOptions algoOpts) {
-		long visitedNodesSum = 0L;
-		if (ghRequest.getPoints().size() != 2 ) {
-			throw new UnsupportedOperationException();
-		}
-		paths = new ArrayList<>();
-        PathWrapper altResponse = new PathWrapper();
+    @Override
+    public List<Path> calcPaths(QueryGraph queryGraph, RoutingAlgorithmFactory algoFactory, AlgorithmOptions algoOpts) {
+        long visitedNodesSum = 0L;
+        if (ghRequest.getPoints().size() != 2) {
+            throw new UnsupportedOperationException();
+        }
+        paths = new ArrayList<>();
+        // PathWrapper altResponse = new PathWrapper();
 
-		StopWatch sw = new StopWatch().start();
-		TimeDependentRoutingAlgorithm algo = (TimeDependentRoutingAlgorithm) algoFactory.createAlgo(queryGraph, algoOpts);
-		String debug = ", algoInit:" + sw.stop().getSeconds() + "s";
+        StopWatch sw = new StopWatch().start();
+        TimeDependentRoutingAlgorithm algo = (TimeDependentRoutingAlgorithm) algoFactory.createAlgo(queryGraph, algoOpts);
+        String debug = ", algoInit:" + sw.stop().getSeconds() + "s";
 
-		sw = new StopWatch().start();
+        sw = new StopWatch().start();
 
-		List<Path> tmpPathList = ((MultiCriteriaLabelSetting) algo).calcPaths(startNode, new HashSet(toNodes), (int) initialTime);
-		debug += ", " + algo.getName() + "-routing:" + sw.stop().getSeconds() + "s";
+        List<Path> tmpPathList = ((MultiCriteriaLabelSetting) algo).calcPaths(startNode, new HashSet(toNodes), (int) initialTime);
+        debug += ", " + algo.getName() + "-routing:" + sw.stop().getSeconds() + "s";
 
-		for (Path path : tmpPathList) {
-			if (path.getTime() < 0)
-				throw new RuntimeException("Time was negative. Please report as bug and include:" + ghRequest);
+        for (Path path : tmpPathList) {
+            if (path.getTime() < 0)
+                throw new RuntimeException("Time was negative. Please report as bug and include:" + ghRequest);
 
-			paths.add(path);
-			debug += ", " + path.getDebugInfo();
-		}
+            paths.add(path);
+            debug += ", " + path.getDebugInfo();
+        }
 
-		altResponse.addDebugInfo(debug);
+        // altResponse.addDebugInfo(debug);
+        // reset all direction enforcements in queryGraph to avoid influencing next path
+        queryGraph.clearUnfavoredStatus();
 
-		// reset all direction enforcements in queryGraph to avoid influencing next path
-		queryGraph.clearUnfavoredStatus();
+        if (algo.getVisitedNodes() >= algoOpts.getMaxVisitedNodes())
+            throw new IllegalArgumentException("No path found due to maximum nodes exceeded " + algoOpts.getMaxVisitedNodes());
 
-		if (algo.getVisitedNodes() >= algoOpts.getMaxVisitedNodes())
-			throw new IllegalArgumentException("No path found due to maximum nodes exceeded " + algoOpts.getMaxVisitedNodes());
+        visitedNodesSum += algo.getVisitedNodes();
 
-		visitedNodesSum += algo.getVisitedNodes();
+        ghResponse.getHints().put("visited_nodes.sum", visitedNodesSum);
+        ghResponse.getHints().put("visited_nodes.average", (float) visitedNodesSum);
 
-		ghResponse.getHints().put("visited_nodes.sum", visitedNodesSum);
-		ghResponse.getHints().put("visited_nodes.average", (float) visitedNodesSum);
+        return paths;
+    }
 
-		return paths;
-	}
+    @Override
+    public boolean isReady(PathMerger pathMerger, Translation tr) {
+        for (Path path : paths) {
+            if (path.isFound()) {
+                PathWrapper wrappedPath = new PathWrapper();
+                PointList waypoints = new PointList(snappedWaypoints.size(), true);
+                for (QueryResult qr : snappedWaypoints) {
+                    waypoints.add(qr.getSnappedPoint());
+                }
+                wrappedPath.setWaypoints(waypoints);
+                InstructionList instructions = new InstructionList(tr);
+                fillInstructions(path, instructions);
+                wrappedPath.setInstructions(instructions);
+                wrappedPath.setDescription(path.getDescription());
+                PointList points = new PointList();
+                for (Instruction instruction : instructions) {
+                    points.add(instruction.getPoints());
+                }
+                wrappedPath.setPoints(points);
+                wrappedPath.setRouteWeight(path.getWeight());
+                wrappedPath.setDistance(path.getDistance());
+                wrappedPath.setTime(path.getTime());
+                int numBoardings = 0;
+                for (EdgeIteratorState edge : path.calcEdges()) {
+                    AbstractPtEdge ptEdge = gtfsStorage.getEdges().get(edge.getEdge());
+                    if (ptEdge instanceof BoardEdge) {
+                        numBoardings++;
+                    }
+                }
+                wrappedPath.setNumChanges(numBoardings - 1);
+                ghResponse.add(wrappedPath);
+            }
+        }
+        if (ghResponse.getAll().isEmpty()) {
+            ghResponse.addError(new RuntimeException("No route found"));
+        } else {
+            Collections.sort(ghResponse.getAll(), (p1, p2) -> Double.compare(p1.getRouteWeight(), p2.getRouteWeight()));
+        }
+        return true;
+    }
 
-	@Override
-	public boolean isReady(PathMerger pathMerger, Translation tr) {
-		for (Path path : paths) {
-			if (path.isFound()) {
-				PathWrapper wrappedPath = new PathWrapper();
-				PointList waypoints = new PointList(snappedWaypoints.size(), true);
-				for (QueryResult qr : snappedWaypoints) {
-					waypoints.add(qr.getSnappedPoint());
-				}
-				wrappedPath.setWaypoints(waypoints);
-				InstructionList instructions = new InstructionList(tr);
-				fillInstructions(path, instructions);
-				wrappedPath.setInstructions(instructions);
-				wrappedPath.setDescription(path.getDescription());
-				PointList points = new PointList();
-				for (Instruction instruction : instructions) {
-					points.add(instruction.getPoints());
-				}
-				wrappedPath.setPoints(points);
-				wrappedPath.setRouteWeight(path.getWeight());
-				wrappedPath.setDistance(path.getDistance());
-				wrappedPath.setTime(path.getTime());
-				int numBoardings = 0;
-				for (EdgeIteratorState edge : path.calcEdges()) {
-					AbstractPtEdge ptEdge = gtfsStorage.getEdges().get(edge.getEdge());
-					if (ptEdge instanceof BoardEdge) {
-						numBoardings++;
-					}
-				}
-				wrappedPath.setNumChanges(numBoardings-1);
-				ghResponse.add(wrappedPath);
-			}
-		}
-		Collections.sort(ghResponse.getAll(), (p1, p2) -> Double.compare(p1.getRouteWeight(), p2.getRouteWeight()));
-		return true;
-	}
+    private void fillInstructions(Path path, InstructionList outInstructions) {
+        PointList points = path.calcPoints();
+        List<EdgeIteratorState> edges = path.calcEdges();
+        for (EdgeIteratorState edge : edges) {
+            outInstructions.add(new Instruction(0, edge.getName(), new InstructionAnnotation(0, edge.getName()), edge.fetchWayGeometry(1)));
+        }
+        if (!points.isEmpty()) {
+            PointList end = new PointList();
+            end.add(points, points.size() - 1);
+            outInstructions.add(new Instruction(0, "Angekommen", new InstructionAnnotation(0, "Vermutlich am Ziel"), end));
+        }
+    }
 
-	private void fillInstructions(Path path, InstructionList outInstructions) {
-		PointList points = path.calcPoints();
-		List<EdgeIteratorState> edges = path.calcEdges();
-		for (EdgeIteratorState edge : edges) {
-			outInstructions.add(new Instruction(0, edge.getName(), new InstructionAnnotation(0, edge.getName()), edge.fetchWayGeometry(1)));
-		}
-		if (!points.isEmpty()) {
-			PointList end = new PointList();
-			end.add(points, points.size()-1);
-			outInstructions.add(new Instruction(0, "Angekommen", new InstructionAnnotation(0, "Vermutlich am Ziel"), end));
-		}
-	}
-
-	@Override
-	public int getMaxRetries() {
-		return 1;
-	}
+    @Override
+    public int getMaxRetries() {
+        return 1;
+    }
 
     private class ForwardInTime extends DepthFirstSearch {
         private final QueryResult source;
@@ -217,7 +220,7 @@ class PtRoutingTemplate implements RoutingTemplate {
 
         @Override
         protected boolean goFurther(int nodeId) {
-lastNode = nodeId;
+            lastNode = nodeId;
             return time < requestedTime;
         }
 
