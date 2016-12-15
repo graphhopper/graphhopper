05139eace7df1aee9a03ecedfc3e44386e8b2514
boldtrn <boldtrn@gmail.com>
2016-12-15 14:20:21 +1100

    Fixed License

--- core/src/main/java/com/graphhopper/debatty/java/stringsimilarity/JaroWinkler.java
+++ core/src/main/java/com/graphhopper/debatty/java/stringsimilarity/JaroWinkler.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2015 Thibault Debatty.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.graphhopper.debatty.java.stringsimilarity;
 
 import java.util.Arrays;
@@ -35,7 +51,6 @@ public class JaroWinkler {
      * Instantiate with given threshold to determine when Winkler bonus should
      * be used.
      * Set threshold to a negative value to get the Jaro distance.
-     * @param threshold
      */
     public JaroWinkler(final double threshold) {
         this.threshold = threshold;
@@ -53,9 +68,6 @@ public class JaroWinkler {
 
     /**
      * Compute JW similarity.
-     * @param s1
-     * @param s2
-     * @return
      */
     public final double similarity(final String s1, final String s2) {
         int[] mtp = matches(s1, s2);
@@ -76,9 +88,6 @@ public class JaroWinkler {
 
     /**
      * Return 1 - similarity.
-     * @param s1
-     * @param s2
-     * @return
      */
     public final double distance(final String s1, final String s2) {
         return 1.0 - similarity(s1, s2);
