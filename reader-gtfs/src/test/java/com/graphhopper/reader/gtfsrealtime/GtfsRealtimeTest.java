18c0d3c47ee3ee7d3df4fd3ae52a4fc8d8905ee3
Michael Zilske <michael.zilske@tu-berlin.de>
2016-12-29 19:14:41 +0100

    GTFS-RT example files.

---
+++ reader-gtfs/src/test/java/com/graphhopper/reader/gtfsrealtime/GtfsRealtimeTest.java
@@ -0,0 +1,32 @@
+package com.graphhopper.reader.gtfsrealtime;
+
+import com.conveyal.gtfs.GTFSFeed;
+import com.conveyal.gtfs.model.Trip;
+import com.google.transit.realtime.GtfsRealtime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Comparator;
+
+public class GtfsRealtimeTest {
+
+    @Test
+    public void testData() throws IOException {
+        GTFSFeed gtfsFeed = GTFSFeed.fromFile("files/bart.zip");
+        URL url = new URL("file:files/bart.proto");
+        GtfsRealtime.FeedMessage feed = GtfsRealtime.FeedMessage.parseFrom(url.openStream());
+        GtfsRealtime.FeedEntity tripUpdateWithBiggestDelay = feed.getEntityList().stream()
+                .max(Comparator.comparingInt(entity ->
+                    entity.getTripUpdate().getStopTimeUpdateList().stream()
+                            .max(Comparator.comparingInt(update ->
+                                    update.getDeparture().getDelay())).get().getDeparture().getDelay()
+        )).get();
+        System.out.println(tripUpdateWithBiggestDelay);
+        Assert.assertEquals(tripUpdateWithBiggestDelay.getTripUpdate().getTrip().getTripId(), "20DCM21");
+        Trip trip = gtfsFeed.trips.get("20DCM21");
+        Assert.assertNotNull(trip);
+    }
+
+}
