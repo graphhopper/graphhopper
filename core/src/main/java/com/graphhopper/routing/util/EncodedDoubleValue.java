923c461437b49377034daec40a0718f937fc5e5f
boldtrn <boldtrn@gmail.com>
2017-02-20 13:34:49 +1100

    Removed rouding for checkValue and made sure that maxValue/factor is divideable without remainder

--- core/src/main/java/com/graphhopper/routing/util/EncodedDoubleValue.java
+++ core/src/main/java/com/graphhopper/routing/util/EncodedDoubleValue.java
@@ -31,6 +31,11 @@ public class EncodedDoubleValue extends EncodedValue {
 
     public EncodedDoubleValue(String name, int shift, int bits, double factor, long defaultValue, int maxValue, boolean allowZero) {
         super(name, shift, bits, factor, defaultValue, maxValue, allowZero);
+        double factorDivision = maxValue / factor;
+        // Interestingly we cannot use modulo here, e.g. 10%00.1 != 0
+        if (factorDivision != (int) factorDivision) {
+            throw new IllegalStateException("MaxValue needs to be divideable by factor without remainder");
+        }
     }
 
     @Override
@@ -54,7 +59,7 @@ public class EncodedDoubleValue extends EncodedValue {
 
         // scale value
         long tmpValue = Math.round(value / factor);
-        checkValue(Math.round(tmpValue * factor));
+        checkValue((long) (tmpValue * factor));
         tmpValue <<= shift;
 
         // clear value bits
