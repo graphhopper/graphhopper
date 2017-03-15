66af4bb165a968c8e1c062b2f5098fed3ef40a61
Michael Zilske <michael.zilske@tu-berlin.de>
2017-03-15 11:44:48 +0100

    Weiter flachklopfen. Jetzt wird alles in einer einzigen Methode aufgebaut, die zwar ziemlich lang ist -- aber das ist eine Abwägung gegen die Anzahl der beweglichen Teile, und die war vorher sehr hoch -- lauter Interfaces, Factories, Builder, Objekte und Vererbung. Wenn es nur darum geht, eine Methode zu unterteilen, sind einfach weitere (private) Methoden "Refactor --> extract method" Mittel der Wahl. Jetzt sehen wir auch die beiden for-Schleifen untereinander.

--- web/src/main/java/com/graphhopper/countries/Countries.java
+++ web/src/main/java/com/graphhopper/countries/Countries.java
@@ -8,7 +8,6 @@ import com.graphhopper.json.geo.JsonFeatureCollection;
 import com.graphhopper.routing.util.spatialrules.Polygon;
 import com.graphhopper.routing.util.spatialrules.SpatialRule;
 import com.graphhopper.routing.util.spatialrules.SpatialRuleLookup;
-import com.graphhopper.routing.util.spatialrules.countries.DefaultSpatialRule;
 import com.graphhopper.srlarray.SpatialRuleLookupArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +18,7 @@ import java.util.*;
 public class Countries {
 
     private static final Logger logger = LoggerFactory.getLogger(Countries.class);
+    private static final String JSON_ID_FIELD = "ISO_A3";
 
     public static SpatialRuleLookup buildIndex(Reader reader, String _rules) {
         GHJson ghJson = new GHJsonBuilder().create();
@@ -50,24 +50,7 @@ public class Countries {
             }
         }
 
-        return build(new SpatialRuleListFactory(spatialRules), jsonFeatureCollection, 1, true);
-    }
-
-    public static SpatialRuleLookup build(SpatialRuleFactory ruleFactory, JsonFeatureCollection jsonFeatureCollection,
-                                          double resolution, boolean exact) {
-        return build("ISO_A3", ruleFactory, jsonFeatureCollection, resolution, exact);
-    }
-
-    /**
-     * This method connects the rules with the jsonFeatureCollection via their <code>jsonProperty</code> property and the rules its
-     * getId method.
-     *
-     * @param jsonProperty the key that should be used to fetch the ID that is passed to SpatialRuleFactory#createSpatialRule
-     * @return the index or null if the specified bounds does not intersect with the calculated ones from the rules.
-     */
-    public static SpatialRuleLookup build(String jsonProperty, SpatialRuleFactory ruleFactory, JsonFeatureCollection jsonFeatureCollection, double resolution, boolean exact) {
-
-        List<SpatialRule> rules = new ArrayList<>();
+        List<SpatialRule> completeRules = new ArrayList<>();
         Set<String> ids = new HashSet<>();
         int unknownCounter = 0;
         for (JsonFeature jsonFeature : jsonFeatureCollection.getFeatures()) {
@@ -76,77 +59,48 @@ public class Countries {
                 continue;
 
             List<Polygon> borders = geometry.asPolygon().getPolygons();
-            String id = (String) jsonFeature.getProperty(jsonProperty);
+            String id = (String) jsonFeature.getProperty(JSON_ID_FIELD);
             if (id == null || id.isEmpty()) {
                 id = "_unknown_id_" + unknownCounter;
                 unknownCounter++;
             }
 
             if (ids.contains(id))
-                throw new RuntimeException("The id " + id + " was already used. Either leave the json property '" + jsonProperty + "' empty or use an unique id.");
+                throw new RuntimeException("The id " + id + " was already used. Either leave the json property '" + JSON_ID_FIELD + "' empty or use an unique id.");
 
             ids.add(id);
-            SpatialRule spatialRule = ruleFactory.createSpatialRule(id, borders);
-            if (spatialRule == SpatialRule.EMPTY)
-                continue;
 
-            rules.add(spatialRule);
+            Map<String, SpatialRule> ruleMap;
 
-        }
-
-        SpatialRuleLookup spatialRuleLookup = new SpatialRuleLookupArray(rules, resolution, exact);
-
-        logger.info("Created the SpatialRuleLookup with the following rules: " + Arrays.toString(rules.toArray()));
-
-        return spatialRuleLookup;
-    }
-
-    public interface SpatialRuleFactory {
-        /**
-         * This method creates a SpatialRule out of the provided polygons indicating the 'border'.
-         */
-        SpatialRule createSpatialRule(String id, final List<Polygon> polygons);
-    }
-
-    public static class SpatialRuleListFactory implements SpatialRuleFactory {
-        protected final Logger logger = LoggerFactory.getLogger(getClass());
-        protected Map<String, SpatialRule> ruleMap;
-
-        public SpatialRuleListFactory(SpatialRule... rules) {
-            this(Arrays.asList(rules));
-        }
-
-
-        public SpatialRuleListFactory(List<SpatialRule> rules) {
-            ruleMap = new HashMap<>(rules.size());
-            for (SpatialRule rule : rules) {
+            ruleMap = new HashMap<>(spatialRules.size());
+            for (SpatialRule rule : spatialRules) {
                 ruleMap.put(rule.getId(), rule);
             }
-        }
 
-        @Override
-        public SpatialRule createSpatialRule(String id, final List<Polygon> polygons) {
             if (id == null)
                 throw new IllegalArgumentException("ID cannot be null to find a SpatialRule");
 
             SpatialRule spatialRule = ruleMap.get(id);
             if (spatialRule != null) {
-                spatialRule.setBorders(polygons);
-                return spatialRule;
+                spatialRule.setBorders(borders);
+            } else {
+                spatialRule = SpatialRule.EMPTY;
             }
-            return SpatialRule.EMPTY;
+
+
+
+            if (spatialRule == SpatialRule.EMPTY)
+                continue;
+
+            completeRules.add(spatialRule);
+
         }
+
+        SpatialRuleLookup spatialRuleLookup = new SpatialRuleLookupArray(completeRules, (double) 1, true);
+
+        logger.info("Created the SpatialRuleLookup with the following rules: " + Arrays.toString(completeRules.toArray()));
+
+        return spatialRuleLookup;
     }
 
-    public static class SpatialRuleDefaultFactory implements SpatialRuleFactory {
-        @Override
-        public SpatialRule createSpatialRule(final String id, final List<Polygon> polygons) {
-            return new DefaultSpatialRule() {
-                @Override
-                public String getId() {
-                    return id;
-                }
-            }.setBorders(polygons);
-        }
-    }
 }
