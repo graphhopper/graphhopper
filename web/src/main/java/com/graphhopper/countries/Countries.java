c1b9fb663612634784d4abf0ff7b187e83ce169a
Michael Zilske <michael.zilske@tu-berlin.de>
2017-03-15 12:30:34 +0100

    Ich hab die beiden for-Schleifen mal umgestellt: Erst mache ich eine Map aus den Features, dann erzeuge ich die Rules. Das hat den charmanten Vorteil, dass es die Rules nur in einem Zustand gibt, nämlich in fertig -- und nicht einmal in fertig und einmal in unfertig, wobei sie in beiden durch die Gegend gereicht werden. Das ist jetzt ein bisschen Geschmackssache, aber meiner Meinung nach braucht man nicht zu versuchen, auf jeder Ebene maximal defensiv gegen alle Arten von Datenfehlern zu sein. Durch das Wegfallen von Ebenen können bestimmte Fehler gar nicht mehr auftreten -- wenn ich ein Objekt gerade selber erzeugt habe, brauche ich nicht mehr zu checken, ob es null ist (das sagt mir dann auch die IDE). Aber ich würde sogar behaupten, dass das hier ein Importer für *eine* Datei ist, nämlich für die, die da im Resource-Pfad liegt. Ziel ist es, den einfach genug zu klopfen, dass ein User ihn als Beispiel nehmen kann, wie sein eigener Importer für seine eigene Datei aussehen kann. Und wie diese *eine* Datei aussieht, wissen wir, sie ist ja eingecheckt. Deswegen brauchen wir nicht gegen Datenfehler zu hedgen, von denen wir wissen, dass diese Datei sie nicht hat. Bzw. können auch ein bisschen darauf vertrauen, dass es dann eben eine NullPointerException gibt, wenn das id-Feld nicht gesetzt ist oder dergleichen. (Auch, weil viel "haarigere" Tests ja gar nicht gemacht werden: Überlappen sich die Polygone? Du schreibst (was völlig OK ist), dass der Lookup selber entscheiden kann, wie er damit umgeht. Dann, finde ich aber, ist es auch OK, wenn ein bisschen unspezifiziert ist, was z.B. passiert, wenn eine id doppelt ist.

--- web/src/main/java/com/graphhopper/countries/Countries.java
+++ web/src/main/java/com/graphhopper/countries/Countries.java
@@ -1,11 +1,8 @@
 package com.graphhopper.countries;
 
-import com.graphhopper.json.GHJson;
 import com.graphhopper.json.GHJsonBuilder;
-import com.graphhopper.json.geo.Geometry;
 import com.graphhopper.json.geo.JsonFeature;
 import com.graphhopper.json.geo.JsonFeatureCollection;
-import com.graphhopper.routing.util.spatialrules.Polygon;
 import com.graphhopper.routing.util.spatialrules.SpatialRule;
 import com.graphhopper.routing.util.spatialrules.SpatialRuleLookup;
 import com.graphhopper.srlarray.SpatialRuleLookupArray;
@@ -21,86 +18,59 @@ public class Countries {
     private static final String JSON_ID_FIELD = "ISO_A3";
 
     public static SpatialRuleLookup buildIndex(Reader reader, String _rules) {
-        GHJson ghJson = new GHJsonBuilder().create();
-        JsonFeatureCollection jsonFeatureCollection = ghJson.fromJson(reader, JsonFeatureCollection.class);
-        String[] rules = (_rules.split(","));
+        JsonFeatureCollection jsonFeatureCollection = new GHJsonBuilder().create().fromJson(reader, JsonFeatureCollection.class);
+
+        Map<String, JsonFeature> jsonFeatures = toMap(jsonFeatureCollection);
 
         List<SpatialRule> spatialRules = new ArrayList<>();
+        String[] rules = (_rules.split(","));
         if (rules.length == 0) {
             throw new IllegalStateException("You have to pass at least one rule");
         }
         for (String rule : rules) {
-            try {
-                // Makes it easy to define a rule as CountrySpatialRule and skip the fqn
-                if (!rule.contains(".")) {
-                    rule = "com.graphhopper.routing.util.spatialrules.countries." + rule;
-                }
-                Object o = Class.forName(rule).newInstance();
-                if (SpatialRule.class.isAssignableFrom(o.getClass())) {
-                    spatialRules.add((SpatialRule) o);
-                } else {
-                    String ex = "Cannot find SpatialRule for rule " + rule + " but found " + o.getClass();
-                    logger.error(ex);
-                    throw new IllegalArgumentException(ex);
-                }
-            } catch (ReflectiveOperationException e) {
-                String ex = "Cannot find SpatialRule for rule " + rule;
-                logger.error(ex);
-                throw new IllegalArgumentException(ex, e);
-            }
+            spatialRules.add(createRuleByReflection(rule, jsonFeatures));
         }
 
-        List<SpatialRule> completeRules = new ArrayList<>();
-        Set<String> ids = new HashSet<>();
-        int unknownCounter = 0;
-        for (JsonFeature jsonFeature : jsonFeatureCollection.getFeatures()) {
-            Geometry geometry = jsonFeature.getGeometry();
-            if (!geometry.isPolygon())
-                continue;
+        SpatialRuleLookup spatialRuleLookup = new SpatialRuleLookupArray(spatialRules, (double) 1, true);
 
-            List<Polygon> borders = geometry.asPolygon().getPolygons();
-            String id = (String) jsonFeature.getProperty(JSON_ID_FIELD);
-            if (id == null || id.isEmpty()) {
-                id = "_unknown_id_" + unknownCounter;
-                unknownCounter++;
-            }
-
-            if (ids.contains(id))
-                throw new RuntimeException("The id " + id + " was already used. Either leave the json property '" + JSON_ID_FIELD + "' empty or use an unique id.");
-
-            ids.add(id);
-
-            Map<String, SpatialRule> ruleMap;
-
-            ruleMap = new HashMap<>(spatialRules.size());
-            for (SpatialRule rule : spatialRules) {
-                ruleMap.put(rule.getId(), rule);
-            }
-
-            if (id == null)
-                throw new IllegalArgumentException("ID cannot be null to find a SpatialRule");
-
-            SpatialRule spatialRule = ruleMap.get(id);
-            if (spatialRule != null) {
-                spatialRule.setBorders(borders);
-            } else {
-                spatialRule = SpatialRule.EMPTY;
-            }
-
-
-
-            if (spatialRule == SpatialRule.EMPTY)
-                continue;
-
-            completeRules.add(spatialRule);
-
-        }
-
-        SpatialRuleLookup spatialRuleLookup = new SpatialRuleLookupArray(completeRules, (double) 1, true);
-
-        logger.info("Created the SpatialRuleLookup with the following rules: " + Arrays.toString(completeRules.toArray()));
+        logger.info("Created the SpatialRuleLookup with the following rules: " + Arrays.toString(spatialRules.toArray()));
 
         return spatialRuleLookup;
+    }
+
+    private static Map<String, JsonFeature> toMap(JsonFeatureCollection jsonFeatureCollection) {
+        Map<String, JsonFeature> jsonFeatures = new HashMap<>();
+        for (JsonFeature jsonFeature : jsonFeatureCollection.getFeatures()) {
+            JsonFeature previous = jsonFeatures.put((String) jsonFeature.getProperty(JSON_ID_FIELD), jsonFeature);
+            if (previous != null) {
+                throw new RuntimeException("The id " + ((String) jsonFeature.getProperty(JSON_ID_FIELD)) + " was already used. Either leave the json property '" + JSON_ID_FIELD + "' empty or use an unique id.");
+            }
+        }
+        return jsonFeatures;
+    }
+
+    private static SpatialRule createRuleByReflection(String ruleClassName, Map<String, JsonFeature> jsonFeatures) {
+        SpatialRule o1;
+        try {
+            // Makes it easy to define a rule as CountrySpatialRule and skip the fqn
+            if (!ruleClassName.contains(".")) {
+                ruleClassName = "com.graphhopper.routing.util.spatialrules.countries." + ruleClassName;
+            }
+            Object o = Class.forName(ruleClassName).newInstance();
+            if (SpatialRule.class.isAssignableFrom(o.getClass())) {
+                o1 = (SpatialRule) o;
+            } else {
+                String ex = "Cannot find SpatialRule for rule " + ruleClassName + " but found " + o.getClass();
+                logger.error(ex);
+                throw new IllegalArgumentException(ex);
+            }
+        } catch (ReflectiveOperationException e) {
+            String ex = "Cannot find SpatialRule for rule " + ruleClassName;
+            logger.error(ex);
+            throw new IllegalArgumentException(ex, e);
+        }
+        o1.setBorders(jsonFeatures.get(o1.getId()).getGeometry().asPolygon().getPolygons());
+        return o1;
     }
 
 }
