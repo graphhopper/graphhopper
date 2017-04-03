32e9beb76ae0df9c585970ef6403a8fc82efb39d
Michael Zilske <michael.zilske@tu-berlin.de>
2017-04-03 19:11:19 +0200

    RouteSerializer can now serialize Legs.

--- web/src/main/java/com/graphhopper/http/SimpleRouteSerializer.java
+++ web/src/main/java/com/graphhopper/http/SimpleRouteSerializer.java
@@ -17,6 +17,16 @@
  */
 package com.graphhopper.http;
 
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
 import com.graphhopper.GHResponse;
 import com.graphhopper.PathWrapper;
 import com.graphhopper.util.Helper;
@@ -25,16 +35,41 @@ import com.graphhopper.util.PointList;
 import com.graphhopper.util.exceptions.GHException;
 import com.graphhopper.util.shapes.BBox;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Peter Karich
  */
 public class SimpleRouteSerializer implements RouteSerializer {
     private final BBox maxBounds;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public SimpleRouteSerializer(BBox maxBounds) {
         this.maxBounds = maxBounds;
+        this.objectMapper.setDateFormat(new SimpleDateFormat("YYYY-MM-dd'T'HH:mm")); // ISO8601 without time zone
+        this.objectMapper.registerModule(new JtsModule());
+
+        // Because VirtualEdgeIteratorState has getters which throw Exceptions.
+        // http://stackoverflow.com/questions/35359430/how-to-make-jackson-ignore-properties-if-the-getters-throw-exceptions
+        this.objectMapper.registerModule(new SimpleModule().setSerializerModifier(new BeanSerializerModifier() {
+            @Override
+            public List<BeanPropertyWriter> changeProperties(SerializationConfig config, BeanDescription beanDesc, List<BeanPropertyWriter> beanProperties) {
+                return beanProperties.stream().map(bpw -> new BeanPropertyWriter(bpw) {
+                    @Override
+                    public void serializeAsField(Object bean, JsonGenerator gen, SerializerProvider prov) throws Exception {
+                        try {
+                            super.serializeAsField(bean, gen, prov);
+                        } catch (Exception e) {
+                            // Ignoring expected exception, see above.
+                        }
+                    }
+                }).collect(Collectors.toList());
+            }
+        }));
+
     }
 
     private String getMessage(Throwable t) {
@@ -77,6 +112,7 @@ public class SimpleRouteSerializer implements RouteSerializer {
                 jsonPath.put("distance", Helper.round(ar.getDistance(), 3));
                 jsonPath.put("weight", Helper.round6(ar.getRouteWeight()));
                 jsonPath.put("time", ar.getTime());
+                jsonPath.put("transfers", ar.getNumChanges());
                 if (!ar.getDescription().isEmpty())
                     jsonPath.put("description", ar.getDescription());
 
@@ -96,11 +132,16 @@ public class SimpleRouteSerializer implements RouteSerializer {
                         jsonPath.put("instructions", instructions.createJson());
                     }
 
+                    jsonPath.put("legs", objectMapper.convertValue(ar.getLegs(), new TypeReference<List<Map<String, Object>>>() {}));
+
                     jsonPath.put("ascend", ar.getAscend());
                     jsonPath.put("descend", ar.getDescend());
                 }
 
                 jsonPath.put("snapped_waypoints", createPoints(ar.getWaypoints(), pointsEncoded, includeElevation));
+                if (ar.getFare() != null) {
+                    jsonPath.put("fare", NumberFormat.getCurrencyInstance().format(ar.getFare()));
+                }
                 jsonPathList.add(jsonPath);
             }
 
