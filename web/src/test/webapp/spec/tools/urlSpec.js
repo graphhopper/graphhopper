02a10a96493b0a3918fea28cbc5efd6c3e043b8d
Peter <graphhopper@gmx.de>
2017-05-12 19:37:26 +0200

    flat url parameters fixes #1034 (#1057)

--- web/src/test/webapp/spec/tools/urlSpec.js
+++ web/src/test/webapp/spec/tools/urlSpec.js
@@ -17,4 +17,25 @@ describe('urlTools', function () {
         params = urlTools.parseUrl("blup?point=&point=49.946505%2C11.571232");
         expect(params.point).toEqual(["", "49.946505,11.571232"]);
     });
+
+    it("should create object from dotted URL parameter", function () {
+        var someObject = urlTools.mergeParamIntoObject({}, "one.two", "12");
+        expect("12").toEqual(someObject.one.two);
+
+        someObject = urlTools.mergeParamIntoObject({"one": {"xy": "34"}}, "one.two", "12");
+        expect("12").toEqual(someObject.one.two);
+        expect("34").toEqual(someObject.one.xy);
+
+        someObject = urlTools.mergeParamIntoObject({}, "one.two.three", "123");
+        expect("123").toEqual(someObject.one.two.three);
+
+        var params = urlTools.parseUrl("localhost:8989?pt.test=now&pt.go.test=single&pt.go.further=far&pt.go.further=now");
+        expect("now").toEqual(params.pt.test);
+        expect("single").toEqual(params.pt.go.test);
+        expect(["far", "now"]).toEqual(params.pt.go.further);
+
+        // does not work at the moment: the second parameter is skipped
+        // params = urlTools.parseUrl("localhost:8989?pt.mix=now&pt.mix.test=now2");
+        // expect(["now", {"test" : "now2"}]).toEqual(params.pt.mix);
+    });
 });
