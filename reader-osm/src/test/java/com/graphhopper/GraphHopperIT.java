b4f723b3a06212b93f52b1ae16da94a054ba102d
Robin <boldtrn@users.noreply.github.com>
2017-07-02 15:19:35 +0200

    Updated Stopover texts for German and English (#1101)
    * Updated Stopover texts for German and English
    
    * Fixed test

--- reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
+++ reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
@@ -337,7 +337,7 @@ public class GraphHopperIT {
         assertEquals("Turn left", resultJson.get(5).get("text"));
         assertEquals("Turn right onto Avenue Albert II", resultJson.get(6).get("text"));
 
-        assertEquals("Stopover 1", resultJson.get(20).get("text"));
+        assertEquals("Waypoint 1", resultJson.get(20).get("text"));
 
         assertEquals("Continue onto Avenue Albert II", resultJson.get(31).get("text"));
         assertEquals("Turn left", resultJson.get(32).get("text"));
