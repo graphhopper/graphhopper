3051eca5683ed9bf1446419fd9e0c4052c33a5a5
Peter <graphhopper@gmx.de>
2017-09-27 15:03:02 +0200

    reduce memory usage for PointList.copy, #1166

--- core/src/main/java/com/graphhopper/util/PointList.java
+++ core/src/main/java/com/graphhopper/util/PointList.java
@@ -424,13 +424,16 @@ public class PointList implements Iterable<GHPoint3D>, PointAccess {
         return clonePL;
     }
 
+    /**
+     * This method does a deep copy of this object for the specified range.
+     */
     public PointList copy(int from, int end) {
         if (from > end)
             throw new IllegalArgumentException("from must be smaller or equals to end");
         if (from < 0 || end > size)
             throw new IllegalArgumentException("Illegal interval: " + from + ", " + end + ", size:" + size);
 
-        PointList copyPL = new PointList(size, is3D);
+        PointList copyPL = new PointList(end - from, is3D);
         if (is3D)
             for (int i = from; i < end; i++) {
                 copyPL.add(latitudes[i], longitudes[i], elevations[i]);
