a87cc9aee2530b9048a7c5eb6fec6a6188f08dfa
Oliver Schlüter <oschlueter@users.noreply.github.com>
2017-10-09 17:07:16 +0200

    moved construction of message and details of ConnectionNotFoundException into the class

--- core/src/main/java/com/graphhopper/util/PathMerger.java
+++ core/src/main/java/com/graphhopper/util/PathMerger.java
@@ -128,8 +128,7 @@ public class PathMerger {
             altRsp.setInstructions(fullInstructions);
 
         if (!allFound) {
-            altRsp.addError(new ConnectionNotFoundException("Connection between locations not found: " +
-                    unconnectedListString(paths), unconnectedInfo(altRsp, paths)));
+            altRsp.addError(new ConnectionNotFoundException(paths, altRsp));
         }
 
         altRsp.setDescription(description).
@@ -181,54 +180,5 @@ public class PathMerger {
         }
         rsp.setAscend(ascendMeters);
         rsp.setDescend(descendMeters);
-    }
-
-    private String unconnectedListString(List<Path> paths) {
-        StringBuilder sb = new StringBuilder().append("[");
-
-        for (int n = 0; n < paths.size(); ++n) {
-            if (!paths.get(n).isFound())
-                sb.append(String.format("(%d,%d), ", n, n+1));
-        }
-
-        if (sb.length() < 2)
-            throw new IllegalArgumentException("list doesn't have unavailable paths");
-
-        sb.setLength(sb.length() - 2); // delete last occurrence of  ", "
-        sb.append("]");
-
-        return sb.toString();
-    }
-
-    private Map<String, Object> unconnectedInfo(PathWrapper altRsp, List<Path> paths) {
-        PointList waypoints = altRsp.getWaypoints();
-
-        ArrayList<Object> idxFrom = new ArrayList<>();
-        ArrayList<Object> latFrom = new ArrayList<>();
-        ArrayList<Object> lonFrom = new ArrayList<>();
-        ArrayList<Object> idxTo = new ArrayList<>();
-        ArrayList<Object> latTo = new ArrayList<>();
-        ArrayList<Object> lonTo = new ArrayList<>();
-
-        for (int pathIndex = 0; pathIndex < paths.size(); ++pathIndex) {
-            if (!paths.get(pathIndex).isFound()) {
-                idxFrom.add(pathIndex);
-                latFrom.add(waypoints.getLat(pathIndex));
-                lonFrom.add(waypoints.getLon(pathIndex));
-                idxTo.add(pathIndex + 1);
-                latTo.add(waypoints.getLat(pathIndex + 1));
-                lonTo.add(waypoints.getLon(pathIndex + 1));
-            }
-        }
-
-        Map<String, Object> info = new HashMap<>();
-        info.put("idxFrom", idxFrom);
-        info.put("latFrom", latFrom);
-        info.put("lonFrom", lonFrom);
-        info.put("idxTo", idxTo);
-        info.put("latTo", latTo);
-        info.put("lonTo", lonTo);
-
-        return info;
     }
 }
--- core/src/main/java/com/graphhopper/util/exceptions/ConnectionNotFoundException.java
+++ core/src/main/java/com/graphhopper/util/exceptions/ConnectionNotFoundException.java
@@ -17,6 +17,13 @@
  */
 package com.graphhopper.util.exceptions;
 
+import com.graphhopper.PathWrapper;
+import com.graphhopper.routing.Path;
+import com.graphhopper.util.PointList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,5 +34,59 @@ import java.util.Map;
 public class ConnectionNotFoundException extends DetailedIllegalArgumentException {
     public ConnectionNotFoundException(String var1, Map<String, Object> details) {
         super(var1, details);
+    }
+
+    public ConnectionNotFoundException(List<Path> paths, PathWrapper altRsp) {
+        super("Connection between locations not found: " +
+                unconnectedListString(paths), unconnectedInfo(altRsp, paths));
+    }
+
+    private static String unconnectedListString(List<Path> paths) {
+        StringBuilder sb = new StringBuilder().append("[");
+
+        for (int n = 0; n < paths.size(); ++n) {
+            if (!paths.get(n).isFound())
+                sb.append(String.format("(%d,%d), ", n, n+1));
+        }
+
+        if (sb.length() < 2)
+            throw new IllegalArgumentException("list doesn't have unavailable paths");
+
+        sb.setLength(sb.length() - 2); // delete last occurrence of  ", "
+        sb.append("]");
+
+        return sb.toString();
+    }
+
+    private static Map<String, Object> unconnectedInfo(PathWrapper altRsp, List<Path> paths) {
+        PointList waypoints = altRsp.getWaypoints();
+
+        ArrayList<Object> idxFrom = new ArrayList<>();
+        ArrayList<Object> latFrom = new ArrayList<>();
+        ArrayList<Object> lonFrom = new ArrayList<>();
+        ArrayList<Object> idxTo = new ArrayList<>();
+        ArrayList<Object> latTo = new ArrayList<>();
+        ArrayList<Object> lonTo = new ArrayList<>();
+
+        for (int pathIndex = 0; pathIndex < paths.size(); ++pathIndex) {
+            if (!paths.get(pathIndex).isFound()) {
+                idxFrom.add(pathIndex);
+                latFrom.add(waypoints.getLat(pathIndex));
+                lonFrom.add(waypoints.getLon(pathIndex));
+                idxTo.add(pathIndex + 1);
+                latTo.add(waypoints.getLat(pathIndex + 1));
+                lonTo.add(waypoints.getLon(pathIndex + 1));
+            }
+        }
+
+        Map<String, Object> info = new HashMap<>();
+        info.put("idxFrom", idxFrom);
+        info.put("latFrom", latFrom);
+        info.put("lonFrom", lonFrom);
+        info.put("idxTo", idxTo);
+        info.put("latTo", latTo);
+        info.put("lonTo", lonTo);
+
+        return info;
     }
 }
