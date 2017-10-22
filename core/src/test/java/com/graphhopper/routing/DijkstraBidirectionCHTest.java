e36b75f7b1dc9db4bc6224de397ed4ddee26dbd0
ammagamma <contactammagamma@gmail.com>
2017-10-22 21:33:44 +0200

    Adds author tag and removes stall on demand public constant.

--- core/src/test/java/com/graphhopper/routing/DijkstraBidirectionCHTest.java
+++ core/src/test/java/com/graphhopper/routing/DijkstraBidirectionCHTest.java
@@ -33,7 +33,6 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.graphhopper.routing.ch.PrepareContractionHierarchies.STALL_ON_DEMAND;
 import static org.junit.Assert.assertEquals;
 
 /**
