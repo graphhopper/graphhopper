8bb61a9c456ec4322a01e5e53d18e80654d2d42f
Michael Zilske <michael.zilske@tu-berlin.de>
2018-03-02 11:39:53 -0800

    Restore transfers on loadExisting
    Signed-off-by: Michael Zilske <michael.zilske@tu-berlin.de>
    
    (cherry picked from commit 9445f83)
    Signed-off-by: Michael Zilske <michael.zilske@tu-berlin.de>

--- reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/GtfsStorage.java
+++ reader-gtfs/src/main/java/com/graphhopper/reader/gtfs/GtfsStorage.java
@@ -153,6 +153,7 @@ public class GtfsStorage implements GraphExtension, GtfsStorageI {
 			try {
 				GTFSFeed feed = new GTFSFeed(dir.getLocation() + "/" + gtfsFeedId);
 				this.gtfsFeeds.put(gtfsFeedId, feed);
+				this.transfers.put(gtfsFeedId, new Transfers(feed));
 			} catch (IOException | ExecutionException e) {
 				throw new RuntimeException(e);
 			}
