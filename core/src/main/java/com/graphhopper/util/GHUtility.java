03b50551241ab31fc90e8a6f5e496af25c6a2e13
ammagamma <contactammagamma@gmail.com>
2018-04-11 22:22:14 +0200

    Fixes another default locale.
    Signed-off-by: ammagamma <contactammagamma@gmail.com>

--- core/src/main/java/com/graphhopper/util/GHUtility.java
+++ core/src/main/java/com/graphhopper/util/GHUtility.java
@@ -154,7 +154,8 @@ public class GHUtility {
         }
         int from = fwd ? edge.getBaseNode() : edge.getAdjNode();
         int to = fwd ? edge.getAdjNode() : edge.getBaseNode();
-        System.out.printf("graph.edge(%d, %d, %f, %s);\n", from, to, edge.getDistance(), fwd && bwd ? "true" : "false");
+        System.out.printf(Locale.ROOT,
+                "graph.edge(%d, %d, %f, %s);\n", from, to, edge.getDistance(), fwd && bwd ? "true" : "false");
     }
 
     public static void printInfo(final Graph g, int startNode, final int counts, final EdgeFilter filter) {
