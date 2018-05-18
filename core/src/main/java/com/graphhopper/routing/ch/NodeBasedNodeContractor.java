84b6b0035e7f64979795b01d4ee1ca941aeddb80
ammagamma <contactammagamma@gmail.com>
2018-05-18 08:04:08 +0200

    Moves edge explorer initialization one level up.
    Signed-off-by: ammagamma <contactammagamma@gmail.com>

--- core/src/main/java/com/graphhopper/routing/ch/NodeBasedNodeContractor.java
+++ core/src/main/java/com/graphhopper/routing/ch/NodeBasedNodeContractor.java
@@ -54,11 +54,7 @@ class NodeBasedNodeContractor extends AbstractNodeContractor {
     public void initFromGraph() {
         super.initFromGraph();
         ignoreNodeFilter = new IgnoreNodeFilter(prepareGraph, maxLevel);
-        FlagEncoder prepareFlagEncoder = prepareWeighting.getFlagEncoder();
-        inEdgeExplorer = prepareGraph.createEdgeExplorer(new DefaultEdgeFilter(prepareFlagEncoder, true, false));
-        outEdgeExplorer = prepareGraph.createEdgeExplorer(new DefaultEdgeFilter(prepareFlagEncoder, false, true));
-
-        final EdgeFilter allFilter = new DefaultEdgeFilter(prepareFlagEncoder, true, true);
+        final EdgeFilter allFilter = new DefaultEdgeFilter(encoder, true, true);
         final EdgeFilter remainingNodesFilter = new LevelEdgeFilter(prepareGraph) {
             @Override
             public final boolean accept(EdgeIteratorState edgeState) {
