5b9b12263b63d0d39447b6a25e1ac8b5f4ee61c8
Michael Zilske <michael.zilske@tu-berlin.de>
2018-05-30 11:01:39 +0200

    Real edge ids in path details, #1384

--- web/src/test/java/com/graphhopper/http/resources/RouteResourceIT.java
+++ web/src/test/java/com/graphhopper/http/resources/RouteResourceIT.java
@@ -25,6 +25,7 @@ import com.graphhopper.PathWrapper;
 import com.graphhopper.api.GraphHopperWeb;
 import com.graphhopper.http.GraphHopperApplication;
 import com.graphhopper.http.GraphHopperServerConfiguration;
+import com.graphhopper.resources.NearestResource;
 import com.graphhopper.util.CmdArgs;
 import com.graphhopper.util.Helper;
 import com.graphhopper.util.details.PathDetail;
@@ -219,7 +220,7 @@ public class RouteResourceIT {
 
     @Test
     public void testPathDetailsWithoutGraphHopperWeb() throws Exception {
-        final Response response = app.client().target("http://localhost:8080/route?point=42.554851,1.536198&point=42.510071,1.548128&details=average_speed").request().buildGet().invoke();
+        final Response response = app.client().target("http://localhost:8080/route?point=42.554851,1.536198&point=42.510071,1.548128&details=average_speed&details=edge_id").request().buildGet().invoke();
         assertEquals(200, response.getStatus());
         JsonNode json = response.readEntity(JsonNode.class);
         JsonNode infoJson = json.get("info");
@@ -233,6 +234,12 @@ public class RouteResourceIT {
         assertEquals(14, averageSpeed.get(0).get(1).asInt());
         assertEquals(60.0, averageSpeed.get(1).get(2).asDouble(), .01);
         assertEquals(19, averageSpeed.get(1).get(1).asInt());
+        assertTrue(details.has("edge_id"));
+        JsonNode edgeIds = details.get("edge_id");
+        int firstLink = edgeIds.get(0).get(2).asInt();
+        int lastLink = edgeIds.get(edgeIds.size()-1).get(2).asInt();
+        assertEquals(880, firstLink);
+        assertEquals(1419, lastLink);
     }
 
     @Test
