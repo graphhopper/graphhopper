0e7af5c3766eb3adf35ff92cef9a78c67b3e22b8
Michael Zilske <michael.zilske@tu-berlin.de>
2018-05-30 17:43:09 +0200

    Module name

--- web-api/src/main/java/com/graphhopper/jackson/CmdArgsDeserializer.java
+++ web-api/src/main/java/com/graphhopper/jackson/CmdArgsDeserializer.java
@@ -1,7 +1,6 @@
 package com.graphhopper.jackson;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
