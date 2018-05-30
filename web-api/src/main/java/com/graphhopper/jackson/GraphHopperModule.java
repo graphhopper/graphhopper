3a325b7dc5c2e443f07f8499010ad544b35bc48d
Michael Zilske <michael.zilske@tu-berlin.de>
2018-05-30 17:31:08 +0200

    Fix CmdArgs deserialization without Annotation

--- web-api/src/main/java/com/graphhopper/jackson/GraphHopperModule.java
+++ web-api/src/main/java/com/graphhopper/jackson/GraphHopperModule.java
@@ -1,6 +1,7 @@
 package com.graphhopper.jackson;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.graphhopper.util.CmdArgs;
 import com.graphhopper.util.InstructionList;
 import com.graphhopper.util.details.PathDetail;
 import com.graphhopper.util.shapes.BBox;
@@ -16,6 +17,7 @@ public class GraphHopperModule extends SimpleModule {
         addDeserializer(PathDetail.class, new PathDetailDeserializer());
         addSerializer(PathDetail.class, new PathDetailSerializer());
         addSerializer(InstructionList.class, new InstructionListSerializer());
+        addDeserializer(CmdArgs.class, new CmdArgsDeserializer());
     }
 
 }
