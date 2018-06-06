bdfc1785574101b8e3320badc66bd17f489dbcbf
Michael Zilske <michael.zilske@tu-berlin.de>
2018-06-06 17:15:51 +0200

    ...and the code comment

--- web-bundle/src/main/java/com/graphhopper/http/GraphHopperBundle.java
+++ web-bundle/src/main/java/com/graphhopper/http/GraphHopperBundle.java
@@ -183,7 +183,7 @@ public class GraphHopperBundle implements ConfiguredBundle<GraphHopperBundleConf
     public void run(GraphHopperBundleConfiguration configuration, Environment environment) {
         configuration.getGraphHopperConfiguration().merge(CmdArgs.readFromSystemProperties());
 
-        // If the "?format=gpx" parameter is present, sets a corresponding media type header
+        // If the "?type=gpx" parameter is present, sets a corresponding media type header
         environment.jersey().register(new TypeGPXFilter());
 
         // Together, these two take care that MultiExceptions thrown from RouteResource
