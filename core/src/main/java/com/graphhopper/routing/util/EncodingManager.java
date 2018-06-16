526038abadf03bffd9905cf0fab57d13c3183d72
Peter <graphhopper@gmx.de>
2018-06-16 21:07:01 +0200

    fixed error message to new config

--- core/src/main/java/com/graphhopper/routing/util/EncodingManager.java
+++ core/src/main/java/com/graphhopper/routing/util/EncodingManager.java
@@ -44,7 +44,7 @@ import static com.graphhopper.util.Helper.toLowerCase;
  */
 public class EncodingManager {
     private static final String ERR = "Encoders are requesting %s bits, more than %s bits of %s flags. ";
-    private static final String WAY_ERR = "Decrease the number of vehicles or increase the flags to take long via graph.bytes_for_flags=8";
+    private static final String WAY_ERR = "Decrease the number of vehicles or increase the flags to take long via graph.bytes_for_flags: 8";
     private final List<AbstractFlagEncoder> edgeEncoders = new ArrayList<>();
     private final int bitsForEdgeFlags;
     private final int bitsForTurnFlags = 8 * 4;
