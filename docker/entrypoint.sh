78e3cbe1c8dafec02ad7755323b1511eefeb5fb5
shivek <shivek.sacklecha@curefit.com>
2018-08-12 15:35:24 +0530

    codeDeploy local changes

---
+++ docker/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# PROD Setting : The Heap Memory settings are based on the assumptions that the application will run mostly on 8Gb Ram Box.  (For fine tuning Heap size look into : https://support.cloudbees.com/hc/en-us/articles/204859670-Java-Heap-settings-best-practice )
+# PROD Setting : The G1GC Garbage collector is used initially, but based on the metrics and performance we can try -XX:+UseConcMarkSweepGC and keep whichever workds better.
+
+
+echo "GRAPHHOPPER will start in ENVIRONMENT : $ENVIRONMENT"
+
+./graphhopper.sh build
+./graphhopper.sh -a web -i ./osrm_location.osm.pbf
