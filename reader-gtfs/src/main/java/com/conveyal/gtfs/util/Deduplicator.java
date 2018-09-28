94adc4e830e785a47a98d710c93fb3f02a35bd60
Michael Zilske <michael.zilske@tu-berlin.de>
2018-09-28 16:29:46 +0530

    Add Conveyal's license as header, remove as file

--- reader-gtfs/src/main/java/com/conveyal/gtfs/util/Deduplicator.java
+++ reader-gtfs/src/main/java/com/conveyal/gtfs/util/Deduplicator.java
@@ -1,10 +1,34 @@
+/*
+ * Copyright (c) 2015, Conveyal
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.conveyal.gtfs.util;
 
 import com.google.common.collect.Maps;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.BitSet;
 import java.util.Map;
 
 /**
@@ -14,29 +38,11 @@ import java.util.Map;
 public class Deduplicator implements Serializable {
     private static final long serialVersionUID = 20140524L;
 
-    private final Map<IntArray, IntArray> canonicalIntArrays = Maps.newHashMap();
     private final Map<String, String> canonicalStrings = Maps.newHashMap();
-    private final Map<BitSet, BitSet> canonicalBitSets = Maps.newHashMap();
-    private final Map<StringArray, StringArray> canonicalStringArrays = Maps.newHashMap();
 
     /** Free up any memory used by the deduplicator. */
     public void reset() {
-        canonicalIntArrays.clear();
         canonicalStrings.clear();
-        canonicalBitSets.clear();
-        canonicalStringArrays.clear();
-    }
-
-    /** Used to deduplicate time and stop sequence arrays. The same times may occur in many trips. */
-    public int[] deduplicateIntArray(int[] original) {
-        if (original == null) return null;
-        IntArray intArray = new IntArray(original);
-        IntArray canonical = canonicalIntArrays.get(intArray);
-        if (canonical == null) {
-            canonical = intArray;
-            canonicalIntArrays.put(canonical, canonical);
-        }
-        return canonical.array;
     }
 
     public String deduplicateString(String original) {
@@ -49,66 +55,4 @@ public class Deduplicator implements Serializable {
         return canonical;
     }
 
-    public BitSet deduplicateBitSet(BitSet original) {
-        if (original == null) return null;
-        BitSet canonical = canonicalBitSets.get(original);
-        if (canonical == null) {
-            canonical = original;
-            canonicalBitSets.put(canonical, canonical);
-        }
-        return canonical;
-    }
-
-    public String[] deduplicateStringArray(String[] original) {
-        if (original == null) return null;
-        StringArray canonical = canonicalStringArrays.get(new StringArray(original, false));
-        if (canonical == null) {
-            canonical = new StringArray(original, true);
-            canonicalStringArrays.put(canonical, canonical);
-        }
-        return canonical.array;
-    }
-
-    /** A wrapper for a primitive int array. This is insane but necessary in Java. */
-    private class IntArray implements Serializable {
-        private static final long serialVersionUID = 20140524L;
-        final int[] array;
-        IntArray(int[] array) {
-            this.array = array;
-        }
-        @Override
-        public boolean equals (Object other) {
-            if (other instanceof IntArray) {
-                return Arrays.equals(array, ((IntArray) other).array);
-            } else return false;
-        }
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(array);
-        }
-    }
-
-    /** A wrapper for a String array. Optionally, the individual Strings may be deduplicated too. */
-    private class StringArray implements Serializable {
-        private static final long serialVersionUID = 20140524L;
-        final String[] array;
-        StringArray(String[] array, boolean deduplicateStrings) {
-            if (deduplicateStrings) {
-                this.array = new String[array.length];
-                for (int i = 0; i < array.length; i++) {
-                    this.array[i] = deduplicateString(array[i]);
-                }
-            } else this.array = array;
-        }
-        @Override
-        public boolean equals (Object other) {
-            if (other instanceof StringArray) {
-                return Arrays.equals(array, ((StringArray) other).array);
-            } else return false;
-        }
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(array);
-        }
-    }
 }
