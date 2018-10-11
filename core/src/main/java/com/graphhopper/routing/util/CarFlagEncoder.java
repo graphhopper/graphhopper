ebb56a6f75b25ad22e99655b339255f613bf60ad
Casper Hollemans <casper.hollemans@route.nl>
2018-10-11 08:25:31 +0200

    Added barrier:cattle_grid to potential barrier list for all main flag encoders.

--- core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -83,6 +83,7 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
         potentialBarriers.add("lift_gate");
         potentialBarriers.add("kissing_gate");
         potentialBarriers.add("swing_gate");
+        potentialBarriers.add("cattle_grid");
 
         absoluteBarriers.add("fence");
         absoluteBarriers.add("bollard");
