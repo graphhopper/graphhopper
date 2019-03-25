9b9cd1a1517b9a29f31a4829e0360377c6d8a032
Shivek Sacklecha <shivek.sacklecha@CF-LP00049.local>
2019-03-25 18:51:47 +0530

    added sleep to give some time to drain requests

--- codedeploy/cleanup.sh
+++ codedeploy/cleanup.sh
@@ -1,4 +1,6 @@
 #true is given so that even if the container doesn't exists, the command should not fail, because if the command fails, it will fail the CodeDeploy process also.
+# Sleeping for 60 seconds before stopping the container. This will ensure that if there are any current requests being served they should be completed.
+sleep 60
 sudo docker stop graphhopper || true
 sudo docker rm graphhopper || true
 sudo rm -rf /home/ubuntu/graphhopper
