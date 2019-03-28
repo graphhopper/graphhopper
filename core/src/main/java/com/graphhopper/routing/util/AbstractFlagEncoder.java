bb29e7f43644ded69c2eca1f0aa05a39c63daf3d
easbar <easbar.mail@posteo.net>
2019-03-28 15:21:31 +0100

    Fixes compiler warning.
    Signed-off-by: easbar <easbar.mail@posteo.net>

--- core/src/main/java/com/graphhopper/routing/util/AbstractFlagEncoder.java
+++ core/src/main/java/com/graphhopper/routing/util/AbstractFlagEncoder.java
@@ -605,7 +605,7 @@ public abstract class AbstractFlagEncoder implements FlagEncoder {
     }
 
     @Override
-    public <T extends Enum> EnumEncodedValue getEnumEncodedValue(String key, Class<T> enumType) {
+    public <T extends Enum> EnumEncodedValue<T> getEnumEncodedValue(String key, Class<T> enumType) {
         return encodedValueLookup.getEnumEncodedValue(key, enumType);
     }
 
