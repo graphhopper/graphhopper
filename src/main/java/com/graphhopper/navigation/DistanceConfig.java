b7cf202b3eb933d91aa7f309e08e902d9f6cb37e
Stefan Kienzl <stefan@riserapp.cpm>
2019-05-21 10:03:30 +0200

    Set right inital voice instruction key

--- src/main/java/com/graphhopper/navigation/DistanceConfig.java
+++ src/main/java/com/graphhopper/navigation/DistanceConfig.java
@@ -14,14 +14,14 @@ public class DistanceConfig {
         this.unit = unit;
         if (unit == DistanceUtils.Unit.METRIC) {
             voiceInstructions = Arrays.asList(
-                    new InitialVoiceInstructionConfig(IN_HIGHER_DISTANCE_PLURAL.metric, 4250, 250, unit),
+                    new InitialVoiceInstructionConfig(FOR_HIGHER_DISTANCE_PLURAL.metric, 4250, 250, unit),
                     new FixedVoiceInstructionConfig(IN_HIGHER_DISTANCE_PLURAL.metric, 2000, 2),
                     new FixedVoiceInstructionConfig(IN_HIGHER_DISTANCE_SINGULAR.metric, 1000, 1),
                     new DecisionVoiceInstructionConfig(IN_LOWER_DISTANCE_PLURAL.metric, new int[]{400, 200}, new int[]{400, 200})
             );
         } else {
             voiceInstructions = Arrays.asList(
-                    new InitialVoiceInstructionConfig(IN_HIGHER_DISTANCE_PLURAL.metric, 4250, 250, unit),
+                    new InitialVoiceInstructionConfig(FOR_HIGHER_DISTANCE_PLURAL.metric, 4250, 250, unit),
                     new FixedVoiceInstructionConfig(IN_HIGHER_DISTANCE_PLURAL.imperial, 3220, 2),
                     new FixedVoiceInstructionConfig(IN_HIGHER_DISTANCE_SINGULAR.imperial, 1610, 1),
                     new DecisionVoiceInstructionConfig(IN_LOWER_DISTANCE_PLURAL.imperial, new int[]{400, 200}, new int[]{1300, 600})
