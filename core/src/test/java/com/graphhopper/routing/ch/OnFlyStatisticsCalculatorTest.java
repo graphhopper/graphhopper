2dbb480829b97395d355374f4760008e88188ad1
Anvoker <anlogcode@gmail.com>
2019-06-02 15:24:48 +0300

    Migrating OnFlyStatisticsCalculatorTest to JUnit5 assertions.

--- core/src/test/java/com/graphhopper/routing/ch/OnFlyStatisticsCalculatorTest.java
+++ core/src/test/java/com/graphhopper/routing/ch/OnFlyStatisticsCalculatorTest.java
@@ -19,7 +19,7 @@ package com.graphhopper.routing.ch;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OnFlyStatisticsCalculatorTest {
 
