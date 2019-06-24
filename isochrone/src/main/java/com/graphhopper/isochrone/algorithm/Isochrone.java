ffa9aedca27e91f052f60e77a418e06d20a25e71
easbar <easbar.mail@posteo.net>
2019-06-24 11:24:14 +0200

    Removes comment.

--- isochrone/src/main/java/com/graphhopper/isochrone/algorithm/Isochrone.java
+++ isochrone/src/main/java/com/graphhopper/isochrone/algorithm/Isochrone.java
@@ -191,7 +191,6 @@ public class Isochrone extends AbstractRoutingAlgorithm {
                     continue;
                 }
                 // minor speed up
-                // todonow: really ?
                 if (currEdge.edge == iter.getEdge()) {
                     continue;
                 }
