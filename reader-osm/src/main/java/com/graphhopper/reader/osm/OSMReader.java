d0fc07a003bb22254cc460898ce39da3723479a1
guillas <s.guillas@systel-sa.com>
2019-08-08 13:45:07 +0200

    import EncodingManager inner classes

--- reader-osm/src/main/java/com/graphhopper/reader/osm/OSMReader.java
+++ reader-osm/src/main/java/com/graphhopper/reader/osm/OSMReader.java
@@ -27,6 +27,8 @@ import com.graphhopper.reader.osm.OSMTurnRelation.TurnCostTableEntry;
 import com.graphhopper.routing.profiles.BooleanEncodedValue;
 import com.graphhopper.routing.util.DefaultEdgeFilter;
 import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.routing.util.EncodingManager.AcceptRelation;
+import com.graphhopper.routing.util.EncodingManager.RelationAcceptation;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.weighting.TurnWeighting;
 import com.graphhopper.storage.*;
