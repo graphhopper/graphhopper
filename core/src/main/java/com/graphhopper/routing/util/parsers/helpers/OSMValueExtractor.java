75007f221076a2593b261fc4258138a434417f5a
Peter <graphhopper@gmx.de>
2019-09-26 13:56:22 +0200

    improve unit parsing for weight and distance

--- core/src/main/java/com/graphhopper/routing/util/parsers/helpers/OSMValueExtractor.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/helpers/OSMValueExtractor.java
@@ -1,19 +1,18 @@
 package com.graphhopper.routing.util.parsers.helpers;
 
-import static com.graphhopper.util.Helper.isEmpty;
-import static com.graphhopper.util.Helper.toLowerCase;
-
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.profiles.DecimalEncodedValue;
 import com.graphhopper.storage.IntsRef;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static com.graphhopper.util.Helper.isEmpty;
+import static com.graphhopper.util.Helper.toLowerCase;
 
 public class OSMValueExtractor {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(OSMValueExtractor.class);
 
     private OSMValueExtractor() {
@@ -36,18 +35,22 @@ public class OSMValueExtractor {
     }
 
     public static double stringToTons(String value) {
-        value = toLowerCase(value).replaceAll(" ", "").replaceAll("(tons|ton)", "t");
+        value = value.toLowerCase().replaceAll("(tons|ton)", "t");
         value = value.replace("mgw", "").trim();
+        if (isInvalid(value))
+            throw new NumberFormatException("Cannot parse value for 'tons': " + value);
+
         double factor = 1;
-        if (value.equals("default") || value.equals("none")) {
-            return -1;
-        } else if (value.endsWith("t")) {
+        if (value.endsWith("t")) {
             value = value.substring(0, value.length() - 1);
         } else if (value.endsWith("lbs")) {
             value = value.substring(0, value.length() - 3);
             factor = 0.00045359237;
+        } else if (value.endsWith("kg")) {
+            value = value.substring(0, value.length() - 2);
+            factor = 0.001;
         }
-    
+
         return Double.parseDouble(value) * factor;
     }
 
@@ -68,17 +71,17 @@ public class OSMValueExtractor {
 
     public static double stringToMeter(String value) {
         value = toLowerCase(value).replaceAll(" ", "").replaceAll("(meters|meter|mtrs|mtr|mt|m\\.)", "m");
+        if (isInvalid(value))
+            throw new NumberFormatException("Cannot parse value for 'meter': " + value);
+
         double factor = 1;
         double offset = 0;
-        value = value.replaceAll("(\\\"|\'\')", "in").replaceAll("(\'|feet)", "ft");
+        value = value.replaceAll("(\"|\'\')", "in").replaceAll("(\'|feet)", "ft");
         if (value.startsWith("~") || value.contains("approx")) {
-            value = value.replaceAll("(\\~|approx)", "").trim();
+            value = value.replaceAll("(~|approx)", "").trim();
             factor = 0.8;
         }
-    
-        if (value.equals("default") || value.equals("none") || value.equals("unsigned"))
-            return -1;
-    
+
         if (value.endsWith("in")) {
             int startIndex = value.indexOf("ft");
             String inchValue;
@@ -87,23 +90,37 @@ public class OSMValueExtractor {
             } else {
                 startIndex += 2;
             }
-    
+
             inchValue = value.substring(startIndex, value.length() - 2);
             value = value.substring(0, startIndex);
             offset = Double.parseDouble(inchValue) * 0.0254;
         }
-    
+
         if (value.endsWith("ft")) {
             value = value.substring(0, value.length() - 2);
             factor *= 0.3048;
+        } else if (value.endsWith("cm")) {
+            value = value.substring(0, value.length() - 2);
+            factor *= 0.01;
         } else if (value.endsWith("m")) {
             value = value.substring(0, value.length() - 1);
         }
-    
+
         if (value.isEmpty()) {
             return offset;
         } else {
             return Double.parseDouble(value) * factor + offset;
         }
+    }
+
+    static boolean isInvalid(String value) {
+        value = value.toLowerCase();
+        return value.isEmpty() || value.startsWith("default") || value.equals("none") || value.equals("unknown")
+                || value.contains("unrestricted") || value.startsWith("〜")
+                || value.contains("narrow") || value.equals("unsigned") || value.equals("fixme") || value.equals("small")
+                || value.contains(";") || value.contains(":") || value.contains("(")
+                || value.contains(">") || value.contains("<") || value.contains("-")
+                // only support '.' and no German decimals
+                || value.contains(",");
     }
 }
