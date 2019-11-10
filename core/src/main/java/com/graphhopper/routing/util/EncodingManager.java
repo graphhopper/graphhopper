a5e597736ceb6031e320ca6138da13b2410e85db
easbar <easbar.mail@posteo.net>
2019-11-10 08:19:20 +0100

    Fix merge issue

--- core/src/main/java/com/graphhopper/routing/util/EncodingManager.java
+++ core/src/main/java/com/graphhopper/routing/util/EncodingManager.java
@@ -521,10 +521,6 @@ public class EncodingManager implements EncodedValueLookup {
         }
     }
 
-    public IntsRef handleWayTags(ReaderWay way, AcceptWay acceptWay) {
-        return handleWayTags(way, acceptWay, 0);
-    }
-
     /**
      * Processes way properties of different kind to determine speed and direction. Properties are
      * directly encoded in 8 bytes.
