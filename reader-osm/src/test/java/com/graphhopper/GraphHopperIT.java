fb3e80360ecf9aedde1b1764aa904b3a49a08483
easbar <easbar.mail@posteo.net>
2019-11-25 00:12:39 +0100

    Fix test

--- reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
+++ reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
@@ -1266,7 +1266,7 @@ public class GraphHopperIT {
         req.setVehicle("foot");
         GHResponse rsp = hopper.route(req);
         assertTrue("using edge-based for encoder without turncost support should be an error, but got:\n" + rsp.getErrors(),
-                rsp.getErrors().toString().contains("You need a turn cost storage to make use of edge_based=true, e.g. use car|turn_costs=true"));
+                rsp.getErrors().toString().contains("You need to set up a turn cost storage to make use of edge_based=true, e.g. use car|turn_costs=true"));
     }
 
     @Test
