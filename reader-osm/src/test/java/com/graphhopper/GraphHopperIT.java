graphhopper/graphhopper

commit daa5b9c0aa3dd0baa963ece9f07c5db207bd61cc

Remove FlagEncoder.getAnnotation (#1811)
* initial version of required encoded values

* remove FlagEncoder.getAnnotation and create GetOffBike EncodedValue for replacement

* fix tests

* clean up

core/src/main/java/com/graphhopper/routing/InstructionsFromEdges.java
@@ -17,7 +17,7 @@
  */
 package com.graphhopper.routing;
 
-import com.graphhopper.routing.profiles.BooleanEncodedValue;
+import com.graphhopper.routing.profiles.*;
 import com.graphhopper.routing.util.DefaultEdgeFilter;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.weighting.Weighting;
@@ -26,6 +26,8 @@ import com.graphhopper.storage.IntsRef;
 import com.graphhopper.storage.NodeAccess;
 import com.graphhopper.util.*;
 import com.graphhopper.util.shapes.GHPoint;
+
+import static com.graphhopper.routing.util.EncodingManager.getKey;
 
 /**
  * This class calculates instructions from the edges in a Path.
@@ -46,6 +48,11 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
     private final EdgeExplorer crossingExplorer;
     private final BooleanEncodedValue roundaboutEnc;
     private final BooleanEncodedValue accessEnc;
+    private final BooleanEncodedValue getOffBikeEnc;
+    private final EnumEncodedValue<RouteNetwork> bikeRouteEnc;
+    private final EnumEncodedValue<RoadClass> roadClassEnc;
+    private final EnumEncodedValue<RoadEnvironment> roadEnvEnc;
+
     /*
      * We need three points to make directions
      *
@@ -81,33 +88,40 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
 
     private final int MAX_U_TURN_DISTANCE = 35;
 
-    public InstructionsFromEdges(Graph graph, Weighting weighting,
-                                 BooleanEncodedValue roundaboutEnc,
+    public InstructionsFromEdges(Graph graph, Weighting weighting, EncodedValueLookup evLookup,
                                  Translation tr, InstructionList ways) {
-        this.weighting = weighting;
         this.encoder = weighting.getFlagEncoder();
-        this.accessEnc = this.encoder.getAccessEnc();
-        this.roundaboutEnc = roundaboutEnc;
+        this.weighting = weighting;
+        this.accessEnc = evLookup.getBooleanEncodedValue(getKey(encoder.toString(), "access"));
+        this.roundaboutEnc = evLookup.getBooleanEncodedValue(Roundabout.KEY);
+
+        // both EncodedValues are optional and only used when bike encoders are added
+        String key = getKey("bike", RouteNetwork.EV_SUFFIX);
+        this.bikeRouteEnc = evLookup.hasEncodedValue(key) ? evLookup.getEnumEncodedValue(key, RouteNetwork.class) : null;
+        this.getOffBikeEnc = evLookup.hasEncodedValue(GetOffBike.KEY) ? evLookup.getBooleanEncodedValue(GetOffBike.KEY) : null;
+
+        this.roadClassEnc = evLookup.getEnumEncodedValue(RoadClass.KEY, RoadClass.class);
+        this.roadEnvEnc = evLookup.getEnumEncodedValue(RoadEnvironment.KEY, RoadEnvironment.class);
         this.nodeAccess = graph.getNodeAccess();
         this.tr = tr;
         this.ways = ways;
         prevNode = -1;
         prevInRoundabout = false;
         prevName = null;
-        outEdgeExplorer = graph.createEdgeExplorer(DefaultEdgeFilter.outEdges(this.encoder));
-        crossingExplorer = graph.createEdgeExplorer(DefaultEdgeFilter.allEdges(this.encoder));
+        outEdgeExplorer = graph.createEdgeExplorer(DefaultEdgeFilter.outEdges(encoder));
+        crossingExplorer = graph.createEdgeExplorer(DefaultEdgeFilter.allEdges(encoder));
     }
 
     /**
      * @return the list of instructions for this path.
      */
-    public static InstructionList calcInstructions(Path path, Graph graph, Weighting weighting, BooleanEncodedValue roundaboutEnc, final Translation tr) {
+    public static InstructionList calcInstructions(Path path, Graph graph, Weighting weighting, EncodedValueLookup evLookup, final Translation tr) {
         final InstructionList ways = new InstructionList(tr);
         if (path.isFound()) {
             if (path.getSize() == 0) {
                 ways.add(new FinishInstruction(graph.getNodeAccess(), path.getEndNode()));
             } else {
-                path.forEveryEdge(new InstructionsFromEdges(graph, weighting, roundaboutEnc, tr, ways));
+                path.forEveryEdge(new InstructionsFromEdges(graph, weighting, evLookup, tr, ways));
             }
         }
         return ways;
@@ -143,7 +157,19 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
         }
 
         String name = edge.getName();
-        InstructionAnnotation annotation = encoder.getAnnotation(flags, tr);
+        InstructionAnnotation annotation = InstructionAnnotation.EMPTY;
+        if (getOffBikeEnc != null) {
+            // only for bikes do
+            if (edge.get(roadClassEnc) == RoadClass.CYCLEWAY
+                    || bikeRouteEnc != null && edge.get(bikeRouteEnc) != RouteNetwork.OTHER) {
+                // for backward compatibility
+                annotation = new InstructionAnnotation(0, tr.tr("cycleway"));
+            } else if (edge.get(getOffBikeEnc)) {
+                annotation = new InstructionAnnotation(1, tr.tr("off_bike"));
+            }
+        } else if (edge.get(roadEnvEnc) == RoadEnvironment.FORD) {
+            annotation = new InstructionAnnotation(1, tr.tr("way_contains_ford"));
+        }
 
         if ((prevName == null) && (!isRoundabout)) // very first instruction (if not in Roundabout)
         {

reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
@@ -820,10 +820,10 @@ public class GraphHopperIT {
 
         PathWrapper arsp = rsp.getBest();
         assertEquals(6932.2, arsp.getDistance(), .1);
-        assertEquals(106, arsp.getPoints().getSize());
+        assertEquals(105, arsp.getPoints().getSize());
 
         InstructionList il = arsp.getInstructions();
-        assertEquals(22, il.size());
+        assertEquals(21, il.size());
 
         assertEquals("continue onto Obere Landstraße", il.get(0).getTurnDescription(tr));
         assertEquals("get off the bike", il.get(0).getAnnotation().getMessage());
@@ -840,8 +840,8 @@ public class GraphHopperIT {
         assertEquals("keep left onto Austraße", il.get(11).getTurnDescription(tr));
         assertEquals("keep left onto Rechte Kremszeile", il.get(12).getTurnDescription(tr));
         //..
-        assertEquals("turn right onto Treppelweg", il.get(18).getTurnDescription(tr));
-        assertEquals("cycleway", il.get(18).getAnnotation().getMessage());
+        assertEquals("turn right onto Treppelweg", il.get(17).getTurnDescription(tr));
+        assertEquals("cycleway", il.get(17).getAnnotation().getMessage());
     }
 
     @Test

core/src/main/java/com/graphhopper/reader/ReaderElement.java
@@ -136,7 +136,7 @@ public abstract class ReaderElement {
     /**
      * Check that a given tag has one of the specified values.
      */
-    public final boolean hasTag(String key, Set<String> values) {
+    public final boolean hasTag(String key, Collection<String> values) {
         return values.contains(getTag(key, ""));
     }
 
@@ -144,7 +144,7 @@ public abstract class ReaderElement {
      * Check a number of tags in the given order for the any of the given values. Used to parse
      * hierarchical access restrictions
      */
-    public boolean hasTag(List<String> keyList, Set<String> values) {
+    public boolean hasTag(List<String> keyList, Collection<String> values) {
         for (String key : keyList) {
             if (values.contains(getTag(key, "")))
                 return true;

core/src/main/java/com/graphhopper/routing/InstructionsOutgoingEdges.java
@@ -18,8 +18,8 @@
 package com.graphhopper.routing;
 
 import com.graphhopper.routing.profiles.BooleanEncodedValue;
-import com.graphhopper.routing.profiles.MaxSpeed;
 import com.graphhopper.routing.profiles.DecimalEncodedValue;
+import com.graphhopper.routing.profiles.MaxSpeed;
 import com.graphhopper.routing.util.DataFlagEncoder;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.storage.IntsRef;
@@ -57,19 +57,14 @@ import java.util.List;
  */
 class InstructionsOutgoingEdges {
 
-    final EdgeIteratorState prevEdge;
-    final EdgeIteratorState currentEdge;
-
+    private final EdgeIteratorState prevEdge;
+    private final EdgeIteratorState currentEdge;
     // Outgoing edges that we would be allowed to turn on
-    final List<EdgeIteratorState> allowedOutgoingEdges;
-
+    private final List<EdgeIteratorState> allowedOutgoingEdges;
     // All outgoing edges, including oneways in the wrong direction
-    final List<EdgeIteratorState> allOutgoingEdges;
-
-    final FlagEncoder encoder;
-    final BooleanEncodedValue accessEnc;
-    final DecimalEncodedValue speedEnc;
-    final NodeAccess nodeAccess;
+    private final List<EdgeIteratorState> allOutgoingEdges;
+    private final DecimalEncodedValue speedEnc;
+    private final NodeAccess nodeAccess;
 
     public InstructionsOutgoingEdges(EdgeIteratorState prevEdge,
                                      EdgeIteratorState currentEdge,
@@ -81,8 +76,7 @@ class InstructionsOutgoingEdges {
                                      int adjNode) {
         this.prevEdge = prevEdge;
         this.currentEdge = currentEdge;
-        this.encoder = encoder;
-        this.accessEnc = encoder.getAccessEnc();
+        BooleanEncodedValue accessEnc = encoder.getAccessEnc();
         this.speedEnc = (encoder instanceof DataFlagEncoder) ? encoder.getDecimalEncodedValue(MaxSpeed.KEY) : encoder.getAverageSpeedEnc();
         this.nodeAccess = nodeAccess;
 

core/src/main/java/com/graphhopper/routing/profiles/DefaultEncodedValueFactory.java
@@ -34,6 +34,8 @@ public class DefaultEncodedValueFactory implements EncodedValueFactory {
         // creating the Country EV is done while SpatialRuleIndex is created and not here
         if (Roundabout.KEY.equals(name)) {
             enc = Roundabout.create();
+        } else if (GetOffBike.KEY.equals(name)) {
+            enc = GetOffBike.create();
         } else if (RoadClass.KEY.equals(name)) {
             enc = new EnumEncodedValue<>(RoadClass.KEY, RoadClass.class);
         } else if (RoadClassLink.KEY.equals(name)) {

core/src/main/java/com/graphhopper/routing/profiles/GetOffBike.java
@@ -1,0 +1,9 @@
+package com.graphhopper.routing.profiles;
+
+public class GetOffBike {
+    public static final String KEY = "get_off_bike";
+
+    public static BooleanEncodedValue create() {
+        return new SimpleBooleanEncodedValue(KEY, false);
+    }
+}

core/src/main/java/com/graphhopper/routing/util/AbstractFlagEncoder.java
@@ -228,11 +228,6 @@ public abstract class AbstractFlagEncoder implements FlagEncoder {
         return 0;
     }
 
-    @Override
-    public InstructionAnnotation getAnnotation(IntsRef edgeFlags, Translation tr) {
-        return InstructionAnnotation.EMPTY;
-    }
-
     /**
      * Sets default flags with specified access.
      */

core/src/main/java/com/graphhopper/routing/util/BikeCommonFlagEncoder.java
@@ -40,10 +40,7 @@ import static com.graphhopper.routing.util.PriorityCode.*;
  * @author ratrun
  */
 abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
-    /**
-     * Reports whether this edge is unpaved.
-     */
-    public static final int K_UNPAVED = 100;
+
     protected static final int PUSHING_SECTION_SPEED = 4;
     // Pushing section highways are parts where you need to get off your bike and push it (German: Schiebestrecke)
     protected final HashSet<String> pushingSectionsHighways = new HashSet<>();
@@ -57,8 +54,6 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
     private final Map<String, Integer> highwaySpeeds = new HashMap<>();
     protected boolean speedTwoDirections;
     DecimalEncodedValue priorityEnc;
-    private BooleanEncodedValue unpavedEnc;
-    private IntEncodedValue wayTypeEnc;
     // Car speed limit which switches the preference from UNCHANGED to AVOID_IF_POSSIBLE
     private int avoidSpeedLimit;
     EnumEncodedValue<RouteNetwork> bikeRouteEnc;
@@ -212,8 +207,6 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         // first two bits are reserved for route handling in superclass
         super.createEncodedValues(registerNewEncodedValue, prefix, index);
         registerNewEncodedValue.add(avgSpeedEnc = new UnsignedDecimalEncodedValue(getKey(prefix, "average_speed"), speedBits, speedFactor, speedTwoDirections));
-        registerNewEncodedValue.add(unpavedEnc = new SimpleBooleanEncodedValue(getKey(prefix, "paved"), false));
-        registerNewEncodedValue.add(wayTypeEnc = new UnsignedIntEncodedValue(getKey(prefix, "waytype"), 2, false));
         registerNewEncodedValue.add(priorityEnc = new UnsignedDecimalEncodedValue(getKey(prefix, "priority"), 3, PriorityCode.getFactor(1), false));
 
         bikeRouteEnc = getEnumEncodedValue(getKey("bike", EV_SUFFIX), RouteNetwork.class);
@@ -324,7 +317,6 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         if (!access.isFerry()) {
             wayTypeSpeed = applyMaxSpeed(way, wayTypeSpeed);
             handleSpeed(edgeFlags, way, wayTypeSpeed);
-            handleBikeRelated(edgeFlags, way, priorityFromRelation != null && priorityFromRelation > UNCHANGED.getValue());
         } else {
             double ferrySpeed = getFerrySpeed(way);
             handleSpeed(edgeFlags, way, ferrySpeed);
@@ -396,48 +388,6 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
                 speed = PUSHING_SECTION_SPEED * 2;
         }
         return speed;
-    }
-
-    @Override
-    public InstructionAnnotation getAnnotation(IntsRef edgeFlags, Translation tr) {
-        int paveType = 0; // paved
-        if (unpavedEnc.getBool(false, edgeFlags))
-            paveType = 1; // unpaved
-
-        int wayType = wayTypeEnc.getInt(false, edgeFlags);
-        String wayName = getWayName(paveType, wayType, tr);
-        return new InstructionAnnotation(0, wayName);
-    }
-
-    String getWayName(int pavementType, int wayType, Translation tr) {
-        String pavementName = "";
-        if (pavementType == 1)
-            pavementName = tr.tr("unpaved");
-
-        String wayTypeName = "";
-        switch (wayType) {
-            case 0:
-                wayTypeName = "";
-                break;
-            case 1:
-                wayTypeName = tr.tr("off_bike");
-                break;
-            case 2:
-                wayTypeName = tr.tr("cycleway");
-                break;
-            case 3:
-                wayTypeName = tr.tr("small_way");
-                break;
-        }
-
-        if (pavementName.isEmpty()) {
-            if (wayType == 0 || wayType == 3)
-                return "";
-            return wayTypeName;
-        } else if (wayTypeName.isEmpty())
-            return pavementName;
-        else
-            return wayTypeName + ", " + pavementName;
     }
 
     /**
@@ -562,46 +512,6 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         }
     }
 
-    /**
-     * Handle surface and wayType encoding
-     */
-    void handleBikeRelated(IntsRef edgeFlags, ReaderWay way, boolean partOfCycleRelation) {
-        String surfaceTag = way.getTag("surface");
-        String highway = way.getTag("highway");
-        String trackType = way.getTag("tracktype");
-
-        // Populate unpavedBit
-        if ("track".equals(highway) && !"grade1".equals(trackType)
-                || "path".equals(highway) && surfaceTag == null
-                || unpavedSurfaceTags.contains(surfaceTag)) {
-            unpavedEnc.setBool(false, edgeFlags, true);
-        }
-
-        WayType wayType;
-        if (roadValues.contains(highway))
-            wayType = WayType.ROAD;
-        else
-            wayType = WayType.OTHER_SMALL_WAY;
-
-        boolean isPushingSection = isPushingSection(way);
-        if (isPushingSection && !partOfCycleRelation || "steps".equals(highway))
-            wayType = WayType.PUSHING_SECTION;
-
-        if (way.hasTag("bicycle", intendedValues)) {
-            if (isPushingSection && !way.hasTag("bicycle", "designated"))
-                wayType = WayType.OTHER_SMALL_WAY;
-            else if (wayType == WayType.OTHER_SMALL_WAY || wayType == WayType.PUSHING_SECTION)
-                wayType = WayType.CYCLEWAY;
-        } else if ("cycleway".equals(highway))
-            wayType = WayType.CYCLEWAY;
-
-        wayTypeEnc.setInt(false, edgeFlags, wayType.getValue());
-    }
-
-    boolean isPushingSection(ReaderWay way) {
-        return way.hasTag("highway", pushingSectionsHighways) || way.hasTag("railway", "platform") || way.hasTag("bicycle", "dismount");
-    }
-
     protected void handleSpeed(IntsRef edgeFlags, ReaderWay way, double speed) {
         avgSpeedEnc.setDecimal(false, edgeFlags, speed);
 
@@ -633,11 +543,11 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         }
     }
 
-    protected void setHighwaySpeed(String highway, int speed) {
+    void setHighwaySpeed(String highway, int speed) {
         highwaySpeeds.put(highway, speed);
     }
 
-    protected int getHighwaySpeed(String key) {
+    int getHighwaySpeed(String key) {
         return highwaySpeeds.get(key);
     }
 
@@ -661,28 +571,11 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
         return PriorityWeighting.class.isAssignableFrom(feature);
     }
 
-    public void setAvoidSpeedLimit(int limit) {
+    void setAvoidSpeedLimit(int limit) {
         avoidSpeedLimit = limit;
     }
 
-    protected void setSpecificClassBicycle(String subkey) {
+    void setSpecificClassBicycle(String subkey) {
         classBicycleKey = "class:bicycle:" + subkey;
-    }
-
-    private enum WayType {
-        ROAD(0),
-        PUSHING_SECTION(1),
-        CYCLEWAY(2),
-        OTHER_SMALL_WAY(3);
-
-        private final int value;
-
-        private WayType(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
     }
 }

commit 1d1ca79f624c8c25750575ea69b1049b1522b87a

fix bug introduced in #1811

core/src/main/java/com/graphhopper/routing/InstructionsFromEdges.java
@@ -18,6 +18,7 @@
 package com.graphhopper.routing;
 
 import com.graphhopper.routing.profiles.*;
+import com.graphhopper.routing.util.BikeCommonFlagEncoder;
 import com.graphhopper.routing.util.DefaultEdgeFilter;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.weighting.Weighting;
@@ -97,10 +98,11 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
         this.accessEnc = evLookup.getBooleanEncodedValue(getKey(encoder.toString(), "access"));
         this.roundaboutEnc = evLookup.getBooleanEncodedValue(Roundabout.KEY);
 
-        // both EncodedValues are optional and only used when bike encoders are added
+        // both EncodedValues are optional; And return annotation only when instructions for bike encoder is requested
         String key = getKey("bike", RouteNetwork.EV_SUFFIX);
         this.bikeRouteEnc = evLookup.hasEncodedValue(key) ? evLookup.getEnumEncodedValue(key, RouteNetwork.class) : null;
-        this.getOffBikeEnc = evLookup.hasEncodedValue(GetOffBike.KEY) ? evLookup.getBooleanEncodedValue(GetOffBike.KEY) : null;
+        this.getOffBikeEnc = encoder instanceof BikeCommonFlagEncoder && evLookup.hasEncodedValue(GetOffBike.KEY)
+                ? evLookup.getBooleanEncodedValue(GetOffBike.KEY) : null;
         this.tollEnc = evLookup.hasEncodedValue(Toll.KEY) ? evLookup.getEnumEncodedValue(Toll.KEY, Toll.class) : null;
 
         this.roadClassEnc = evLookup.getEnumEncodedValue(RoadClass.KEY, RoadClass.class);
@@ -161,15 +163,14 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
 
         String name = edge.getName();
         String info = "";
-        int importance = 0;
+        int importance = 1;
         if (getOffBikeEnc != null) {
-            // only for bikes do
             if (edge.get(roadClassEnc) == RoadClass.CYCLEWAY
                     || bikeRouteEnc != null && edge.get(bikeRouteEnc) != RouteNetwork.OTHER) {
                 // for backward compatibility
+                importance = 0;
                 info = tr.tr("cycleway");
             } else if (edge.get(getOffBikeEnc)) {
-                importance = 1;
                 info = tr.tr("off_bike");
             }
         }
@@ -179,20 +180,16 @@ public class InstructionsFromEdges implements Path.EdgeVisitor {
             importance = 2; // could be dangerous
             info = tr.tr("way_contains_ford");
         } else if (re == RoadEnvironment.FERRY) {
-            importance = 1;
             info = tr.tr("way_contains_ferry");
         }
 
+        // if private access is allowed we need a warning
         RoadAccess ra = edge.get(roadAccessEnc);
-        if (ra == RoadAccess.PRIVATE) {
-            importance = 1; // if private access is allowed we need a warning
+        if (ra == RoadAccess.PRIVATE)
             info = info.isEmpty() ? tr.tr("way_contains_private") : info + ", " + tr.tr("way_contains_private");
-        }
 
-        if (tollEnc != null && edge.get(tollEnc) != Toll.NO) {
-            importance = 1;
+        if (tollEnc != null && edge.get(tollEnc) != Toll.NO)
             info = info.isEmpty() ? tr.tr("way_contains_toll") : info + ", " + tr.tr("way_contains_toll");
-        }
 
         InstructionAnnotation annotation = info.isEmpty()
                 ? InstructionAnnotation.EMPTY

reader-osm/src/test/java/com/graphhopper/GraphHopperIT.java
@@ -803,7 +803,7 @@ public class GraphHopperIT {
     public void testKremsCyclewayInstructionsWithWayTypeInfo() {
         String tmpOsmFile = DIR + "/krems.osm.gz";
         String tmpVehicle = "bike";
-        String tmpImportVehicles = "car,bike";
+        String tmpImportVehicles = "foot,bike";
         String tmpWeightCalcStr = "fastest";
 
         GraphHopper tmpHopper = new GraphHopperOSM().
@@ -816,14 +816,14 @@ public class GraphHopperIT {
 
         Translation tr = tmpHopper.getTranslationMap().getWithFallBack(Locale.US);
         GHResponse rsp = tmpHopper.route(new GHRequest(48.410987, 15.599492, 48.383419, 15.659294).
-                setAlgorithm(ASTAR).setVehicle(tmpVehicle).setWeighting(tmpWeightCalcStr));
-
+                setVehicle(tmpVehicle).setWeighting(tmpWeightCalcStr));
+        assertFalse(rsp.hasErrors());
         PathWrapper arsp = rsp.getBest();
         assertEquals(6932.2, arsp.getDistance(), .1);
-        assertEquals(105, arsp.getPoints().getSize());
+        assertEquals(106, arsp.getPoints().getSize());
 
         InstructionList il = arsp.getInstructions();
-        assertEquals(21, il.size());
+        assertEquals(22, il.size());
 
         assertEquals("continue onto Obere Landstraße", il.get(0).getTurnDescription(tr));
         assertEquals("get off the bike", il.get(0).getAnnotation().getMessage());
@@ -840,8 +840,16 @@ public class GraphHopperIT {
         assertEquals("keep left onto Austraße", il.get(11).getTurnDescription(tr));
         assertEquals("keep left onto Rechte Kremszeile", il.get(12).getTurnDescription(tr));
         //..
-        assertEquals("turn right onto Treppelweg", il.get(17).getTurnDescription(tr));
-        assertEquals("cycleway", il.get(17).getAnnotation().getMessage());
+        assertEquals("turn right onto Treppelweg", il.get(18).getTurnDescription(tr));
+        assertEquals("cycleway", il.get(18).getAnnotation().getMessage());
+
+        // do not return 'get off bike' for foot
+        rsp = tmpHopper.route(new GHRequest(48.410987, 15.599492, 48.411172, 15.600371).
+                setAlgorithm(ASTAR).setVehicle("foot").setWeighting(tmpWeightCalcStr));
+        assertFalse(rsp.hasErrors());
+        il = rsp.getBest().getInstructions();
+        assertEquals("continue onto Obere Landstraße", il.get(0).getTurnDescription(tr));
+        assertEquals("", il.get(0).getAnnotation().getMessage());
     }
 
     @Test