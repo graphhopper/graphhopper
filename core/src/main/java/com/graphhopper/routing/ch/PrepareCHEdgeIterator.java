3b3a1b0237d04f9d82e3202b3096d23170aed910
easbar <easbar.mail@posteo.net>
2019-12-05 18:46:35 +0100

    Remove accessEnc from PrepareCHEdgeIterator

--- core/src/main/java/com/graphhopper/routing/ch/PrepareCHEdgeIterator.java
+++ core/src/main/java/com/graphhopper/routing/ch/PrepareCHEdgeIterator.java
@@ -18,7 +18,6 @@
 
 package com.graphhopper.routing.ch;
 
-import com.graphhopper.routing.profiles.BooleanEncodedValue;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.util.CHEdgeExplorer;
 import com.graphhopper.util.CHEdgeIterator;
@@ -27,13 +26,11 @@ import com.graphhopper.util.EdgeIterator;
 public class PrepareCHEdgeIterator implements PrepareCHEdgeExplorer {
     private final CHEdgeExplorer edgeExplorer;
     private final Weighting weighting;
-    private final BooleanEncodedValue accessEnc;
     private CHEdgeIterator chIterator;
 
     public PrepareCHEdgeIterator(CHEdgeExplorer edgeExplorer, Weighting weighting) {
         this.edgeExplorer = edgeExplorer;
         this.weighting = weighting;
-        this.accessEnc = weighting.getFlagEncoder().getAccessEnc();
     }
 
     @Override
