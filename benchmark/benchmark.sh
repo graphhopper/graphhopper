dcf43891f6f2c8a3938865d87318025420d420fa
easbar <easbar.mail@posteo.net>
2019-12-28 15:52:37 +0100

    Setup periodic benchmarks
    If the benchmark build runs with special flag set (PERIODIC_BUILD) the measurement time instead of the git commit
    time is used as reference time for the resulting benchmark result file

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # usage:
-# benchmark/benchmark.sh <data_dir> <results_dir> <small_osm_map_path> <big_osm_map_path>
+# benchmark/benchmark.sh <data_dir> <results_dir> <small_osm_map_path> <big_osm_map_path> <use_measurement_time_as_ref_time>
 #
 # where:
 # <data_dir> = base directory used to store results
 # <results_dir> = name of directory where results of this run are stored (inside <data_dir>)
 # <small_osm_map_path> = path to osm map the measurement is run on for slow measurements
 # <big_osm_map_path> = path to osm map the measurement is run on for fast measurements
+# <use_measurement_time_as_ref_time> = true/false, false by default, meaning the git commit time will be used as reference
 
 # make this script exit if a command fails, a variable is missing etc.
 set -euo pipefail
@@ -15,6 +16,7 @@ defaultDataDir=measurements/
 defaultSingleResultsDir=measurements/results/$(date '+%d-%m-%Y-%s%N')/
 defaultSmallMap=core/files/andorra.osm.pbf
 defaultBigMap=core/files/andorra.osm.pbf
+defaultUseMeasurementTimeAsRefTime=false
 
 # this is the directory where we read/write data from/to
 DATA_DIR=${1:-$defaultDataDir}
@@ -23,6 +25,7 @@ TMP_DIR=${DATA_DIR}tmp/
 SINGLE_RESULTS_DIR=${2:-$defaultSingleResultsDir}
 SMALL_OSM_MAP=${3:-$defaultSmallMap}
 BIG_OSM_MAP=${4:-$defaultBigMap}
+USE_MEASUREMENT_TIME_AS_REF_TIME=${5:-$defaultUseMeasurementTimeAsRefTime}
 
 # create directories
 mkdir -p ${DATA_DIR}
@@ -48,7 +51,8 @@ graph.location=${TMP_DIR}measurement-small-gh \
 prepare.min_network_size=10000 \
 prepare.min_oneway_network_size=10000 \
 measurement.json=true \
-measurement.count=5000
+measurement.count=5000 \
+measurement.useMeasurementTimeAsRefTime=${USE_MEASUREMENT_TIME_AS_REF_TIME}
 
 # 2 - big map: node-based CH + landmarks
 java -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar com.graphhopper.tools.Measurement \
@@ -67,4 +71,5 @@ graph.location=${TMP_DIR}measurement-big-gh \
 prepare.min_network_size=10000 \
 prepare.min_oneway_network_size=10000 \
 measurement.json=true \
-measurement.count=5000
+measurement.count=5000 \
+measurement.useMeasurementTimeAsRefTime=${USE_MEASUREMENT_TIME_AS_REF_TIME}
--- tools/src/main/java/com/graphhopper/tools/Measurement.java
+++ tools/src/main/java/com/graphhopper/tools/Measurement.java
@@ -234,7 +234,7 @@ public class Measurement {
                 writeSummary(summaryLocation, propLocation);
             }
             if (useJson) {
-                storeJson(propLocation);
+                storeJson(propLocation, args.getBool("measurement.useMeasurementTimeAsRefTime", false));
             } else {
                 storeProperties(propLocation);
             }
@@ -595,10 +595,10 @@ public class Measurement {
         properties.put(key, val);
     }
 
-    private void storeJson(String jsonLocation) {
+    private void storeJson(String jsonLocation, boolean useMeasurementTimeAsRefTime) {
         logger.info("storing measurement json in " + jsonLocation);
-        Map<String, Object> result = new HashMap<>();
         Map<String, String> gitInfoMap = new HashMap<>();
+        // add git info if available
         if (Constants.GIT_INFO != null) {
             properties.remove("gh.gitinfo");
             gitInfoMap.put("commitHash", Constants.GIT_INFO.getCommitHash());
@@ -607,6 +607,17 @@ public class Measurement {
             gitInfoMap.put("branch", Constants.GIT_INFO.getBranch());
             gitInfoMap.put("dirty", String.valueOf(Constants.GIT_INFO.isDirty()));
         }
+        Map<String, Object> result = new HashMap<>();
+        // add measurement time, use same format as git commit time
+        String measurementTime = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ssZ").format(new Date());
+        result.put("measurementTime", measurementTime);
+        // set ref time, this is either the git commit time or the measurement time
+        if (Constants.GIT_INFO != null && !useMeasurementTimeAsRefTime) {
+            result.put("refTime", Constants.GIT_INFO.getCommitTime());
+        } else {
+            result.put("refTime", measurementTime);
+        }
+        result.put("periodicBuild", useMeasurementTimeAsRefTime);
         result.put("gitinfo", gitInfoMap);
         result.put("metrics", properties);
         try {
