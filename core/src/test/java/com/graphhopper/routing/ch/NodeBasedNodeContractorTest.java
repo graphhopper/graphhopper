b521afbd5da24d6fa64e04be714af454d89baaf8
easbar <easbar.mail@posteo.net>
2019-12-29 21:24:45 +0100

    Rename PreparationWeighting -> CHWeighting

--- core/src/test/java/com/graphhopper/routing/ch/NodeBasedNodeContractorTest.java
+++ core/src/test/java/com/graphhopper/routing/ch/NodeBasedNodeContractorTest.java
@@ -319,7 +319,7 @@ public class NodeBasedNodeContractorTest {
         Dijkstra dikstra = new Dijkstra(graph, weighting, TraversalMode.NODE_BASED);
         Path dijkstraPath = dikstra.calcPath(from, to);
 
-        DijkstraBidirectionCH ch = new DijkstraBidirectionCH(lg, new PreparationWeighting(weighting));
+        DijkstraBidirectionCH ch = new DijkstraBidirectionCH(lg, new CHWeighting(weighting));
         Path chPath = ch.calcPath(from, to);
         assertEquals(dijkstraPath.calcNodes(), chPath.calcNodes());
         assertEquals(dijkstraPath.getDistance(), chPath.getDistance(), 1.e-6);
@@ -368,7 +368,7 @@ public class NodeBasedNodeContractorTest {
         Dijkstra dikstra = new Dijkstra(graph, weighting, TraversalMode.NODE_BASED);
         Path dijkstraPath = dikstra.calcPath(from, to);
 
-        DijkstraBidirectionCH ch = new DijkstraBidirectionCH(lg, new PreparationWeighting(weighting));
+        DijkstraBidirectionCH ch = new DijkstraBidirectionCH(lg, new CHWeighting(weighting));
         Path chPath = ch.calcPath(from, to);
         assertEquals(dijkstraPath.calcNodes(), chPath.calcNodes());
         assertEquals(dijkstraPath.getDistance(), chPath.getDistance(), 1.e-6);
@@ -440,7 +440,7 @@ public class NodeBasedNodeContractorTest {
 
         // without heading
         {
-            DijkstraBidirectionCH ch = new DijkstraBidirectionCH(chQueryGraph, new PreparationWeighting(weighting));
+            DijkstraBidirectionCH ch = new DijkstraBidirectionCH(chQueryGraph, new CHWeighting(weighting));
             Path chPath = ch.calcPath(7, 3);
             assertEquals(IntArrayList.from(7, 2, 3), chPath.calcNodes());
         }
@@ -452,7 +452,7 @@ public class NodeBasedNodeContractorTest {
             assertEquals(IntArrayList.from(7, 0, 1, 6, 5, 2, 3), path.calcNodes());
 
             chQueryGraph.enforceHeading(7, 90, false);
-            DijkstraBidirectionCH ch = new DijkstraBidirectionCH(chQueryGraph, new PreparationWeighting(weighting));
+            DijkstraBidirectionCH ch = new DijkstraBidirectionCH(chQueryGraph, new CHWeighting(weighting));
             Path chPath = ch.calcPath(7, 3);
             // the route takes a u-turn at node 1 and 'skips' the virtual node when going from 0 to 3 (because it takes
             // the shortcut from 1 to 2), which both should not be
