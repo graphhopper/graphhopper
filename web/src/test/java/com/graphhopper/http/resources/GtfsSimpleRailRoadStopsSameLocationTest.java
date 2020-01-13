77486a1513279a18eb31247170af262d947d304b
Mathieu St-Pierre <mathieu.st-pierre@curbside.com>
2020-01-13 08:37:11 -0500

    Fix test method names.

--- web/src/test/java/com/graphhopper/http/resources/GtfsSimpleRailRoadStopsSameLocationTest.java
+++ web/src/test/java/com/graphhopper/http/resources/GtfsSimpleRailRoadStopsSameLocationTest.java
@@ -63,7 +63,7 @@ public class GtfsSimpleRailRoadStopsSameLocationTest {
     }
 
     @Test
-    public void testTwoConnectedTransitOver40kmTripQuery() {
+    public void testTwoConnectedTransitsOver40kmTripQuery() {
         final Response response = app.client().target("http://localhost:8080/route")
                 .queryParam("point","46.436038,-63.639194") //SB1
                 .queryParam("point","46.273173,-63.153361") //SR2
--- web/src/test/java/com/graphhopper/http/resources/GtfsSimpleRailRoadStopsSameLocationWithTransferFileTest.java
+++ web/src/test/java/com/graphhopper/http/resources/GtfsSimpleRailRoadStopsSameLocationWithTransferFileTest.java
@@ -63,7 +63,7 @@ public class GtfsSimpleRailRoadStopsSameLocationWithTransferFileTest {
     }
 
     @Test
-    public void testTwoDisconnectTransitOver40kmTripQuery() {
+    public void testTwoConnectedTransitsOver40kmTripWithTransfersFileQuery() {
         final Response response = app.client().target("http://localhost:8080/route")
                 .queryParam("point","46.436038,-63.639194") //SB1
                 .queryParam("point","46.273173,-63.153361") //SR2
