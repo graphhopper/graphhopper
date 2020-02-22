9a21955415817c541bedb368414da8ff482f95d6
easbar <easbar.mail@posteo.net>
2020-02-22 08:35:36 +0100

    No longer use simple factory for LM, error if non-LM algo is requested

--- core/src/main/java/com/graphhopper/routing/lm/LMRoutingAlgorithmFactory.java
+++ core/src/main/java/com/graphhopper/routing/lm/LMRoutingAlgorithmFactory.java
@@ -18,20 +18,22 @@
 
 package com.graphhopper.routing.lm;
 
+import com.graphhopper.routing.AStar;
 import com.graphhopper.routing.*;
 import com.graphhopper.routing.weighting.Weighting;
 import com.graphhopper.storage.Graph;
 import com.graphhopper.util.Parameters;
 
+import static com.graphhopper.util.Parameters.Algorithms.*;
+import static com.graphhopper.util.Parameters.Algorithms.AltRoute.*;
+
 public class LMRoutingAlgorithmFactory implements RoutingAlgorithmFactory {
-    private final RoutingAlgorithmFactory defaultAlgoFactory;
     private final LandmarkStorage lms;
     private final Weighting prepareWeighting;
     private final int numBaseNodes;
     private int defaultActiveLandmarks;
 
     public LMRoutingAlgorithmFactory(LandmarkStorage lms) {
-        this.defaultAlgoFactory = new RoutingAlgorithmFactorySimple();
         this.lms = lms;
         this.prepareWeighting = lms.getWeighting();
         this.numBaseNodes = lms.getBaseNodes();
@@ -45,42 +47,44 @@ public class LMRoutingAlgorithmFactory implements RoutingAlgorithmFactory {
 
     @Override
     public RoutingAlgorithm createAlgo(Graph g, AlgorithmOptions opts) {
-        RoutingAlgorithm algo = defaultAlgoFactory.createAlgo(g, opts);
-        return getPreparedRoutingAlgorithm(g, algo, opts);
-    }
-
-    private RoutingAlgorithm getPreparedRoutingAlgorithm(Graph qGraph, RoutingAlgorithm algo, AlgorithmOptions opts) {
+        if (!lms.isInitialized())
+            throw new IllegalStateException("Initialize landmark storage before creating algorithms");
         int activeLM = Math.max(1, opts.getHints().getInt(Parameters.Landmark.ACTIVE_COUNT, defaultActiveLandmarks));
-        if (algo instanceof AStar) {
-            if (!lms.isInitialized())
-                throw new IllegalStateException("Initialize landmark storage before creating algorithms");
-
+        final String algoStr = opts.getAlgorithm();
+        final Weighting weighting = g.wrapWeighting(opts.getWeighting());
+        if (ASTAR.equalsIgnoreCase(algoStr)) {
             double epsilon = opts.getHints().getDouble(Parameters.Algorithms.AStar.EPSILON, 1);
-            AStar astar = (AStar) algo;
-            astar.setApproximation(new LMApproximator(qGraph, prepareWeighting, numBaseNodes, lms, activeLM, lms.getFactor(), false).
-                    setEpsilon(epsilon));
+            AStar algo = new AStar(g, weighting, opts.getTraversalMode());
+            algo.setApproximation(getApproximator(g, activeLM, epsilon));
+            algo.setMaxVisitedNodes(opts.getMaxVisitedNodes());
             return algo;
-        } else if (algo instanceof AStarBidirection) {
-            if (!lms.isInitialized())
-                throw new IllegalStateException("Initialize landmark storage before creating algorithms");
-
+        } else if (ASTAR_BI.equalsIgnoreCase(algoStr)) {
             double epsilon = opts.getHints().getDouble(Parameters.Algorithms.AStarBi.EPSILON, 1);
-            AStarBidirection astarbi = (AStarBidirection) algo;
-            astarbi.setApproximation(new LMApproximator(qGraph, prepareWeighting, numBaseNodes, lms, activeLM, lms.getFactor(), false).
-                    setEpsilon(epsilon));
+            AStarBidirection algo = new AStarBidirection(g, weighting, opts.getTraversalMode());
+            algo.setApproximation(getApproximator(g, activeLM, epsilon));
+            algo.setMaxVisitedNodes(opts.getMaxVisitedNodes());
             return algo;
-        } else if (algo instanceof AlternativeRoute) {
-            if (!lms.isInitialized())
-                throw new IllegalStateException("Initialize landmark storage before creating algorithms");
-
+        } else if (ALT_ROUTE.equalsIgnoreCase(algoStr)) {
             double epsilon = opts.getHints().getDouble(Parameters.Algorithms.AStarBi.EPSILON, 1);
-            AlternativeRoute altRoute = (AlternativeRoute) algo;
-            altRoute.setApproximation(new LMApproximator(qGraph, prepareWeighting, numBaseNodes, lms, activeLM, lms.getFactor(), false).
-                    setEpsilon(epsilon));
+            AlternativeRoute algo = new AlternativeRoute(g, weighting, opts.getTraversalMode());
+            algo.setMaxPaths(opts.getHints().getInt(MAX_PATHS, 2));
+            algo.setMaxWeightFactor(opts.getHints().getDouble(MAX_WEIGHT, 1.4));
+            algo.setMaxShareFactor(opts.getHints().getDouble(MAX_SHARE, 0.6));
+            algo.setMinPlateauFactor(opts.getHints().getDouble("alternative_route.min_plateau_factor", 0.2));
+            algo.setApproximation(getApproximator(g, activeLM, epsilon));
             // landmark algorithm follows good compromise between fast response and exploring 'interesting' paths so we
             // can decrease this exploration factor further (1->dijkstra, 0.8->bidir. A*)
-            altRoute.setMaxExplorationFactor(0.6);
+            algo.setMaxExplorationFactor(0.6);
+            algo.setMaxVisitedNodes(opts.getMaxVisitedNodes());
+            return algo;
+        } else {
+            throw new IllegalArgumentException("Landmarks algorithm only supports algorithm="
+                    + ASTAR + "," + ASTAR_BI + " or " + ALT_ROUTE + ", but got: " + algoStr);
         }
-        return algo;
+    }
+
+    private LMApproximator getApproximator(Graph g, int activeLM, double epsilon) {
+        return new LMApproximator(g, prepareWeighting, numBaseNodes, lms, activeLM, lms.getFactor(), false).
+                setEpsilon(epsilon);
     }
 }
