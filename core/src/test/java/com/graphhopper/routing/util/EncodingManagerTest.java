graphhopper/graphhopper

commit 04d1070c91a04b1ebe072efc82f0cff36ae7f7c0

destination support for car that doesn't modify the speed (#1752)
* avoid speed changes if destination tag, requires road_access usage, fixes #733

* fix comments

core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -41,8 +41,6 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
     // This value determines the maximal possible on roads with bad surfaces
     protected int badSurfaceSpeed;
 
-    // This value determines the speed for roads with access=destination
-    protected int destinationSpeed;
     protected boolean speedTwoDirections;
     /**
      * A map which associates string to speed. Get some impression:
@@ -143,7 +141,6 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
 
         // limit speed on bad surfaces to 30 km/h
         badSurfaceSpeed = 30;
-        destinationSpeed = 5;
         maxPossibleSpeed = 140;
         speedDefault = defaultSpeedMap.get("secondary");
 
@@ -280,14 +277,6 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
                 setSpeed(true, edgeFlags, ferrySpeed);
         }
 
-        for (String restriction : restrictions) {
-            if (way.hasTag(restriction, "destination")) {
-                // This is problematic as Speed != Time
-                setSpeed(false, edgeFlags, destinationSpeed);
-                if (speedTwoDirections)
-                    setSpeed(true, edgeFlags, destinationSpeed);
-            }
-        }
         return edgeFlags;
     }
 

core/src/main/java/com/graphhopper/routing/weighting/FastestWeighting.java
@@ -17,6 +17,8 @@
  */
 package com.graphhopper.routing.weighting;
 
+import com.graphhopper.routing.profiles.EnumEncodedValue;
+import com.graphhopper.routing.profiles.RoadAccess;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.util.HintsMap;
 import com.graphhopper.util.EdgeIteratorState;
@@ -39,12 +41,21 @@ public class FastestWeighting extends AbstractWeighting {
     private final double headingPenalty;
     private final long headingPenaltyMillis;
     private final double maxSpeed;
+    private EnumEncodedValue<RoadAccess> roadAccessEnc = null;
+    // this factor puts a penalty on roads with a "destination"-only access, see #733
+    private double roadAccessPenalty;
 
     public FastestWeighting(FlagEncoder encoder, PMap map) {
         super(encoder);
         headingPenalty = map.getDouble(Routing.HEADING_PENALTY, Routing.DEFAULT_HEADING_PENALTY);
         headingPenaltyMillis = Math.round(headingPenalty * 1000);
         maxSpeed = encoder.getMaxSpeed() / SPEED_CONV;
+
+        if (encoder.hasEncodedValue(RoadAccess.KEY)) {
+            // ensure that we do not need to change getMinWeight, i.e. road_access_factor >= 1
+            roadAccessPenalty = checkBounds("road_access_factor", map.getDouble("road_access_factor", 10), 1, 10);
+            roadAccessEnc = encoder.getEnumEncodedValue(RoadAccess.KEY, RoadAccess.class);
+        }
     }
 
     public FastestWeighting(FlagEncoder encoder) {
@@ -64,6 +75,9 @@ public class FastestWeighting extends AbstractWeighting {
 
         double time = edge.getDistance() / speed * SPEED_CONV;
 
+        if (roadAccessEnc != null && edge.get(roadAccessEnc) == RoadAccess.DESTINATION)
+            time *= roadAccessPenalty;
+
         // add direction penalties at start/stop/via points
         boolean unfavoredEdge = edge.get(EdgeIteratorState.UNFAVORED_EDGE);
         if (unfavoredEdge)
@@ -81,6 +95,13 @@ public class FastestWeighting extends AbstractWeighting {
             time += headingPenaltyMillis;
 
         return time + super.calcMillis(edgeState, reverse, prevOrNextEdgeId);
+    }
+
+    static double checkBounds(String key, double val, double from, double to) {
+        if (val < from || val > to)
+            throw new IllegalArgumentException(key + " has invalid range should be within [" + from + ", " + to + "]");
+
+        return val;
     }
 
     @Override

core/src/test/java/com/graphhopper/routing/util/CarFlagEncoderTest.java
@@ -19,15 +19,20 @@ package com.graphhopper.routing.util;
 
 import com.graphhopper.reader.ReaderNode;
 import com.graphhopper.reader.ReaderWay;
-import com.graphhopper.routing.profiles.*;
+import com.graphhopper.routing.profiles.BooleanEncodedValue;
+import com.graphhopper.routing.profiles.DecimalEncodedValue;
+import com.graphhopper.routing.profiles.EncodedValue;
+import com.graphhopper.routing.profiles.Roundabout;
+import com.graphhopper.routing.util.parsers.OSMRoadAccessParser;
+import com.graphhopper.routing.weighting.FastestWeighting;
 import com.graphhopper.storage.IntsRef;
+import com.graphhopper.util.GHUtility;
 import com.graphhopper.util.Helper;
 import com.graphhopper.util.PMap;
 import org.junit.Test;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -37,8 +42,10 @@ import static org.junit.Assert.*;
  * @author Peter Karich
  */
 public class CarFlagEncoderTest {
-    private final EncodingManager em = EncodingManager.create(Arrays.asList(new CarFlagEncoder(new PMap("speed_two_directions=true")),
-            new BikeFlagEncoder(), new FootFlagEncoder()));
+    private final EncodingManager em = new EncodingManager.Builder().
+            add(new OSMRoadAccessParser()).
+            add(new CarFlagEncoder(new PMap("speed_two_directions=true"))).
+            add(new BikeFlagEncoder()).add(new FootFlagEncoder()).build();
     private final CarFlagEncoder encoder = (CarFlagEncoder) em.getEncoder("car");
     private final BooleanEncodedValue roundaboutEnc = em.getBooleanEncodedValue(Roundabout.KEY);
     private final DecimalEncodedValue avSpeedEnc = encoder.getAverageSpeedEnc();
@@ -208,13 +215,18 @@ public class CarFlagEncoderTest {
 
     @Test
     public void testDestinationTag() {
+        FastestWeighting weighting = new FastestWeighting(encoder);
+
         ReaderWay way = new ReaderWay(1);
         way.setTag("highway", "secondary");
-        assertEquals(60, encoder.getSpeed(way), 1e-1);
+        EncodingManager.AcceptWay acceptWay = new EncodingManager.AcceptWay();
+        assertTrue(em.acceptWay(way, acceptWay));
+        IntsRef edgeFlags = em.handleWayTags(way, acceptWay, 0);
+        assertEquals(60, weighting.calcWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false, -1), 0.1);
 
         way.setTag("vehicle", "destination");
-        IntsRef flags = encoder.handleWayTags(em.createEdgeFlags(), way, encoder.getAccess(way), 0);
-        assertEquals(5, avSpeedEnc.getDecimal(false, flags), 1e-1);
+        edgeFlags = em.handleWayTags(way, acceptWay, 0);
+        assertEquals(600, weighting.calcWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false, -1), 0.1);
     }
 
     @Test

core/src/main/java/com/graphhopper/routing/weighting/ShortFastestWeighting.java
@@ -37,11 +37,10 @@ public class ShortFastestWeighting extends FastestWeighting {
 
     public ShortFastestWeighting(FlagEncoder encoder, PMap map) {
         super(encoder);
-        timeFactor = checkBounds(TIME_FACTOR, map.getDouble(TIME_FACTOR, 1));
+        timeFactor = checkBounds(TIME_FACTOR, map.getDouble(TIME_FACTOR, 1), 0, 10);
 
-        // is it faster to include timeFactor via distanceFactor = tmp / timeFactor?
         // default value derived from the cost for time e.g. 25€/hour and for distance 0.5€/km
-        distanceFactor = checkBounds(DISTANCE_FACTOR, map.getDouble(DISTANCE_FACTOR, 0.07));
+        distanceFactor = checkBounds(DISTANCE_FACTOR, map.getDouble(DISTANCE_FACTOR, 0.07), 0, 10);
 
         if (timeFactor < 1e-5 && distanceFactor < 1e-5)
             throw new IllegalArgumentException("[" + NAME + "] one of distance_factor or time_factor has to be non-zero");
@@ -49,7 +48,7 @@ public class ShortFastestWeighting extends FastestWeighting {
 
     public ShortFastestWeighting(FlagEncoder encoder, double distanceFactor) {
         super(encoder);
-        this.distanceFactor = checkBounds(DISTANCE_FACTOR, distanceFactor);
+        this.distanceFactor = checkBounds(DISTANCE_FACTOR, distanceFactor, 0, 10);
         this.timeFactor = 1;
     }
 
@@ -67,12 +66,5 @@ public class ShortFastestWeighting extends FastestWeighting {
     @Override
     public String getName() {
         return NAME;
-    }
-
-    private double checkBounds(String key, double val) {
-        if (val < 0 || val > 10)
-            throw new IllegalArgumentException(key + " has invalid range should be within [0, 10]");
-
-        return val;
     }
 }

commit 1f2084fde36c9250d7fea59b609234145a226792

avoidance of destination only for car, fixes #1831, bug introduced in #1752

core/src/main/java/com/graphhopper/routing/util/BikeCommonFlagEncoder.java
@@ -19,6 +19,7 @@ package com.graphhopper.routing.util;
 
 import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.profiles.*;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.routing.weighting.PriorityWeighting;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.Helper;
@@ -193,6 +194,11 @@ abstract public class BikeCommonFlagEncoder extends AbstractFlagEncoder {
 
         speedDefault = highwaySpeeds.get("cycleway");
         setAvoidSpeedLimit(71);
+    }
+
+    @Override
+    public TransportationMode getTransportationMode() {
+        return TransportationMode.BICYCLE;
     }
 
     @Override

core/src/main/java/com/graphhopper/routing/util/CarFlagEncoder.java
@@ -17,10 +17,10 @@
  */
 package com.graphhopper.routing.util;
 
-import com.graphhopper.reader.OSMTurnRelation;
 import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.profiles.EncodedValue;
 import com.graphhopper.routing.profiles.UnsignedDecimalEncodedValue;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.Helper;
 import com.graphhopper.util.PMap;
@@ -142,6 +142,10 @@ public class CarFlagEncoder extends AbstractFlagEncoder {
         badSurfaceSpeed = 30;
         maxPossibleSpeed = 140;
         speedDefault = defaultSpeedMap.get("secondary");
+    }
+
+    public TransportationMode getTransportationMode() {
+        return TransportationMode.MOTOR_VEHICLE;
     }
 
     @Override

core/src/main/java/com/graphhopper/routing/util/FlagEncoder.java
@@ -20,6 +20,7 @@ package com.graphhopper.routing.util;
 import com.graphhopper.routing.profiles.BooleanEncodedValue;
 import com.graphhopper.routing.profiles.DecimalEncodedValue;
 import com.graphhopper.routing.profiles.EncodedValueLookup;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 
 /**
  * This class provides methods to define how a value (like speed or direction) converts to a flag
@@ -34,6 +35,8 @@ public interface FlagEncoder extends EncodedValueLookup {
      * introduced
      */
     int getVersion();
+
+    TransportationMode getTransportationMode();
 
     /**
      * @return the maximum speed in km/h

core/src/main/java/com/graphhopper/routing/util/FootFlagEncoder.java
@@ -19,6 +19,7 @@ package com.graphhopper.routing.util;
 
 import com.graphhopper.reader.ReaderWay;
 import com.graphhopper.routing.profiles.*;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.routing.weighting.PriorityWeighting;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.PMap;
@@ -141,6 +142,10 @@ public class FootFlagEncoder extends AbstractFlagEncoder {
 
         maxPossibleSpeed = FERRY_SPEED;
         speedDefault = MEAN_SPEED;
+    }
+
+    public TransportationMode getTransportationMode() {
+        return TransportationMode.FOOT;
     }
 
     @Override

core/src/main/java/com/graphhopper/routing/util/spatialrules/TransportationMode.java
@@ -27,9 +27,7 @@ package com.graphhopper.routing.util.spatialrules;
  */
 public enum TransportationMode {
 
-    MOTOR_VEHICLE(0),
-    BICYCLE(1),
-    FOOT(2);
+    OTHER(0), MOTOR_VEHICLE(1), BICYCLE(2), FOOT(3);
 
     private final int value;
 

core/src/main/java/com/graphhopper/routing/weighting/FastestWeighting.java
@@ -21,6 +21,7 @@ import com.graphhopper.routing.profiles.EnumEncodedValue;
 import com.graphhopper.routing.profiles.RoadAccess;
 import com.graphhopper.routing.util.FlagEncoder;
 import com.graphhopper.routing.util.HintsMap;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.util.EdgeIteratorState;
 import com.graphhopper.util.PMap;
 import com.graphhopper.util.Parameters.Routing;
@@ -45,7 +46,7 @@ public class FastestWeighting extends AbstractWeighting {
     private final double maxSpeed;
     private final EnumEncodedValue<RoadAccess> roadAccessEnc;
     // this factor puts a penalty on roads with a "destination"-only access, see #733
-    private final double roadAccessPenalty;
+    private final double destinationPenalty;
 
     public FastestWeighting(FlagEncoder encoder) {
         this(encoder, new HintsMap(0));
@@ -65,14 +66,13 @@ public class FastestWeighting extends AbstractWeighting {
         headingPenaltyMillis = Math.round(headingPenalty * 1000);
         maxSpeed = encoder.getMaxSpeed() / SPEED_CONV;
 
-        if (encoder.hasEncodedValue(RoadAccess.KEY)) {
-            // ensure that we do not need to change getMinWeight, i.e. road_access_factor >= 1
-            roadAccessPenalty = checkBounds("road_access_factor", map.getDouble("road_access_factor", 10), 1, 10);
-            roadAccessEnc = encoder.getEnumEncodedValue(RoadAccess.KEY, RoadAccess.class);
-        } else {
-            roadAccessPenalty = 0;
-            roadAccessEnc = null;
-        }
+        if (!encoder.hasEncodedValue(RoadAccess.KEY))
+            throw new IllegalArgumentException("road_access is not available but expected for FastestWeighting");
+
+        // ensure that we do not need to change getMinWeight, i.e. road_access_factor >= 1
+        double defaultFactor = encoder.getTransportationMode() == TransportationMode.MOTOR_VEHICLE ? 10 : 1;
+        destinationPenalty = checkBounds("road_access_destination_factor", map.getDouble("road_access_destination_factor", defaultFactor), 1, 10);
+        roadAccessEnc = destinationPenalty > 1 ? encoder.getEnumEncodedValue(RoadAccess.KEY, RoadAccess.class) : null;
     }
 
     @Override
@@ -88,9 +88,11 @@ public class FastestWeighting extends AbstractWeighting {
 
         double time = edgeState.getDistance() / speed * SPEED_CONV;
 
-        if (roadAccessEnc != null && edgeState.get(roadAccessEnc) == RoadAccess.DESTINATION)
-            time *= roadAccessPenalty;
-
+        if (roadAccessEnc != null) {
+            RoadAccess access = edgeState.get(roadAccessEnc);
+            if (access == RoadAccess.DESTINATION)
+                time *= destinationPenalty;
+        }
         // add direction penalties at start/stop/via points
         boolean unfavoredEdge = edgeState.get(EdgeIteratorState.UNFAVORED_EDGE);
         if (unfavoredEdge)

core/src/test/java/com/graphhopper/routing/util/CarFlagEncoderTest.java
@@ -221,16 +221,20 @@ public class CarFlagEncoderTest {
         IntsRef relFlags = em.createRelationFlags();
 
         FastestWeighting weighting = new FastestWeighting(encoder);
+        FastestWeighting bikeWeighting = new FastestWeighting(em.getEncoder("bike"));
+
         ReaderWay way = new ReaderWay(1);
         way.setTag("highway", "secondary");
         EncodingManager.AcceptWay acceptWay = new EncodingManager.AcceptWay();
         assertTrue(em.acceptWay(way, acceptWay));
         IntsRef edgeFlags = em.handleWayTags(way, acceptWay, relFlags);
         assertEquals(60, weighting.calcEdgeWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false), 0.1);
+        assertEquals(200, bikeWeighting.calcEdgeWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false), 0.1);
 
         way.setTag("vehicle", "destination");
         edgeFlags = em.handleWayTags(way, acceptWay, relFlags);
         assertEquals(600, weighting.calcEdgeWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false), 0.1);
+        assertEquals(200, bikeWeighting.calcEdgeWeight(GHUtility.createMockedEdgeIteratorState(1000, edgeFlags), false), 0.1);
     }
 
     @Test

core/src/test/java/com/graphhopper/routing/util/EncodingManagerTest.java
@@ -23,6 +23,7 @@ import com.graphhopper.routing.profiles.BooleanEncodedValue;
 import com.graphhopper.routing.profiles.EnumEncodedValue;
 import com.graphhopper.routing.profiles.Roundabout;
 import com.graphhopper.routing.profiles.RouteNetwork;
+import com.graphhopper.routing.util.spatialrules.TransportationMode;
 import com.graphhopper.storage.IntsRef;
 import org.junit.Test;
 
@@ -83,6 +84,10 @@ public class EncodingManagerTest {
     @Test
     public void testToDetailsStringIncludesEncoderVersionNumber() {
         FlagEncoder encoder = new AbstractFlagEncoder(1, 2.0, 0) {
+            public TransportationMode getTransportationMode() {
+                return TransportationMode.BICYCLE;
+            }
+
             @Override
             public int getVersion() {
                 return 10;