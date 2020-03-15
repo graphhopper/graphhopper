1b07cd6408003413aa90a87ecb0b48ab71a425e7
Peter <graphhopper@gmx.de>
2020-03-15 19:35:16 +0100

    javadoc change

--- core/src/main/java/com/graphhopper/util/FetchMode.java
+++ core/src/main/java/com/graphhopper/util/FetchMode.java
@@ -1,8 +1,8 @@
 package com.graphhopper.util;
 
 /**
- * This enum is used to specify which nodes should be included when calling FetchMode. See
- * {@link EdgeIteratorState#fetchWayGeometry(FetchMode)} and docs/core/low-level-api.md
+ * This enum is used to specify which nodes should be included in the PointList when calling
+ * {@link EdgeIteratorState#fetchWayGeometry(FetchMode)}. See also docs/core/low-level-api.md
  */
 public enum FetchMode {
     TOWER_ONLY, PILLAR_ONLY, BASE_AND_PILLAR, PILLAR_AND_ADJ, ALL;
