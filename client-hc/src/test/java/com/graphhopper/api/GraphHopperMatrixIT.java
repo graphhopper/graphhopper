8122118853555ea2e571779c8021d1f068b1b66d
Peter <graphhopper@gmx.de>
2020-04-07 23:28:48 +0200

    client-hc: fix integration tests (we need to throw them out here)

--- client-hc/src/test/java/com/graphhopper/api/GraphHopperMatrixIT.java
+++ client-hc/src/test/java/com/graphhopper/api/GraphHopperMatrixIT.java
@@ -1,12 +1,15 @@
 package com.graphhopper.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.graphhopper.util.shapes.GHPoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.*;
 
@@ -44,20 +47,20 @@ public class GraphHopperMatrixIT {
         // no distances available
         try {
             assertEquals(0, res.getDistance(1, 2), 1);
-            fail();
+            fail("there should be an exception when trying to get distances");
         } catch (Exception ex) {
         }
 
         // ... only weight:
-        assertEquals(1930, res.getWeight(1, 2), 10);
+        assertEquals(1940, res.getWeight(1, 2), 20);
 
         req = AbstractGHMatrixWebTester.createRequest();
         req.addOutArray("weights");
         req.addOutArray("distances");
         res = ghMatrix.route(req);
 
-        assertEquals(9800, res.getDistance(1, 2), 50);
-        assertEquals(1930, res.getWeight(1, 2), 10);
+        assertEquals(9970, res.getDistance(1, 2), 100);
+        assertEquals(1940, res.getWeight(1, 2), 20);
     }
 
     @Test
@@ -270,6 +273,51 @@ public class GraphHopperMatrixIT {
                 }
             }
         }
+    }
+
+    @Test
+    public void testMatrix_DoNotWrapHints() {
+        final GraphHopperMatrixWeb tmpGHMatrix = new GraphHopperMatrixWeb(new GHMatrixBatchRequester() {
+            @Override
+            protected String postJson(String url, JsonNode data) throws IOException {
+                assertFalse(data.has("hints"));
+                assertTrue(data.has("something"));
+                return super.postJson(url, data);
+            }
+        });
+        tmpGHMatrix.setKey(System.getProperty("graphhopper.key", GraphHopperWebIT.KEY));
+
+        GHMRequest req = new GHMRequest();
+        req.addPoint(new GHPoint(49.6724, 11.3494));
+        req.addPoint(new GHPoint(49.6550, 11.4180));
+        req.putHint("something", "xy");
+        tmpGHMatrix.route(req);
+
+        // clashing parameter will overwrite!
+        req.putHint("vehicle", "xy");
+        assertEquals("xy", req.getVehicle());
+    }
+
+    @Test
+    public void doNotIncludeEmptyCurbsidesList() {
+        final AtomicInteger counter = new AtomicInteger(0);
+        final GraphHopperMatrixWeb ghMatrix = new GraphHopperMatrixWeb(new GHMatrixBatchRequester() {
+            @Override
+            protected String postJson(String url, JsonNode data) throws IOException {
+                assertFalse(data.has("curbsides"));
+                assertTrue(data.has("points"));
+                counter.incrementAndGet();
+                return "";
+            }
+        });
+        GHMRequest req = new GHMRequest();
+        req.addPoint(new GHPoint(49.6724, 11.3494));
+        req.addPoint(new GHPoint(49.6550, 11.4180));
+        try {
+            ghMatrix.route(req);
+        } catch (Exception ex) {
+        }
+        assertEquals(1, counter.get());
     }
 
     @Test
