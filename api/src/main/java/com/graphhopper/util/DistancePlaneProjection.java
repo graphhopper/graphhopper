ffb20d90066c83380b58c3f8f16b04532a15cb0e
easbar <easbar.mail@posteo.net>
2020-06-02 13:10:09 +0200

    try taylor

--- api/src/main/java/com/graphhopper/util/DistancePlaneProjection.java
+++ api/src/main/java/com/graphhopper/util/DistancePlaneProjection.java
@@ -65,7 +65,7 @@ public class DistancePlaneProjection extends DistanceCalcEarth {
         double dLat = toRadians(toLat - fromLat);
         double dLon = toRadians(toLon - fromLon);
 //        double left = cos((float)toRadians((fromLat + toLat) / 2)) * dLon;
-        double left = FastMath.cos((float) toRadians((fromLat + toLat) / 2)) * dLon;
+        double left = FastMath.cos(toRadians((fromLat + toLat) / 2)) * dLon;
         return dLat * dLat + left * left;
     }
 
@@ -75,20 +75,34 @@ public class DistancePlaneProjection extends DistanceCalcEarth {
     }
 
     public static class FastMath {
-        private static final double[] vals = new double[65536];
+        private static final double PI2 = Math.PI * 0.5;
 
-        public static double sin(float f) {
-            return vals[(int) (f * 10430.378F) & '\uffff'];
+        public static double sin(double x) {
+            double x2 = x * x;
+            double x3 = x2 * x;
+            double x5 = x2 * x3;
+            double x7 = x2 * x5;
+            double x9 = x2 * x7;
+            double x11 = x2 * x9;
+            double x13 = x2 * x11;
+            double x15 = x2 * x13;
+            double x17 = x2 * x15;
+
+            double val = x;
+            val -= x3 * 0.16666666666666666666666666666667;
+            val += x5 * 0.00833333333333333333333333333333;
+            val -= x7 * 1.984126984126984126984126984127e-4;
+            val += x9 * 2.7557319223985890652557319223986e-6;
+            val -= x11 * 2.5052108385441718775052108385442e-8;
+            val += x13 * 1.6059043836821614599392377170155e-10;
+            val -= x15 * 7.6471637318198164759011319857881e-13;
+            val += x17 * 2.8114572543455207631989455830103e-15;
+            return val;
         }
 
-        public static double cos(float f) {
-            return vals[(int) (f * 10430.378F + 16384.0F) & '\uffff'];
+        public static double cos(double rad) {
+            return sin(rad + PI2);
         }
 
-        static {
-            for (int i = 0; i < 65536; ++i) {
-                vals[i] = Math.sin((double) i * 3.141592653589793D * 2.0D / 65536.0D);
-            }
-        }
     }
 }
