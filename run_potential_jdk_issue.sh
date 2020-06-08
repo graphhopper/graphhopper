3ad5694a17451c88b66877b27a564cf0a1a998eb
Peter <graphhopper@gmx.de>
2020-06-09 01:22:01 +0200

    swap both runs to show that even a 'warmed up' run is slower than the 'cold'/first

--- run_potential_jdk_issue.sh
+++ run_potential_jdk_issue.sh
@@ -5,15 +5,16 @@ OUTPUT_FILE=${1:-$DEFAULT_OUTPUT_FILE}
 if [ ! -f $ALL_IN_ONE_JAR ]; then
     mvn package -am -pl tools -DskipTests
 fi
-# this runs the program with some extra code that does affect the results (routingCH.mean increases), even though
-# it should not
-java -cp $ALL_IN_ONE_JAR com.graphhopper.tools.Measurement \
-   measurement.count_new_edge_tests=5000 \
-   measurement.count=5000 \
-   measurement.summaryfile=$OUTPUT_FILE
 
 # this runs the program without the extra code resulting in lower values of routingCH.mean
 java -cp $ALL_IN_ONE_JAR com.graphhopper.tools.Measurement \
    measurement.count_new_edge_tests=0 \
+   measurement.count=5000 \
+   measurement.summaryfile=$OUTPUT_FILE
+
+# this runs the program with some extra code that does affect the results (routingCH.mean increases), even though
+# it should not
+java -cp $ALL_IN_ONE_JAR com.graphhopper.tools.Measurement \
+   measurement.count_new_edge_tests=5000 \
    measurement.count=5000 \
    measurement.summaryfile=$OUTPUT_FILE
