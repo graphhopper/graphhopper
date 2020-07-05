83a2af30f47a1ec03472322ab5f12805feeccfe6
Peter <graphhopper@gmx.de>
2020-07-05 15:04:34 +0200

    try -XX:+UnlockExperimentalVMOptions and -XX:+UseZGC

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -38,7 +38,7 @@ mkdir -p ${RESULTS_DIR}
 mkdir -p ${TMP_DIR}
 mkdir -p ${SINGLE_RESULTS_DIR}
 
-JAVA_OPTS="-XX:+UseTransparentHugePages -XX:+AlwaysPreTouch"
+JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseZGC"
 # actually run the benchmarks:
 echo "1 - small map: node- and edge-based CH + slow routing"
 java $JAVA_OPTS -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar com.graphhopper.tools.Measurement \
