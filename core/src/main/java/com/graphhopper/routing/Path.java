84640e26a1011af4bd767d0d2ef11534f01f850b
easbar <easbar.mail@posteo.net>
2020-07-20 17:36:47 +0200

    Fix previous edge id in forEveryEdge

--- core/src/main/java/com/graphhopper/routing/Path.java
+++ core/src/main/java/com/graphhopper/routing/Path.java
@@ -204,7 +204,9 @@ public class Path {
             tmpNode = edgeBase.getBaseNode();
             // more efficient swap, currently not implemented for virtual edges: visitor.next(edgeBase.detach(true), i);
             edgeBase = singleEdgeExplorer.setEdge(edgeBase.getEdge(), tmpNode);
+            int prev = edgeBase.getEdge();
             visitor.next(edgeBase, i, prevEdgeId);
+            prevEdgeId = prev;
         }
         visitor.finish();
     }
