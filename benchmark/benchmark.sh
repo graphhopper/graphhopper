bee18adbca599b39022ae94081962f526aa4f810
easbar <easbar.mail@posteo.net>
2020-09-11 18:54:37 +0200

    Big map only

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -39,31 +39,6 @@ mkdir -p ${TMP_DIR}
 mkdir -p ${SINGLE_RESULTS_DIR}
 
 # actually run the benchmarks:
-echo "1 - small map: node- and edge-based CH + slow routing"
-java -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar \
--XX:+UseParallelGC -Xmx20g -Xms20g \
-com.graphhopper.tools.Measurement \
-datareader.file=${SMALL_OSM_MAP} \
-datareader.date_range_parser_day=2019-11-01 \
-measurement.name=small_map \
-measurement.folder=${SINGLE_RESULTS_DIR} \
-measurement.clean=true \
-measurement.summaryfile=${RESULTS_DIR}summary_small.dat \
-measurement.repeats=1 \
-measurement.run_slow_routing=true \
-measurement.weighting=fastest \
-measurement.ch.node=true \
-measurement.ch.edge=true \
-measurement.lm=false \
-"graph.flag_encoders=car|turn_costs=true" \
-graph.location=${TMP_DIR}measurement-small-gh \
-prepare.min_network_size=10000 \
-spatial_rules.borders_directory=${BORDERS_DIRECTORY} \
-measurement.json=true \
-measurement.count=5000 \
-measurement.use_measurement_time_as_ref_time=${USE_MEASUREMENT_TIME_AS_REF_TIME} \
-"measurement.block_area=49.394664,11.144428,49.348388,11.144943,49.355768,11.227169,49.411643,11.227512"
-
 echo "2 - big map: node-based CH + landmarks (edge- & node-based for LM)"
 java -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar \
 -XX:+UseParallelGC -Xmx20g -Xms20g \
@@ -77,73 +52,13 @@ measurement.summaryfile=${RESULTS_DIR}summary_big.dat \
 measurement.repeats=1 \
 measurement.run_slow_routing=false \
 measurement.weighting=fastest \
-measurement.ch.node=true \
+measurement.ch.node=false \
 measurement.ch.edge=false \
 measurement.lm=true \
 "measurement.lm.active_counts=[4,8,12,16]" \
 measurement.lm.edge_based=true \
 "graph.flag_encoders=car|turn_costs=true" \
 graph.location=${TMP_DIR}measurement-big-gh \
-prepare.min_network_size=10000 \
-spatial_rules.borders_directory=${BORDERS_DIRECTORY} \
-measurement.json=true \
-measurement.count=5000 \
-measurement.use_measurement_time_as_ref_time=${USE_MEASUREMENT_TIME_AS_REF_TIME} \
-"measurement.block_area=49.394664,11.144428,49.348388,11.144943,49.355768,11.227169,49.411643,11.227512"
-
-echo "3 - big map with a custom model that is 'a little customized', i.e. similar to the standard fastest-car profile"
-echo "node-based CH + LM"
-java -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar \
--XX:+UseParallelGC -Xmx20g -Xms20g \
-com.graphhopper.tools.Measurement \
-datareader.file=${BIG_OSM_MAP} \
-datareader.date_range_parser_day=2019-11-01 \
-measurement.name=big_map_little_custom \
-measurement.folder=${SINGLE_RESULTS_DIR} \
-measurement.clean=true \
-measurement.summaryfile=${RESULTS_DIR}summary_big_little_custom.dat \
-measurement.repeats=1 \
-measurement.run_slow_routing=false \
-measurement.weighting=custom \
-measurement.custom_model_file=benchmark/little_custom.yml \
-graph.encoded_values=max_width,max_height,toll,hazmat \
-measurement.ch.node=true \
-measurement.ch.edge=false \
-measurement.lm=true \
-"measurement.lm.active_counts=[8]" \
-measurement.lm.edge_based=false \
-"graph.flag_encoders=car|turn_costs=true" \
-graph.location=${TMP_DIR}measurement-big-little-custom-gh \
-prepare.min_network_size=10000 \
-spatial_rules.borders_directory=${BORDERS_DIRECTORY} \
-measurement.json=true \
-measurement.count=5000 \
-measurement.use_measurement_time_as_ref_time=${USE_MEASUREMENT_TIME_AS_REF_TIME} \
-"measurement.block_area=49.394664,11.144428,49.348388,11.144943,49.355768,11.227169,49.411643,11.227512"
-
-echo "4 - big map with a custom model that is 'very customized', i.e. has many custom weighting rules"
-echo "node-based CH + LM"
-java -cp tools/target/graphhopper-tools-*-jar-with-dependencies.jar \
--XX:+UseParallelGC -Xmx20g -Xms20g \
-com.graphhopper.tools.Measurement \
-datareader.file=${BIG_OSM_MAP} \
-datareader.date_range_parser_day=2019-11-01 \
-measurement.name=big_map_very_custom \
-measurement.folder=${SINGLE_RESULTS_DIR} \
-measurement.clean=true \
-measurement.summaryfile=${RESULTS_DIR}summary_big_very_custom.dat \
-measurement.repeats=1 \
-measurement.run_slow_routing=false \
-measurement.weighting=custom \
-measurement.custom_model_file=benchmark/very_custom.yml \
-graph.encoded_values=max_width,max_height,toll,hazmat \
-measurement.ch.node=true \
-measurement.ch.edge=false \
-measurement.lm=true \
-"measurement.lm.active_counts=[8]" \
-measurement.lm.edge_based=false \
-"graph.flag_encoders=car|turn_costs=true" \
-graph.location=${TMP_DIR}measurement-big-very-custom-gh \
 prepare.min_network_size=10000 \
 spatial_rules.borders_directory=${BORDERS_DIRECTORY} \
 measurement.json=true \
