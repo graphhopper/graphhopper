9e9e0a811fe811e2ca4c4a1a457679d146bb9b17
Peter <graphhopper@gmx.de>
2020-10-01 23:07:53 +0200

    moved most of the examples from the docs into refactorable source code

---
+++ example/src/test/java/com/graphhopper/example/LocationIndexExampleTest.java
@@ -0,0 +1,55 @@
+package com.graphhopper.example;
+
+import com.graphhopper.GraphHopper;
+import com.graphhopper.reader.osm.GraphHopperOSM;
+import com.graphhopper.routing.ev.EncodedValue;
+import com.graphhopper.routing.util.CarFlagEncoder;
+import com.graphhopper.routing.util.EdgeFilter;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.Directory;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.storage.RAMDirectory;
+import com.graphhopper.storage.index.LocationIndex;
+import com.graphhopper.storage.index.LocationIndexTree;
+import com.graphhopper.storage.index.Snap;
+import com.graphhopper.util.EdgeIteratorState;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LocationIndexExampleTest {
+
+    @Test
+    public void graphhopperLocationIndex() {
+        GraphHopper hopper = new GraphHopperOSM().setEncodingManager(EncodingManager.create(new CarFlagEncoder()));
+        hopper.setDataReaderFile("../core/files/andorra.osm.pbf");
+        hopper.setGraphHopperLocation("target/locationindex-graph-cache");
+        hopper.importOrLoad();
+
+        LocationIndex index = hopper.getLocationIndex();
+
+        // now you can fetch the closest edge via:
+        Snap snap = index.findClosest(42.508552, 1.532936, EdgeFilter.ALL_EDGES);
+        EdgeIteratorState edge = snap.getClosestEdge();
+        assertEquals("Avinguda Meritxell", edge.getName());
+    }
+
+    @Test
+    public void lowLevelLocationIndex() {
+        // If you don't use the GraphHopper class you have to use the low level API:
+        Directory dir = new RAMDirectory();
+        GraphHopperStorage graph = new GraphHopperStorage(dir, EncodingManager.create(new CarFlagEncoder()), false).create(100);
+        graph.edge(0, 1).setName("test edge");
+        graph.getNodeAccess().setNode(0, 12, 42);
+        graph.getNodeAccess().setNode(1, 12.01, 42.01);
+
+        LocationIndexTree index = new LocationIndexTree(graph.getBaseGraph(), dir);
+        index.setResolution(300);
+        index.setMaxRegionSearch(4);
+        if (!index.loadExisting())
+            index.prepareIndex();
+        Snap snap = index.findClosest(12, 42, EdgeFilter.ALL_EDGES);
+        EdgeIteratorState edge = snap.getClosestEdge();
+        assertEquals("test edge", edge.getName());
+    }
+}
