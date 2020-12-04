27b94f6aba0a6b87685a260ec98da0b4a9c833ef
Thomas Butz <thomas.butz@optitool.de>
2020-12-04 09:50:44 +0100

    Replace SpatialRuleParserTest with CustomAreaParserTest

---
+++ core/src/test/java/com/graphhopper/routing/util/parsers/CustomAreaParserTest.java
@@ -0,0 +1,44 @@
+package com.graphhopper.routing.util.parsers;
+
+import com.graphhopper.config.CustomArea;
+import com.graphhopper.routing.ev.EnumEncodedValue;
+import com.graphhopper.routing.ev.Surface;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.routing.util.area.CustomAreaLookup;
+import com.graphhopper.routing.util.area.CustomAreaLookupJTS;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Polygon;
+
+import static junit.framework.TestCase.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CustomAreaParserTest {
+    
+    private static final GeometryFactory FAC = new GeometryFactory();
+    
+    private EncodingManager em;
+    private CustomArea areaLeft;
+    private CustomArea areaRight;
+
+    @Before
+    public void setUp() {
+        Polygon borderLeft = FAC.createPolygon(new Coordinate[] { new Coordinate(1, 1), new Coordinate(2, 1), new Coordinate(2, 2), new Coordinate(1, 2), new Coordinate(1, 1) });
+        areaLeft = new CustomArea("left", Collections.singletonList(borderLeft), "area_left");
+        Polygon borderRight = FAC.createPolygon(new Coordinate[] { new Coordinate(3, 1), new Coordinate(4, 1), new Coordinate(4, 2), new Coordinate(3, 2), new Coordinate(3, 1) });
+        areaRight = new CustomArea("right", Collections.singletonList(borderRight), "area_right");
+        CustomAreaLookup customAreaLookup = new CustomAreaLookupJTS(Arrays.asList(areaLeft, areaRight));
+        em = new EncodingManager.Builder().setCustomAreaLookup(customAreaLookup).build();
+    }
+
+    @Test
+    public void testEncodedValueExistence() {
+        assertTrue(em.hasEncodedValue(areaLeft.getEncodedValue()));
+        assertTrue(em.hasEncodedValue(areaRight.getEncodedValue()));
+    }
+}
