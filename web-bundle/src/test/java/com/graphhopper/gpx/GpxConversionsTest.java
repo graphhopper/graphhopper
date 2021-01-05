ed99cd070f1e6ab44e6c95708d4001a6d32b6ed1
easbar <easbar.mail@posteo.net>
2021-01-05 10:11:33 +0100

    Move gpx-schema.xsd to fix GpxConversionsTest

--- web-bundle/src/test/java/com/graphhopper/gpx/GpxConversionsTest.java
+++ web-bundle/src/test/java/com/graphhopper/gpx/GpxConversionsTest.java
@@ -30,8 +30,8 @@ import com.graphhopper.storage.Graph;
 import com.graphhopper.storage.GraphBuilder;
 import com.graphhopper.storage.NodeAccess;
 import com.graphhopper.util.*;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -45,7 +45,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GpxConversionsTest {
 
@@ -53,7 +53,7 @@ public class GpxConversionsTest {
     private FlagEncoder carEncoder;
     private TranslationMap trMap;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         carEncoder = new CarFlagEncoder();
         carManager = EncodingManager.create(carEncoder);
@@ -111,17 +111,17 @@ public class GpxConversionsTest {
         verifyGPX(gpxStr);
 //        System.out.println(gpxStr);
 
-        assertTrue(gpxStr, gpxStr.contains("<trkpt lat=\"15.0\" lon=\"10.0\"><time>1970-01-01T00:00:00Z</time>"));
-        assertTrue(gpxStr, gpxStr.contains("<extensions>") && gpxStr.contains("</extensions>"));
-        assertTrue(gpxStr, gpxStr.contains("<rtept lat=\"15.1\" lon=\"10.0\">"));
-        assertTrue(gpxStr, gpxStr.contains("<gh:distance>8000.0</gh:distance>"));
-        assertTrue(gpxStr, gpxStr.contains("<desc>turn left onto 2-3</desc>"));
-        assertTrue(gpxStr, gpxStr.contains("<gh:sign>-2</gh:sign>"));
+        assertTrue(gpxStr.contains("<trkpt lat=\"15.0\" lon=\"10.0\"><time>1970-01-01T00:00:00Z</time>"), gpxStr);
+        assertTrue(gpxStr.contains("<extensions>") && gpxStr.contains("</extensions>"), gpxStr);
+        assertTrue(gpxStr.contains("<rtept lat=\"15.1\" lon=\"10.0\">"), gpxStr);
+        assertTrue(gpxStr.contains("<gh:distance>8000.0</gh:distance>"), gpxStr);
+        assertTrue(gpxStr.contains("<desc>turn left onto 2-3</desc>"), gpxStr);
+        assertTrue(gpxStr.contains("<gh:sign>-2</gh:sign>"), gpxStr);
 
-        assertTrue(gpxStr, gpxStr.contains("<gh:direction>N</gh:direction>"));
-        assertTrue(gpxStr, gpxStr.contains("<gh:azimuth>0.0</gh:azimuth>"));
+        assertTrue(gpxStr.contains("<gh:direction>N</gh:direction>"), gpxStr);
+        assertTrue(gpxStr.contains("<gh:azimuth>0.0</gh:azimuth>"), gpxStr);
 
-        assertFalse(gpxStr, gpxStr.contains("NaN"));
+        assertFalse(gpxStr.contains("NaN"));
     }
 
     @Test
@@ -170,7 +170,7 @@ public class GpxConversionsTest {
 
         String gpxStr = GpxConversions.createGPX(instructions, "test", 0, true, true, false, false, Constants.VERSION, trMap.getWithFallBack(Locale.US));
 
-        assertTrue(gpxStr, gpxStr.contains("<gh:exit_number>3</gh:exit_number>"));
+        assertTrue(gpxStr.contains("<gh:exit_number>3</gh:exit_number>"), gpxStr);
         verifyGPX(gpxStr);
     }
 
@@ -187,9 +187,9 @@ public class GpxConversionsTest {
 
         String gpxStr = GpxConversions.createGPX(instructions, "test", 0, true, true, true, true, Constants.VERSION, trMap.getWithFallBack(Locale.US));
 
-        assertFalse(gpxStr, gpxStr.contains("E-"));
-        assertTrue(gpxStr, gpxStr.contains("0.000001"));
-        assertTrue(gpxStr, gpxStr.contains("-0.000125"));
+        assertFalse(gpxStr.contains("E-"), gpxStr);
+        assertTrue(gpxStr.contains("0.000001"), gpxStr);
+        assertTrue(gpxStr.contains("-0.000125"), gpxStr);
         verifyGPX(gpxStr);
     }
 
