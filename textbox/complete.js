386b5306323f31d36e3b99bec6b1a859b150ab5f
easbar <easbar.mail@posteo.net>
2021-02-16 09:07:11 +0100

    Add complete function

---
+++ textbox/complete.js
@@ -0,0 +1,63 @@
+import {parse} from './parse';
+import {tokenAtPos} from "./tokenize";
+
+function complete(expression, pos, categories) {
+    const lastNonWhitespace = getLastNonWhitespacePos(expression);
+    if (pos > lastNonWhitespace) {
+        // this is a little trick: we run parse on a manipulated expression where we inserted a dummy character to
+        // see which completions are offered to us (assuming we typed in something)
+        let parseExpression = expression;
+        while (parseExpression.length < pos)
+            parseExpression += ' ';
+        parseExpression = parseExpression.slice(0, pos) + '…';
+        const parseResult = parse(parseExpression, categories);
+        const tokenPos = tokenAtPos(parseExpression, pos);
+
+        // in case the expression has an error at a position that is parsed before our position we return no suggestions
+        if (parseResult.range[0] !== tokenPos.range[0])
+            return empty();
+
+        // we only keep the suggestions that match the already existing characters if there are any
+        const suggestions = parseResult.completions.filter(c => {
+            // we need to remove our dummy character for the filtering
+            const partialToken = tokenPos.token.substring(0, tokenPos.token.length - 1);
+            return c.startsWith(partialToken);
+        });
+        return {
+            suggestions: suggestions,
+            range: suggestions.length === 0 ? null : [tokenPos.range[0], pos]
+        }
+    } else {
+        const tokenPos = tokenAtPos(expression, pos);
+        if (tokenPos.token === null) {
+            return empty();
+        } else {
+            const parseExpression = expression.substring(0, tokenPos.range[0]) + '…' + expression.substring(tokenPos.range[1]);
+            const parseResult = parse(parseExpression, categories);
+            if (parseResult.range[0] !== tokenPos.range[0])
+                return empty();
+            return {
+                suggestions: parseResult.completions,
+                range: tokenPos.range
+            }
+        }
+    }
+}
+
+function empty() {
+    return {
+        suggestions: [],
+        range: null
+    }
+}
+
+function getLastNonWhitespacePos(str) {
+    for (let i = str.length - 1; i >= 0; --i) {
+        if (str.slice(i, i + 1).trim() !== '') {
+            return i;
+        }
+    }
+    return -1;
+}
+
+export {complete};
---
+++ textbox/complete.test.js
@@ -0,0 +1,79 @@
+import {complete} from './complete';
+
+const categories = {
+    'a': ['a1a', 'a1b', 'a2a', 'a2b'],
+    'b': ['b1', 'b2']
+};
+
+describe("complete", () => {
+    test("complete at end of expression", () => {
+        test_complete('a == ', 5, ['a1a', 'a1b', 'a2a', 'a2b'], [5, 5]);
+        test_complete('b == ', 5, ['b1', 'b2'], [5, 5]);
+        test_complete('b ==       ', 5, ['b1', 'b2'], [5, 5]);
+        test_complete('', 12, ['a', 'b'], [12, 12]);
+        test_complete('  ', 12, ['a', 'b'], [12, 12]);
+        test_complete('\t\n', 12, ['a', 'b'], [12, 12]);
+        test_complete('    ', 0, ['a', 'b'], [0, 0]);
+        test_complete('    ', 1, ['a', 'b'], [1, 1]);
+        test_complete('    ', 2, ['a', 'b'], [2, 2]);
+        test_complete('b == ', 4, ['b1', 'b2'], [4, 4]);
+        test_complete('b ==', 4, ['b1', 'b2'], [4, 4]);
+        test_complete('b ==', 9, ['b1', 'b2'], [9, 9]);
+        test_complete('b == ', 9, ['b1', 'b2'], [9, 9]);
+    });
+
+    test("complete at end of expression, incomplete token", () => {
+        // if we complete at the end of the (non-whitespace part of the) expression we get completion suggestions
+        // that consider the characters that were entered already
+        test_complete('a == a1    ', 7, ['a1a', 'a1b'], [5, 7]);
+        test_complete('a == x1    ', 7, [], null);
+        // here we get no suggestions, because the 'previous' expression is invalid (we evaluate 'a == a1 x')
+        test_complete('a == a1    ', 8, [], null);
+    });
+
+    test("complete at end of expression, with previous error", () => {
+        // in case the expression contains an error that is found before the position our cursor is at we do not
+        // get any suggestions, because without further work they are simply not available! we do *not* want to get
+        // suggestions to fix the first error (here a/b instead of xyz)!
+        test_complete('xyz == a1 && a == ', 20, [], null);
+        // here we get suggestions despite the error (unmatched opening '('). this is because even though the opening
+        // parentheses comes first in the expression the error is not detected before the position we are editing is
+        // inspected
+        test_complete('( a == ', 8, ['a1a', 'a1b', 'a2a', 'a2b'], [8, 8]);
+    });
+
+    test("complete at whitespace within expression", () => {
+        test_complete('a == a1', 1, [], null);
+        test_complete('a == a1', 4, [], null);
+        // later: at whitespace within expression we still might to be able to 'add' something
+        // test_complete('a == x1a && b == b1', 4, ['a1a', 'a1b', 'a2a', 'a2b'], [4, 5]);
+        // previous error
+        // test_complete('a == x1a && b == b1', 8, [], [null]);
+    });
+
+    test("complete at token within expression", () => {
+        test_complete('a == a1a', 0, ['a', 'b'], [0, 1]);
+        test_complete('a == a1a', 2, ['==', '!='], [2, 4]);
+        test_complete('a == a1b', 5, ['a1a', 'a1b', 'a2a', 'a2b'], [5, 8]);
+        test_complete('a == a2a', 6, ['a1a', 'a1b', 'a2a', 'a2b'], [5, 8]);
+        test_complete('a == a2b', 7, ['a1a', 'a1b', 'a2a', 'a2b'], [5, 8]);
+        test_complete('a == x2b && b == b1 || a == a1a', 5, ['a1a', 'a1b', 'a2a', 'a2b'], [5, 8]);
+        // previous error
+        test_complete('a == x2b && b == b1 || a == a1a', 12, [], null);
+        test_complete('a == x2b && b == b1 || a == a1a', 17, [], null);
+    });
+
+    // todo: similar to end of expression: partial token within expression!?! maybe we could also check the expression
+    // and only offer partial completions if there is an error already, otherwise offer 'replace'
+});
+
+function test_complete(expression, pos, suggestions, range) {
+    const completion = complete(expression, pos, categories)
+    try {
+        expect(completion.suggestions).toStrictEqual(suggestions);
+        expect(completion.range).toStrictEqual(range);
+    } catch (e) {
+        Error.captureStackTrace(e, test_complete);
+        throw e;
+    }
+}
