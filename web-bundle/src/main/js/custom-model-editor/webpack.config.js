75502443e581772419bd4b70cf85a8df35faef97
easbar <easbar.mail@posteo.net>
2021-02-24 12:07:46 +0100

    Make sure everything is transpiled to es5 including node_modules

--- web-bundle/src/main/js/custom-model-editor/webpack.config.js
+++ web-bundle/src/main/js/custom-model-editor/webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    // with webpack 5 this is needed to transpile webpack's 'glue code' even though we set target es5 in tsconfig already
+    target: 'es5',
     module: {
         rules: [
             {
                 test: /\.js$/,
                 use: 'ts-loader',
-                exclude: /node_modules/,
             }
         ]
     },
