954663c25778f1584c28ab1d814bbcd1d6b0b372
Peter <graphhopper@gmx.de>
2021-05-04 18:25:38 +0200

    use @easbar's suggestion

--- navigation/src/main/java/com/graphhopper/navigation/NavigateResource.java
+++ navigation/src/main/java/com/graphhopper/navigation/NavigateResource.java
@@ -190,7 +190,7 @@ public class NavigateResource {
     private List<GHPoint> getPointsFromRequest(HttpServletRequest httpServletRequest, String profile) {
         String url = httpServletRequest.getRequestURI();
         String urlStart = "/navigate/directions/v5/gh/" + profile + "/";
-        if (url.indexOf(urlStart) != 0) throw new IllegalArgumentException("Incorrect URL " + url);
+        if (!url.startsWith(urlStart)) throw new IllegalArgumentException("Incorrect URL " + url);
         url = url.substring(urlStart.length());
         String[] pointStrings = url.split(";");
         List<GHPoint> points = new ArrayList<>(pointStrings.length);
