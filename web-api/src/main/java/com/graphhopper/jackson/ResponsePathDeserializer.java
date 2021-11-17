5d89be4b4d056eb647e36ff65eca0e604fe112d2
Thomas Butz <thomas.butz@optitool.de>
2021-11-17 14:14:25 +0100

    Add deserialization support

--- web-api/src/main/java/com/graphhopper/jackson/ResponsePathDeserializer.java
+++ web-api/src/main/java/com/graphhopper/jackson/ResponsePathDeserializer.java
@@ -265,6 +265,9 @@ public class ResponsePathDeserializer extends JsonDeserializer<ResponsePath> {
                 errors.add(new DetailedIllegalArgumentException(exMessage, toMap(objectMapper, error)));
             else if (exClass.equals(ConnectionNotFoundException.class.getName())) {
                 errors.add(new ConnectionNotFoundException(exMessage, toMap(objectMapper, error)));
+            } else if (exClass.equals(MaximumNodesExceededException.class.getName())) {
+                int maxVisitedNodes = error.get(MaximumNodesExceededException.NODES_KEY).asInt();
+                errors.add(new MaximumNodesExceededException(exMessage, maxVisitedNodes));
             } else if (exClass.equals(PointNotFoundException.class.getName())) {
                 int pointIndex = error.get(PointNotFoundException.INDEX_KEY).asInt();
                 errors.add(new PointNotFoundException(exMessage, pointIndex));
