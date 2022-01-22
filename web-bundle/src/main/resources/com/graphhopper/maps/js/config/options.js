ec7789e3b58030e10938fc2ba6b96453b35df3b7
Andy McCoy <Andykmcc@users.noreply.github.com>
2022-01-21 22:53:17 -0800

    Update tilesets (#1)
    * Add omniscale API key
    * removes unwanted tile servers

--- web-bundle/src/main/resources/com/graphhopper/maps/js/config/options.js
+++ web-bundle/src/main/resources/com/graphhopper/maps/js/config/options.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-// We know that you love 'free', we love it too :)! And so the entire GraphHopper routing engine is not 
-// only free but even Open Source! The GraphHopper Directions API is also free for development. 
+// We know that you love 'free', we love it too :)! And so the entire GraphHopper routing engine is not
+// only free but even Open Source! The GraphHopper Directions API is also free for development.
 // Grab an API key and have fun with installing anything: https://graphhopper.com/#directions-api
 // Misuse of API keys that you don't own is prohibited and you'll be blocked.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -16,6 +16,6 @@ exports.options = {
     routing: {host: '', api_key: ''},
     geocoding: {host: '', api_key: ''},
     thunderforest: {api_key: ''},
-    omniscale: {api_key: ''},
+    omniscale: {api_key: 'bikehopper-staging-d2e01e6f'},
     mapilion: {api_key: ''}
 };
