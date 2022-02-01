62764ab0615281e6a4f5cfbe055d05c6b0a000be
Andy McCoy <andy.k.mccoy@gmail.com>
2022-02-01 01:59:29 -0800

    remove typo

--- graphhopper.sh
+++ graphhopper.sh
@@ -4,7 +4,7 @@ curl -J "http://graphhopper-data-server-svc.$POD_NAMESPACE.svc.cluster.local/geo
 echo 'Finished downloading osm.pbf.';
 
 echo 'Fetching gtfs...';
-curl -J "http://graphhopper-data-server-svc.$POD_NAMESPACE.svc.cluster.local/511/$API_511_PATH" -o $API_511_FILE_NAME --silent;
+curl -J "http://graphhopper-data-server-svc.$POD_NAMESPACE.svc.cluster.local/511$API_511_PATH" -o $API_511_FILE_NAME --silent;
 echo 'Finished downloading gtfs.';
 
 exec java -jar *.jar server /graphhopper/bay-area/config.yml
