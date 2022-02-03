bc54d0097c3c5c3112ff04de9493a6127da73d35
Aina <aina@rasoldier.cf>
2022-02-03 12:50:37 +0100

    accept all route_types

--- reader-gtfs/src/main/java/com/conveyal/gtfs/model/Route.java
+++ reader-gtfs/src/main/java/com/conveyal/gtfs/model/Route.java
@@ -30,51 +30,11 @@ import com.conveyal.gtfs.GTFSFeed;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class Route extends Entity { // implements Entity.Factory<Route>
 
     private static final long serialVersionUID = -819444896818029068L;
-
-    //Used for converting extended route types to simple route types
-    //based on codes found here: https://developers.google.com/transit/gtfs/reference/extended-route-types    
-    public static final Map<Integer, Integer> EXTENTED_ROUTE_TYPE_MAPPING = new HashMap<>();
-    
-    static {
-        EXTENTED_ROUTE_TYPE_MAPPING.put(100, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(101, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(102, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(103, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(105, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(106, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(107, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(108, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(109, 2);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(200, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(201, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(202, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(204, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(208, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(400, 1);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(401, 1);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(402, 1);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(405, 1);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(700, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(701, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(702, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(704, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(715, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(717, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(800, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(900, 0);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(1000, 4);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(1300, 6);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(1400, 7);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(1501, 3);
-        EXTENTED_ROUTE_TYPE_MAPPING.put(1700, 3);
-    }
 
     public String route_id;
     public String agency_id;
@@ -119,7 +79,7 @@ public class Route extends Entity { // implements Entity.Factory<Route>
             r.route_short_name = getStringField("route_short_name", false); // one or the other required, needs a special validator
             r.route_long_name = getStringField("route_long_name", false);
             r.route_desc = getStringField("route_desc", false);
-            r.route_type = getIntField("route_type", true, 0, 7, 0, EXTENTED_ROUTE_TYPE_MAPPING);
+            r.route_type = getIntField("route_type", true, 0, 1702, 0);
             r.route_url = getUrlField("route_url", false);
             r.route_color = getStringField("route_color", false);
             r.route_text_color = getStringField("route_text_color", false);
