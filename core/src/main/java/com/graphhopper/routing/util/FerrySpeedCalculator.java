6a27d59a6e73dbed06406dd46ad5d65579274cdd
easbar <easbar.mail@posteo.net>
2022-03-01 22:09:40 +0100

    FerrySpeedCalculator: Remove unnecessary try/catch

--- core/src/main/java/com/graphhopper/routing/util/FerrySpeedCalculator.java
+++ core/src/main/java/com/graphhopper/routing/util/FerrySpeedCalculator.java
@@ -28,36 +28,34 @@ public class FerrySpeedCalculator {
         double durationInHours = duration / 60d / 60d;
         // Check if our graphhopper specific artificially created estimated_distance way tag is present
         Number estimatedLength = way.getTag("estimated_distance", null);
-        if (durationInHours > 0)
-            try {
-                if (estimatedLength != null) {
-                    double estimatedLengthInKm = estimatedLength.doubleValue() / 1000;
-                    // If duration AND distance is available we can calculate the speed more precisely
-                    // and set both speed to the same value. Factor 1.4 slower because of waiting time!
-                    double calculatedTripSpeed = estimatedLengthInKm / durationInHours / 1.4;
-                    // Plausibility check especially for the case of wrongly used PxM format with the intention to
-                    // specify the duration in minutes, but actually using months
-                    if (calculatedTripSpeed > 0.01d) {
-                        if (calculatedTripSpeed > maxSpeed) {
-                            return maxSpeed;
-                        }
-                        // If the speed is lower than the speed we can store, we have to set it to the minSpeed, but > 0
-                        if (Math.round(calculatedTripSpeed) < speedFactor / 2) {
-                            return speedFactor / 2;
-                        }
-
-                        return Math.round(calculatedTripSpeed);
-                    } else {
-                        long lastId = way.getNodes().isEmpty() ? -1 : way.getNodes().get(way.getNodes().size() - 1);
-                        long firstId = way.getNodes().isEmpty() ? -1 : way.getNodes().get(0);
-                        if (firstId != lastId)
-                            LOGGER.warn("Unrealistic long duration ignored in way with way ID=" + way.getId() + " : Duration tag value="
-                                    + way.getTag("duration") + " (=" + Math.round(duration / 60d) + " minutes)");
-                        durationInHours = 0;
+        if (durationInHours > 0) {
+            if (estimatedLength != null) {
+                double estimatedLengthInKm = estimatedLength.doubleValue() / 1000;
+                // If duration AND distance is available we can calculate the speed more precisely
+                // and set both speed to the same value. Factor 1.4 slower because of waiting time!
+                double calculatedTripSpeed = estimatedLengthInKm / durationInHours / 1.4;
+                // Plausibility check especially for the case of wrongly used PxM format with the intention to
+                // specify the duration in minutes, but actually using months
+                if (calculatedTripSpeed > 0.01d) {
+                    if (calculatedTripSpeed > maxSpeed) {
+                        return maxSpeed;
                     }
+                    // If the speed is lower than the speed we can store, we have to set it to the minSpeed, but > 0
+                    if (Math.round(calculatedTripSpeed) < speedFactor / 2) {
+                        return speedFactor / 2;
+                    }
+
+                    return Math.round(calculatedTripSpeed);
+                } else {
+                    long lastId = way.getNodes().isEmpty() ? -1 : way.getNodes().get(way.getNodes().size() - 1);
+                    long firstId = way.getNodes().isEmpty() ? -1 : way.getNodes().get(0);
+                    if (firstId != lastId)
+                        LOGGER.warn("Unrealistic long duration ignored in way with way ID=" + way.getId() + " : Duration tag value="
+                                + way.getTag("duration") + " (=" + Math.round(duration / 60d) + " minutes)");
+                    durationInHours = 0;
                 }
-            } catch (Exception ex) {
             }
+        }
 
         if (durationInHours == 0) {
             if (estimatedLength != null && estimatedLength.doubleValue() <= 300)
