526e9472b923195629b569fcc6d78c4d216313e8
Andy McCoy <Andykmcc@users.noreply.github.com>
2022-03-03 14:52:42 -0800

    Update proxy-cache service path (#23)

--- graphhopper.sh
+++ graphhopper.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo 'Fetching osm.pbf...';
-curl -J "http://graphhopper-data-proxy-svc.$POD_NAMESPACE.svc.cluster.local/geofabrik$GEOFABRIK_PATH" -o $OSM_PBF_FILE_NAME --silent;
+curl -J "http://proxy-cache-svc.$POD_NAMESPACE/geofabrik$GEOFABRIK_PATH" -o $OSM_PBF_FILE_NAME --silent;
 echo 'Finished downloading osm.pbf.';
 
 echo 'Fetching gtfs...';
-curl -J "http://graphhopper-data-proxy-svc.$POD_NAMESPACE.svc.cluster.local/511$API_511_PATH" -o $API_511_FILE_NAME --silent;
+curl -J "http://proxy-cache-svc.$POD_NAMESPACE/511$API_511_PATH" -o $API_511_FILE_NAME --silent;
 echo 'Finished downloading gtfs.';
 
 exec java -jar *.jar server /graphhopper/bay-area/config.yml
