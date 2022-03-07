59a10d9de77e81a760ea7df78a66e76fcf07577e
Justin Chen <jjchen10@illinois.edu>
2022-03-07 14:14:24 -0600

    refactoring

--- reader-gtfs/src/test/java/com/graphhopper/gtfs/TransfersTest.java
+++ reader-gtfs/src/test/java/com/graphhopper/gtfs/TransfersTest.java
@@ -75,12 +75,10 @@ public class TransfersTest {
     public void testInternalTransfersByToRouteIfRouteSpecific() {
         List<Transfer> transfersToStop = sampleFeed.getTransfersToStop("BEATTY_AIRPORT", "AB");
         assertEquals(5, transfersToStop.size());
-        Set<String> ids = new HashSet<>(Arrays.asList("AB", "FUNNY_BLOCK_AB", "STBA", "AAMV", "ABBFC")); 
-        assertTrue(ids.remove(transfersToStop.get(0).from_route_id));
-        assertTrue(ids.remove(transfersToStop.get(1).from_route_id));
-        assertTrue(ids.remove(transfersToStop.get(2).from_route_id));
-        assertTrue(ids.remove(transfersToStop.get(3).from_route_id));
-        assertTrue(ids.remove(transfersToStop.get(4).from_route_id));
+        Set<String> actualIds = new HashSet<>();
+        transfersToStop.forEach((route) -> actualIds.add(route.from_route_id));
+        Set<String> expectedIds = new HashSet<>(Arrays.asList("AB", "FUNNY_BLOCK_AB", "STBA", "AAMV", "ABBFC")); 
+        assertEquals(expectedIds, actualIds);
     }
 
 }
