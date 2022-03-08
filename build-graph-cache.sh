a9d3843783f537263e3ce756bf73cc720b7b2236
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-08 14:56:14 -0800

    update build shell script

--- build-graph-cache.sh
+++ build-graph-cache.sh
@@ -7,8 +7,20 @@ echo 'Fetching gtfs...';
 curl -J "http://proxy-cache-svc.$POD_NAMESPACE/511$API_511_PATH" -o $API_511_FILE_NAME --silent;
 echo 'Finished downloading gtfs.';
 
+ls -halt
+
+echo 'Building graph-cache...';
 exec java -jar *.jar import /graphhopper/bay-area/config.yml
+echo 'Finished graph-cache.';
 
+ls -halt
+
+echo 'Building graph-cache tarball...';
 tar -zvcf "graph-cache.tar.gz" ./graph-cache
+echo 'Finished building tarball.';
 
+ls -halt
+
+echo 'Uploading tarball to MINIO...'
+echo "BUCKET_NAME: ${BUCKET_NAME}, "
 AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api put-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key graphhopper/graph-cache.tar.gz --body graph-cache.tar.gz
