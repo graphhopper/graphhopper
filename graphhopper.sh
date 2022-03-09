1340d8159a62fd9cc6a4025c3dd3010e43bc2ea3
Andy McCoy <andy.k.mccoy@gmail.com>
2022-03-08 16:39:55 -0800

    add graph location arg

--- graphhopper.sh
+++ graphhopper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key graphhopper/graph-cache.tar.gz graph-cache.tar.gz
 
-exec java -jar *.jar server /graphhopper/bay-area/config.yml
+tar -xvzf graph-cache.tar.gz
+
+exec java -Ddw.graphhopper.graph.location=./graph-cache -jar *.jar server /graphhopper/bay-area/config.yml
