23ba9ddc93efad3fc51ea017772e04e159398a72
easbar <easbar.mail@posteo.net>
2022-04-21 10:51:02 +0200

    Remove SPTEntry#clone

--- core/src/main/java/com/graphhopper/routing/SPTEntry.java
+++ core/src/main/java/com/graphhopper/routing/SPTEntry.java
@@ -25,7 +25,7 @@ import com.graphhopper.util.EdgeIterator;
  *
  * @author Peter Karich
  */
-public class SPTEntry implements Cloneable, Comparable<SPTEntry> {
+public class SPTEntry implements Comparable<SPTEntry> {
     public int edge;
     public int adjNode;
     public double weight;
@@ -52,23 +52,6 @@ public class SPTEntry implements Cloneable, Comparable<SPTEntry> {
 
     public SPTEntry getParent() {
         return parent;
-    }
-
-    @Override
-    public SPTEntry clone() {
-        return new SPTEntry(edge, adjNode, weight);
-    }
-
-    public SPTEntry cloneFull() {
-        SPTEntry de = clone();
-        SPTEntry tmpPrev = parent;
-        SPTEntry cl = de;
-        while (tmpPrev != null) {
-            cl.parent = tmpPrev.clone();
-            cl = cl.parent;
-            tmpPrev = tmpPrev.parent;
-        }
-        return de;
     }
 
     @Override
