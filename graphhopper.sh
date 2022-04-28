1c84417472010845c103b755d9649f5bab160f04
Andy McCoy <andy.k.mccoy@gmail.com>
2022-04-27 21:39:37 -0700

    hardcode graphcache to staging

--- graphhopper.sh
+++ graphhopper.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key graphhopper/graph-cache.tar.gz graph-cache.tar.gz
+set -e
+# AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.$POD_NAMESPACE --bucket $BUCKET_NAME --key graphhopper/graph-cache.tar.gz graph-cache.tar.gz
+AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url http://$MINIO_HOST.staging --bucket $BUCKET_NAME --key graphhopper/graph-cache.tar.gz graph-cache.tar.gz
 
 tar -xvzf graph-cache.tar.gz
 
