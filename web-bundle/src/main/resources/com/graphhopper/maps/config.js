18e2fdf4ac02d407251772858e10208eb6b5ebdb
easbar <easbar.mail@posteo.net>
2022-10-11 13:46:31 +0200

    update

--- web-bundle/src/main/resources/com/graphhopper/maps/config.js
+++ web-bundle/src/main/resources/com/graphhopper/maps/config.js
@@ -5,12 +5,22 @@ const config = {
     api: 'http://localhost:8989/',
     defaultTiles: 'OpenStreetMap',
     keys: {
-        graphhopper: "missing_api_key",
+        graphhopper: "",
         maptiler: "missing_api_key",
         omniscale: "missing_api_key",
         thunderforest: "missing_api_key",
         kurviger: "missing_api_key"
     },
     routingGraphLayerAllowed: true,
-    extraProfiles: {}
+    extraProfiles: {},
+    request: {
+        details: [
+            'road_class',
+            'road_environment',
+            'max_speed',
+            'average_speed',
+            'country',
+        ],
+        snapPreventions: ['ferry'],
+    },
 }
