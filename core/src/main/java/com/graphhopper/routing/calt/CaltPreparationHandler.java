b43dec8a0597fd33360164377e9f29ea48253266
aoles <andrzej.oles@gmail.com>
2022-10-31 11:07:23 +0100

    Remove obsolete class

--- core/src/main/java/com/graphhopper/routing/calt/CaltPreparationHandler.java
+++
@@ -1,7 +0,0 @@
-package com.graphhopper.routing.calt;
-// ORS-GH MOD - additional class
-// TODO ORS (major): implement this class along the lines of the corresponding
-//           handlers for LM and CH to replace RoutingAlgorithmFactoryDecorator
-public class CaltPreparationHandler {
-    public CaltPreparationHandler() {}
-}
