293f422ce22e35c32b604850f38b5cba28bee588
Scott Feeney <scott@oceanbase.org>
2023-01-09 16:17:15 -0800

    fix tar options

--- graphhopper.sh
+++ graphhopper.sh
@@ -2,6 +2,6 @@
 set -e
 AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY aws s3api get-object --endpoint-url $MINIO_HOST --bucket $BUCKET_NAME --key $GRAPH_CACHE_KEY $OSM_PBF_FILE_NAME
 
-tar --use-compress-program="pigz -d" -xzf $OSM_PBF_FILE_NAME
+tar --use-compress-program="pigz -d" -xf $OSM_PBF_FILE_NAME
 
 exec java $JVM_ARGS -Ddw.graphhopper.graph.location=./graph-cache -jar *.jar server /graphhopper/bay-area/config.yml
