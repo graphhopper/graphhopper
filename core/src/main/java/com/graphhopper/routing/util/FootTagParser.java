928c0802a92c579b9c9881c9ff6c6b018975a378
Peter <graphhopper@gmx.de>
2023-01-27 17:43:23 +0100

    support for foot:backward and forward, fixes #2565

--- core/src/main/java/com/graphhopper/routing/util/FootTagParser.java
+++ core/src/main/java/com/graphhopper/routing/util/FootTagParser.java
@@ -197,8 +197,16 @@ public class FootTagParser extends VehicleTagParser {
             return;
 
         Integer priorityFromRelation = routeMap.get(footRouteEnc.getEnum(false, edgeFlags));
-        accessEnc.setBool(false, edgeFlags, true);
-        accessEnc.setBool(true, edgeFlags, true);
+        if (way.hasTag("oneway:foot", oneways) || way.hasTag("foot:backward") || way.hasTag("foot:forward")
+                || way.hasTag("oneway", oneways) && way.hasTag("highway", "steps") // outdated mapping style
+        ) {
+            boolean reverse = way.hasTag("oneway:foot", "-1") || way.hasTag("foot:backward", "yes") || way.hasTag("foot:forward", "no");
+            accessEnc.setBool(reverse, edgeFlags, true);
+        } else {
+            accessEnc.setBool(false, edgeFlags, true);
+            accessEnc.setBool(true, edgeFlags, true);
+        }
+
         if (!access.isFerry()) {
             String sacScale = way.getTag("sac_scale");
             if (sacScale != null) {
