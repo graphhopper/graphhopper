graphhopper/graphhopper

commit 48e84a93511bbf5c3c1c5a8bd673c5e302adfb5f

Split vehicle tag parsers into access, speed and priority (#2738)

core/src/main/java/com/graphhopper/routing/util/parsers/FootAccessParser.java
@@ -15,59 +15,43 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.graphhopper.routing.util;
+package com.graphhopper.routing.util.parsers;
 
 import com.graphhopper.reader.ReaderWay;
-import com.graphhopper.routing.ev.*;
+import com.graphhopper.routing.ev.BooleanEncodedValue;
+import com.graphhopper.routing.ev.EncodedValueLookup;
+import com.graphhopper.routing.ev.RouteNetwork;
+import com.graphhopper.routing.ev.VehicleAccess;
+import com.graphhopper.routing.util.TransportationMode;
+import com.graphhopper.routing.util.WayAccess;
 import com.graphhopper.storage.IntsRef;
 import com.graphhopper.util.PMap;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static com.graphhopper.routing.ev.RouteNetwork.*;
-import static com.graphhopper.routing.util.PriorityCode.*;
+import static com.graphhopper.routing.util.PriorityCode.UNCHANGED;
 
-/**
- * Defines bit layout for pedestrians (speed, access, surface, ...). Here we put a penalty on unsafe
- * roads only. If you wish to also prefer routes due to beauty like hiking routes use the
- * HikeTagParser instead.
- * <p>
- *
- * @author Peter Karich
- * @author Nop
- * @author Karl Hübner
- */
-public class FootTagParser extends VehicleTagParser {
-    static final int SLOW_SPEED = 2;
-    static final int MEAN_SPEED = 5;
-    // larger value required - ferries are faster than pedestrians
-    static final int FERRY_SPEED = 15;
+public class FootAccessParser extends AbstractAccessParser implements TagParser {
+
     final Set<String> safeHighwayTags = new HashSet<>();
     final Set<String> allowedHighwayTags = new HashSet<>();
     final Set<String> avoidHighwayTags = new HashSet<>();
     final Set<String> allowedSacScale = new HashSet<>();
     protected HashSet<String> sidewalkValues = new HashSet<>(5);
-    protected HashSet<String> sidewalksNoValues = new HashSet<>(5);
-    protected final DecimalEncodedValue priorityWayEncoder;
-    protected EnumEncodedValue<RouteNetwork> footRouteEnc;
     protected Map<RouteNetwork, Integer> routeMap = new HashMap<>();
 
-    public FootTagParser(EncodedValueLookup lookup, PMap properties) {
-        this(
-                lookup.getBooleanEncodedValue(VehicleAccess.key(properties.getString("name", "foot"))),
-                lookup.getDecimalEncodedValue(VehicleSpeed.key(properties.getString("name", "foot"))),
-                lookup.getDecimalEncodedValue(VehiclePriority.key(properties.getString("name", "foot"))),
-                lookup.getEnumEncodedValue(FootNetwork.KEY, RouteNetwork.class)
-        );
+    public FootAccessParser(EncodedValueLookup lookup, PMap properties) {
+        this(lookup.getBooleanEncodedValue(VehicleAccess.key(properties.getString("name", "foot"))));
         blockPrivate(properties.getBool("block_private", true));
         blockFords(properties.getBool("block_fords", false));
     }
 
-    protected FootTagParser(BooleanEncodedValue accessEnc, DecimalEncodedValue speedEnc, DecimalEncodedValue priorityEnc,
-                            EnumEncodedValue<RouteNetwork> footRouteEnc) {
-        super(accessEnc, speedEnc, null, TransportationMode.FOOT, speedEnc.getNextStorableValue(FERRY_SPEED));
-        this.footRouteEnc = footRouteEnc;
-        priorityWayEncoder = priorityEnc;
+    protected FootAccessParser(BooleanEncodedValue accessEnc) {
+        super(accessEnc, TransportationMode.FOOT);
 
         restrictedValues.add("no");
         restrictedValues.add("restricted");
@@ -79,11 +63,6 @@ public class FootTagParser extends VehicleTagParser {
         intendedValues.add("designated");
         intendedValues.add("official");
         intendedValues.add("permissive");
-
-        sidewalksNoValues.add("no");
-        sidewalksNoValues.add("none");
-        // see #712
-        sidewalksNoValues.add("separate");
 
         sidewalkValues.add("yes");
         sidewalkValues.add("both");
@@ -196,7 +175,6 @@ public class FootTagParser extends VehicleTagParser {
         if (access.canSkip())
             return;
 
-        Integer priorityFromRelation = routeMap.get(footRouteEnc.getEnum(false, edgeFlags));
         if (way.hasTag("oneway:foot", oneways) || way.hasTag("foot:backward") || way.hasTag("foot:forward")
                 || way.hasTag("oneway", oneways) && way.hasTag("highway", "steps") // outdated mapping style
         ) {
@@ -206,69 +184,5 @@ public class FootTagParser extends VehicleTagParser {
             accessEnc.setBool(false, edgeFlags, true);
             accessEnc.setBool(true, edgeFlags, true);
         }
-
-        if (!access.isFerry()) {
-            String sacScale = way.getTag("sac_scale");
-            if (sacScale != null) {
-                setSpeed(edgeFlags, true, true, "hiking".equals(sacScale) ? MEAN_SPEED : SLOW_SPEED);
-            } else {
-                setSpeed(edgeFlags, true, true, way.hasTag("highway", "steps") ? MEAN_SPEED - 2 : MEAN_SPEED);
-            }
-        } else {
-            priorityFromRelation = PriorityCode.SLIGHT_AVOID.getValue();
-            double ferrySpeed = ferrySpeedCalc.getSpeed(way);
-            setSpeed(edgeFlags, true, true, ferrySpeed);
-        }
-
-        priorityWayEncoder.setDecimal(false, edgeFlags, PriorityCode.getValue(handlePriority(way, priorityFromRelation)));
-    }
-
-    void setSpeed(IntsRef edgeFlags, boolean fwd, boolean bwd, double speed) {
-        if (speed > getMaxSpeed())
-            speed = getMaxSpeed();
-        if (fwd)
-            avgSpeedEnc.setDecimal(false, edgeFlags, speed);
-        if (bwd && avgSpeedEnc.isStoreTwoDirections())
-            avgSpeedEnc.setDecimal(true, edgeFlags, speed);
-    }
-
-    int handlePriority(ReaderWay way, Integer priorityFromRelation) {
-        TreeMap<Double, Integer> weightToPrioMap = new TreeMap<>();
-        if (priorityFromRelation == null)
-            weightToPrioMap.put(0d, UNCHANGED.getValue());
-        else
-            weightToPrioMap.put(110d, priorityFromRelation);
-
-        collect(way, weightToPrioMap);
-
-        // pick priority with biggest order value
-        return weightToPrioMap.lastEntry().getValue();
-    }
-
-    /**
-     * @param weightToPrioMap associate a weight with every priority. This sorted map allows
-     *                        subclasses to 'insert' more important priorities as well as overwrite determined priorities.
-     */
-    void collect(ReaderWay way, TreeMap<Double, Integer> weightToPrioMap) {
-        String highway = way.getTag("highway");
-        if (way.hasTag("foot", "designated"))
-            weightToPrioMap.put(100d, PREFER.getValue());
-
-        double maxSpeed = Math.max(getMaxSpeed(way, false), getMaxSpeed(way, true));
-        if (safeHighwayTags.contains(highway) || (isValidSpeed(maxSpeed) && maxSpeed <= 20)) {
-            weightToPrioMap.put(40d, PREFER.getValue());
-            if (way.hasTag("tunnel", intendedValues)) {
-                if (way.hasTag("sidewalk", sidewalksNoValues))
-                    weightToPrioMap.put(40d, SLIGHT_AVOID.getValue());
-                else
-                    weightToPrioMap.put(40d, UNCHANGED.getValue());
-            }
-        } else if ((isValidSpeed(maxSpeed) && maxSpeed > 50) || avoidHighwayTags.contains(highway)) {
-            if (!way.hasTag("sidewalk", sidewalkValues))
-                weightToPrioMap.put(45d, AVOID.getValue());
-        }
-
-        if (way.hasTag("bicycle", "official") || way.hasTag("bicycle", "designated"))
-            weightToPrioMap.put(44d, SLIGHT_AVOID.getValue());
     }
 }

commit 11c3deea29a8c2c12f9bebbc44bb43afebf8a2ab

foot vehicle: important fix regarding barrier nodes, bug introduced in #2738

core/src/main/java/com/graphhopper/routing/util/parsers/FootAccessParser.java
@@ -34,6 +34,7 @@ import java.util.Set;
 
 import static com.graphhopper.routing.ev.RouteNetwork.*;
 import static com.graphhopper.routing.util.PriorityCode.UNCHANGED;
+import static java.util.Collections.emptyMap;
 
 public class FootAccessParser extends AbstractAccessParser implements TagParser {
 
@@ -182,5 +183,8 @@ public class FootAccessParser extends AbstractAccessParser implements TagParser 
             accessEnc.setBool(false, edgeFlags, true);
             accessEnc.setBool(true, edgeFlags, true);
         }
+
+        Map<String, Object> nodeTags = way.getTag("node_tags", emptyMap());
+        handleNodeTags(edgeFlags, nodeTags);
     }
 }

core/src/test/java/com/graphhopper/routing/util/parsers/FootTagParserTest.java
@@ -30,6 +30,8 @@ import com.graphhopper.util.*;
 import org.junit.jupiter.api.Test;
 
 import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 
 import static com.graphhopper.routing.util.parsers.FootAverageSpeedParser.MEAN_SPEED;
@@ -402,6 +404,22 @@ public class FootTagParserTest {
         flags = encodingManager.createEdgeFlags();
         speedParser.handleWayTags(flags, way);
         assertEquals(SLOW_SPEED, footAvgSpeedEnc.getDecimal(false, flags), 1e-1);
+    }
+
+    @Test
+    public void testReadBarrierNodesFromWay() {
+        IntsRef intsRef = encodingManager.createEdgeFlags();
+        ReaderWay way = new ReaderWay(1);
+        way.setTag("highway", "secondary");
+
+        ReaderNode node = new ReaderNode(1, -1, -1);
+        node.setTag("barrier", "gate");
+        node.setTag("access", "no");
+        way.setTag("node_tags", node.getTags());
+        accessParser.handleWayTags(intsRef, way);
+
+        assertFalse(footAccessEnc.getBool(false, intsRef));
+        assertFalse(footAccessEnc.getBool(true, intsRef));
     }
 
     @Test