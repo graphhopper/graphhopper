ac64643383c86e236355d5e19f307ebdce4b4a6d
Lukas Weber <32765578+lukasalexanderweber@users.noreply.github.com>
2023-05-08 14:56:32 +0200

    clarify foot access parser (#2801)

--- core/src/main/java/com/graphhopper/routing/util/parsers/FootAccessParser.java
+++ core/src/main/java/com/graphhopper/routing/util/parsers/FootAccessParser.java
@@ -30,9 +30,7 @@ import static com.graphhopper.routing.util.PriorityCode.UNCHANGED;
 
 public class FootAccessParser extends AbstractAccessParser implements TagParser {
 
-    final Set<String> safeHighwayTags = new HashSet<>();
     final Set<String> allowedHighwayTags = new HashSet<>();
-    final Set<String> avoidHighwayTags = new HashSet<>();
     final Set<String> allowedSacScale = new HashSet<>();
     protected HashSet<String> sidewalkValues = new HashSet<>(5);
     protected Map<RouteNetwork, Integer> routeMap = new HashMap<>();
@@ -58,27 +56,23 @@ public class FootAccessParser extends AbstractAccessParser implements TagParser 
 
         barriers.add("fence");
 
-        safeHighwayTags.add("footway");
-        safeHighwayTags.add("path");
-        safeHighwayTags.add("steps");
-        safeHighwayTags.add("pedestrian");
-        safeHighwayTags.add("living_street");
-        safeHighwayTags.add("track");
-        safeHighwayTags.add("residential");
-        safeHighwayTags.add("service");
-        safeHighwayTags.add("platform");
-
-        avoidHighwayTags.add("trunk");
-        avoidHighwayTags.add("trunk_link");
-        avoidHighwayTags.add("primary");
-        avoidHighwayTags.add("primary_link");
-        avoidHighwayTags.add("secondary");
-        avoidHighwayTags.add("secondary_link");
-        avoidHighwayTags.add("tertiary");
-        avoidHighwayTags.add("tertiary_link");
-
-        allowedHighwayTags.addAll(safeHighwayTags);
-        allowedHighwayTags.addAll(avoidHighwayTags);
+        allowedHighwayTags.add("footway");
+        allowedHighwayTags.add("path");
+        allowedHighwayTags.add("steps");
+        allowedHighwayTags.add("pedestrian");
+        allowedHighwayTags.add("living_street");
+        allowedHighwayTags.add("track");
+        allowedHighwayTags.add("residential");
+        allowedHighwayTags.add("service");
+        allowedHighwayTags.add("platform");
+        allowedHighwayTags.add("trunk");
+        allowedHighwayTags.add("trunk_link");
+        allowedHighwayTags.add("primary");
+        allowedHighwayTags.add("primary_link");
+        allowedHighwayTags.add("secondary");
+        allowedHighwayTags.add("secondary_link");
+        allowedHighwayTags.add("tertiary");
+        allowedHighwayTags.add("tertiary_link");
         allowedHighwayTags.add("cycleway");
         allowedHighwayTags.add("unclassified");
         allowedHighwayTags.add("road");
