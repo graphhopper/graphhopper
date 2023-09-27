42351f21657af9db41048a362eba210580500aaf
Michael Zilske <michael.zilske@tu-berlin.de>
2023-09-27 10:05:32 -0700

    start producing walk legs

--- web/src/main/java/com/graphhopper/application/GraphHopperApplication.java
+++ web/src/main/java/com/graphhopper/application/GraphHopperApplication.java
@@ -19,7 +19,6 @@ package com.graphhopper.application;
 
 import com.graphhopper.application.cli.ImportCommand;
 import com.graphhopper.application.cli.MatchCommand;
-import com.graphhopper.application.cli.TestTripTransfersCommand;
 import com.graphhopper.application.resources.RootResource;
 import com.graphhopper.http.CORSFilter;
 import com.graphhopper.http.GraphHopperBundle;
@@ -45,7 +44,6 @@ public final class GraphHopperApplication extends Application<GraphHopperServerC
         bootstrap.addBundle(new RealtimeBundle());
         bootstrap.addCommand(new ImportCommand());
         bootstrap.addCommand(new MatchCommand());
-        bootstrap.addCommand(new TestTripTransfersCommand());
         bootstrap.addBundle(new AssetsBundle("/com/graphhopper/maps/", "/maps/", "index.html"));
         // see this link even though its outdated?! // https://www.webjars.org/documentation#dropwizard
         bootstrap.addBundle(new AssetsBundle("/META-INF/resources/webjars", "/webjars/", null, "webjars"));
