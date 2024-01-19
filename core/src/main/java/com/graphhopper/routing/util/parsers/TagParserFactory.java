fbdad2ad9202e0e01669284fa47d0f4269a9932b
easbar <easbar.mail@posteo.net>
2024-01-19 11:32:05 +0100

    remove tagparserfactory

--- core/src/main/java/com/graphhopper/routing/util/parsers/TagParserFactory.java
+++
@@ -1,8 +0,0 @@
-package com.graphhopper.routing.util.parsers;
-
-import com.graphhopper.routing.ev.EncodedValueLookup;
-import com.graphhopper.util.PMap;
-
-public interface TagParserFactory {
-    TagParser create(EncodedValueLookup lookup, PMap properties);
-}
