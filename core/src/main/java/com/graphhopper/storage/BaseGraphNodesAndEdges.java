fcd28267d8dcd134e4df88384b8cd2145adc3620
Peter <graphhopper@gmx.de>
2024-04-28 15:01:09 +0200

    avoid byte array for georef

--- core/src/main/java/com/graphhopper/storage/BaseGraphNodesAndEdges.java
+++ core/src/main/java/com/graphhopper/storage/BaseGraphNodesAndEdges.java
@@ -21,7 +21,6 @@ package com.graphhopper.storage;
 import com.graphhopper.routing.ev.EdgeIntAccess;
 import com.graphhopper.util.*;
 import com.graphhopper.util.shapes.BBox;
-import org.slf4j.LoggerFactory;
 
 import java.util.Locale;
 
@@ -324,9 +323,11 @@ class BaseGraphNodesAndEdges implements EdgeIntAccess {
     }
 
     public void setGeoRef(long edgePointer, long geoRef) {
-        byte[] bytes = new byte[5];
-        BitUtil.LITTLE.fromULong5(bytes, geoRef, 0);
-        edges.setBytes(edgePointer + E_GEO, bytes, bytes.length);
+        if ((geoRef & 0xFFFF_FF00_0000_0000L) != 0)
+            throw new IllegalArgumentException("geoRef is too large " + geoRef);
+
+        edges.setInt(edgePointer + E_GEO, (int) geoRef);
+        edges.setByte(edgePointer + E_GEO + 4, (byte) (geoRef >>> 32));
     }
 
     public void setKeyValuesRef(long edgePointer, int nameRef) {
@@ -356,9 +357,9 @@ class BaseGraphNodesAndEdges implements EdgeIntAccess {
     }
 
     public long getGeoRef(long edgePointer) {
-        byte[] bytes = new byte[5];
-        edges.getBytes(edgePointer + E_GEO, bytes, bytes.length);
-        return BitUtil.LITTLE.toULong5(bytes, 0);
+        int int0 = edges.getInt(edgePointer + E_GEO);
+        byte byte5 = edges.getByte(edgePointer + E_GEO + 4);
+        return BitUtil.LITTLE.toLong(int0, byte5);
     }
 
     public int getKeyValuesRef(long edgePointer) {
--- core/src/main/java/com/graphhopper/util/BitUtil.java
+++ core/src/main/java/com/graphhopper/util/BitUtil.java
@@ -138,15 +138,11 @@ public class BitUtil {
     }
 
     public final long toLong(int intLow, int intHigh) {
-        return ((long) intHigh << 32) | (intLow & 0xFFFFFFFFL);
+        return ((long) intHigh << 32) | (intLow & 0xFFFF_FFFFL);
     }
 
     public final long toLong(byte[] b, int offset) {
-        return ((long) toInt(b, offset + 4) << 32) | (toInt(b, offset) & 0xFFFFFFFFL);
-    }
-
-    public final long toULong5(byte[] b, int offset) {
-        return ((long) (b[offset + 4] & 0xFF) << 32) | (toInt(b, offset) & 0xFFFFFFFFL);
+        return ((long) toInt(b, offset + 4) << 32) | (toInt(b, offset) & 0xFFFF_FFFFL);
     }
 
     public final byte[] fromLong(long value) {
@@ -163,14 +159,6 @@ public class BitUtil {
         bytes[offset + 7] = (byte) (value >> 56);
         bytes[offset + 6] = (byte) (value >> 48);
         bytes[offset + 5] = (byte) (value >> 40);
-        bytes[offset + 4] = (byte) (value >> 32);
-        bytes[offset + 3] = (byte) (value >> 24);
-        bytes[offset + 2] = (byte) (value >> 16);
-        bytes[offset + 1] = (byte) (value >> 8);
-        bytes[offset] = (byte) (value);
-    }
-
-    public final void fromULong5(byte[] bytes, long value, int offset) {
         bytes[offset + 4] = (byte) (value >> 32);
         bytes[offset + 3] = (byte) (value >> 24);
         bytes[offset + 2] = (byte) (value >> 16);
@@ -271,7 +259,7 @@ public class BitUtil {
     }
 
     public final int getIntLow(long longValue) {
-        return (int) (longValue & 0xFFFFFFFFL);
+        return (int) (longValue & 0xFFFF_FFFFL);
     }
 
     public final int getIntHigh(long longValue) {
