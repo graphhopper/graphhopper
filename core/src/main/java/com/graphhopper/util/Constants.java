de601306e81fbe584b0a89d43372b289ca103123
easbar <easbar.mail@posteo.net>
2024-05-04 14:12:10 +0200

    Increase way geometry version

--- core/src/main/java/com/graphhopper/util/Constants.java
+++ core/src/main/java/com/graphhopper/util/Constants.java
@@ -64,7 +64,7 @@ public class Constants {
     public static final int VERSION_EM = 4;
     public static final int VERSION_SHORTCUT = 9;
     public static final int VERSION_NODE_CH = 0;
-    public static final int VERSION_GEOMETRY = 6;
+    public static final int VERSION_GEOMETRY = 7;
     public static final int VERSION_TURN_COSTS = 0;
     public static final int VERSION_LOCATION_IDX = 5;
     public static final int VERSION_KV_STORAGE = 2;
