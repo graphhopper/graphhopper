7f79bbdc5b5064d3624ae2ef9669cc6b2c53912c
Peter <graphhopper@gmx.de>
2024-09-09 12:31:13 +0200

    configure turn cost calculation (#2957)
    * initial commit
    
    * make configurable also per request
    
    * make tests passing: disable TC customization if no orientation EV
    
    * move turn cost provider into CustomWeighting and clean up
    
    * clean up
    
    * remove TODO NOW
    
    * use same name as in config: turn_costs; add path detail; increase min to +-25°
    
    * bug fix for alternative_route
    
    * fix: plus u_turn_costs
    
    * test fix
    
    * minor fixes
    
    * minor optimization
    
    * use 0 cost as default
    
    * minor fixes
    
    * test fix
    
    * remove checkTurnCostConfigForLM; better and possible to use double not Double
    
    * use conditional instead inheritance
    
    * private
    
    * for now do not include turn_costs path detail and add it later on to support indicated that only a single coordinate is affected
    
    * make left/right min-max symmetric
    
    * use separate, less tested custom model
    
    * fix min_left_angle config
    
    * additional xy_sharp values
    
    * easy to forget left_sharp config. ensure that left_sharp >= left (same for right)
    
    * use degrees for orientation
    
    * use more common north-based angle. still keep orientation as name. Later change calcOrientation and related to xAxisAngle
    
    * make consistent: use postfix _costs. rename max_angle to min_u_turn_angle and return u_turn_costs instead of infinity
    
    * simplify DefaultTurnCostProvider
    
    * comment
    
    * in test: differentiate between sharp and non-sharp
    
    * plural in java like in config
    
    * missing

--- core/src/main/java/com/graphhopper/routing/DefaultWeightingFactory.java
+++ core/src/main/java/com/graphhopper/routing/DefaultWeightingFactory.java
@@ -20,6 +20,8 @@ package com.graphhopper.routing;
 
 import com.graphhopper.config.Profile;
 import com.graphhopper.routing.ev.BooleanEncodedValue;
+import com.graphhopper.routing.ev.DecimalEncodedValue;
+import com.graphhopper.routing.ev.Orientation;
 import com.graphhopper.routing.ev.TurnRestriction;
 import com.graphhopper.routing.util.EncodingManager;
 import com.graphhopper.routing.weighting.DefaultTurnCostProvider;
@@ -31,6 +33,7 @@ import com.graphhopper.storage.BaseGraph;
 import com.graphhopper.util.CustomModel;
 import com.graphhopper.util.PMap;
 import com.graphhopper.util.Parameters;
+import com.graphhopper.util.TurnCostsConfig;
 
 import static com.graphhopper.routing.weighting.TurnCostProvider.NO_TURN_COST_PROVIDER;
 import static com.graphhopper.util.Helper.toLowerCase;
@@ -59,8 +62,12 @@ public class DefaultWeightingFactory implements WeightingFactory {
             BooleanEncodedValue turnRestrictionEnc = encodingManager.getTurnBooleanEncodedValue(TurnRestriction.key(profile.getName()));
             if (turnRestrictionEnc == null)
                 throw new IllegalArgumentException("Cannot find turn restriction encoded value for " + profile.getName());
+            DecimalEncodedValue oEnc = encodingManager.hasEncodedValue(Orientation.KEY) ? encodingManager.getDecimalEncodedValue(Orientation.KEY) : null;
+            if (profile.getTurnCostsConfig().hasLeftRightStraightCosts() && oEnc == null)
+                throw new IllegalArgumentException("Using left_costs,left_sharp_costs,right_costs,right_sharp_costs or straight_costs for turn_costs requires 'orientation' in graph.encoded_values");
             int uTurnCosts = hints.getInt(Parameters.Routing.U_TURN_COSTS, profile.getTurnCostsConfig().getUTurnCosts());
-            turnCostProvider = new DefaultTurnCostProvider(turnRestrictionEnc, graph.getTurnCostStorage(), uTurnCosts);
+            TurnCostsConfig tcConfig = new TurnCostsConfig(profile.getTurnCostsConfig()).setUTurnCosts(uTurnCosts);
+            turnCostProvider = new DefaultTurnCostProvider(turnRestrictionEnc, oEnc, graph, tcConfig);
         } else {
             turnCostProvider = NO_TURN_COST_PROVIDER;
         }
--- web-api/src/main/java/com/graphhopper/util/TurnCostsConfig.java
+++ web-api/src/main/java/com/graphhopper/util/TurnCostsConfig.java
@@ -2,11 +2,25 @@ package com.graphhopper.util;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class TurnCostsConfig {
     public static final int INFINITE_U_TURN_COSTS = -1;
+    private double leftCosts; // in seconds
+    private double leftSharpCosts; // in seconds
+    private double straightCosts;
+    private double rightCosts;
+    private double rightSharpCosts;
+
+    // The "right" and "left" turns are symmetric and the negative values are used for "left".
+    // From 0 to minAngle no turn costs are added.
+    // From minAngle to minSharpAngle the rightCosts (or leftCosts) are added.
+    // From minSharpAngle to minUTurnAngle the rightSharpCosts (or leftSharpCosts) are added.
+    // And beyond minUTurnAngle the uTurnCosts are added.
+    private double minAngle = 25, minSharpAngle = 80, minUTurnAngle = 180;
+
     private int uTurnCosts = INFINITE_U_TURN_COSTS;
     private List<String> vehicleTypes;
     // ensure that no typos can occur like motor_car vs motorcar or bike vs bicycle
@@ -27,8 +41,22 @@ public class TurnCostsConfig {
         return new TurnCostsConfig(List.of("bicycle"));
     }
 
-    // jackson
     public TurnCostsConfig() {
+    }
+
+    public TurnCostsConfig(TurnCostsConfig copy) {
+        leftCosts = copy.leftCosts;
+        leftSharpCosts = copy.leftSharpCosts;
+        straightCosts = copy.straightCosts;
+        rightCosts = copy.rightCosts;
+        rightSharpCosts = copy.rightSharpCosts;
+        uTurnCosts = copy.uTurnCosts;
+
+        minAngle = copy.minAngle;
+        minSharpAngle = copy.minSharpAngle;
+        minUTurnAngle = copy.minUTurnAngle;
+        if (copy.vehicleTypes != null)
+            vehicleTypes = new ArrayList<>(copy.vehicleTypes);
     }
 
     public TurnCostsConfig(List<String> vehicleTypes) {
@@ -38,10 +66,6 @@ public class TurnCostsConfig {
     public TurnCostsConfig(List<String> vehicleTypes, int uTurnCost) {
         this.vehicleTypes = check(vehicleTypes);
         this.uTurnCosts = uTurnCost;
-    }
-
-    public void setVehicleTypes(List<String> vehicleTypes) {
-        this.vehicleTypes = check(vehicleTypes);
     }
 
     List<String> check(List<String> restrictions) {
@@ -54,12 +78,21 @@ public class TurnCostsConfig {
         return restrictions;
     }
 
+    public TurnCostsConfig setVehicleTypes(List<String> vehicleTypes) {
+        this.vehicleTypes = check(vehicleTypes);
+        return this;
+    }
+
     @JsonProperty("vehicle_types")
     public List<String> getVehicleTypes() {
         check(vehicleTypes);
         return vehicleTypes;
     }
 
+    /**
+     * @param uTurnCosts the costs of an u-turn in seconds, for {@link TurnCostsConfig#INFINITE_U_TURN_COSTS}
+     *                   the u-turn costs will be infinite
+     */
     public TurnCostsConfig setUTurnCosts(int uTurnCosts) {
         this.uTurnCosts = uTurnCosts;
         return this;
@@ -70,8 +103,96 @@ public class TurnCostsConfig {
         return uTurnCosts;
     }
 
+    public boolean hasLeftRightStraightCosts() {
+        return leftCosts != 0 || leftSharpCosts != 0 || straightCosts != 0 || rightCosts != 0 || rightSharpCosts != 0;
+    }
+
+    public TurnCostsConfig setLeftCosts(double leftCosts) {
+        this.leftCosts = leftCosts;
+        return this;
+    }
+
+    @JsonProperty("left_costs")
+    public double getLeftCosts() {
+        return leftCosts;
+    }
+
+    public TurnCostsConfig setLeftSharpCosts(double leftSharpCosts) {
+        this.leftSharpCosts = leftSharpCosts;
+        return this;
+    }
+
+    @JsonProperty("left_sharp_costs")
+    public double getLeftSharpCosts() {
+        return leftSharpCosts;
+    }
+
+    public TurnCostsConfig setRightCosts(double rightCosts) {
+        this.rightCosts = rightCosts;
+        return this;
+    }
+
+    @JsonProperty("right_costs")
+    public double getRightCosts() {
+        return rightCosts;
+    }
+
+    public TurnCostsConfig setRightSharpCosts(double rightSharpCosts) {
+        this.rightSharpCosts = rightSharpCosts;
+        return this;
+    }
+
+    @JsonProperty("right_sharp_costs")
+    public double getRightSharpCosts() {
+        return rightSharpCosts;
+    }
+
+    public TurnCostsConfig setStraightCosts(double straightCosts) {
+        this.straightCosts = straightCosts;
+        return this;
+    }
+
+    @JsonProperty("straight_costs")
+    public double getStraightCosts() {
+        return straightCosts;
+    }
+
+    @JsonProperty("min_angle")
+    public TurnCostsConfig setMinAngle(double minAngle) {
+        this.minAngle = minAngle;
+        return this;
+    }
+
+    public double getMinAngle() {
+        return minAngle;
+    }
+
+    @JsonProperty("min_sharp_angle")
+    public TurnCostsConfig setMinSharpAngle(double minSharpAngle) {
+        this.minSharpAngle = minSharpAngle;
+        return this;
+    }
+
+    public double getMinSharpAngle() {
+        return minSharpAngle;
+    }
+
+    @JsonProperty("min_u_turn_angle")
+    public TurnCostsConfig setMinUTurnAngle(double minUTurnAngle) {
+        this.minUTurnAngle = minUTurnAngle;
+        return this;
+    }
+
+    public double getMinUTurnAngle() {
+        return minUTurnAngle;
+    }
+
     @Override
     public String toString() {
-        return "vehicleTypes=" + vehicleTypes + ", uTurnCosts=" + uTurnCosts;
+        return "left=" + leftCosts + ", leftSharp=" + leftSharpCosts
+                + ", straight=" + straightCosts
+                + ", right=" + rightCosts + ", rightSharp=" + rightSharpCosts
+                + ", minAngle=" + minAngle + ", minSharpAngle=" + minSharpAngle + ", minUTurnAngle=" + minUTurnAngle
+                + ", uTurnCosts=" + uTurnCosts + ", vehicleTypes=" + vehicleTypes;
     }
 }
