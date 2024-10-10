8c0b0dfa9fdc8b0f1787e6847261d15d2ae97a68
Stefan Radomski <stefan.radomski@umi.city>
2024-10-10 16:01:48 +0200

    First working version

--- web-bundle/src/main/java/com/graphhopper/resources/MapMatchingResource.java
+++ web-bundle/src/main/java/com/graphhopper/resources/MapMatchingResource.java
@@ -34,6 +34,7 @@ import com.graphhopper.jackson.ResponsePathSerializer;
 import com.graphhopper.matching.*;
 import com.graphhopper.storage.index.LocationIndexTree;
 import com.graphhopper.util.*;
+import com.graphhopper.util.details.PathDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -159,6 +160,45 @@ public class MapMatchingResource {
             responsePath.getErrors().clear();
             GHResponse rsp = new GHResponse();
             rsp.add(responsePath);
+
+            // if we had timestamps in the gpx input, change the meaning of the time details!
+            Map<String, List<PathDetail>> details = responsePath.getPathDetails();
+            if (details.containsKey("edge_id") &&
+                    details.containsKey("time") &&
+                    matchResult.getEdgeMatches().size() > 0 &&
+                    matchResult.getEdgeMatches().get(0).getStates().size() > 0 &&
+                    matchResult.getEdgeMatches().get(0).getStates().get(0).getEntry().getTime().toInstant().getEpochSecond() > 0
+            ) {
+                List<PathDetail> edges = details.get("edge_id");
+                List<PathDetail> times = details.get("time");
+                assert edges.size() == times.size();
+                int currentEdgeMatchIdx = 0;
+                int currentTimeIdx = 0;
+                List<EdgeMatch> edgeMatches = matchResult.getEdgeMatches();
+
+                State currentState = edgeMatches.get(0).getStates().get(0);
+
+                for (PathDetail edge : edges) {
+                    // edge id as per edge path detail
+                    int edgeId = (int) edge.getValue();
+
+                    // we need to find the fitting EdgeMatch and its state with the observation
+                    while(edgeId != edgeMatches.get(currentEdgeMatchIdx).getEdgeState().getEdge()) {
+                        currentEdgeMatchIdx += 1;
+                    }
+                    if (edgeMatches.get(currentEdgeMatchIdx).getStates().size() > 0) {
+                        currentState = edgeMatches.get(currentEdgeMatchIdx).getStates().get(0);
+                    }
+
+                    Observation currentObs = currentState.getEntry();
+                    PathDetail mmTime = new PathDetail(currentObs.getTime().toInstant().toString());
+                    mmTime.setFirst(edge.getFirst());
+                    mmTime.setLast(edge.getLast());
+                    times.set(currentTimeIdx, mmTime);
+                    currentTimeIdx += 1;
+                }
+
+            }
 
             if (writeGPX) {
                 long time = gpx.trk.get(0).getStartTime()
