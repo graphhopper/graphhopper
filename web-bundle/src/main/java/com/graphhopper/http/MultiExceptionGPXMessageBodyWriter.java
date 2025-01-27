03f9e4e2f29a8bf4dc5cca2d0470b1dbe1af3634
easbar <easbar.mail@posteo.net>
2025-01-27 15:13:10 +0100

    Make /nearest return 400 instead of 500 for invalid snaps, fix #3109

--- web-bundle/src/main/java/com/graphhopper/http/MultiExceptionGPXMessageBodyWriter.java
+++ web-bundle/src/main/java/com/graphhopper/http/MultiExceptionGPXMessageBodyWriter.java
@@ -23,7 +23,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -36,7 +35,6 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
@@ -56,7 +54,7 @@ public class MultiExceptionGPXMessageBodyWriter implements MessageBodyWriter<Mul
     }
 
     @Override
-    public void writeTo(MultiException e, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+    public void writeTo(MultiException e, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) {
         if (e.getErrors().isEmpty())
             throw new RuntimeException("errorsToXML should not be called with an empty list");
 
