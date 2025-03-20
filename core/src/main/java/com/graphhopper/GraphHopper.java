7233f72c5107588e55c3c0a7d8daaa921bf9e162
Jason Baker <jbaker@gosonar.com>
2025-03-20 09:28:43 -0500

    remove comment aboute read only file system / datareader.file truthiness

--- core/src/main/java/com/graphhopper/GraphHopper.java
+++ core/src/main/java/com/graphhopper/GraphHopper.java
@@ -342,10 +342,6 @@ public class GraphHopper {
         return this;
     }
 
-    /**
-     * Validator to check that allow_writes is true when OSM File is truthy.
-     * This allows for loadig a graph from a read-only file system.
-     */
     void validateOSMFileAndWritesAllowed() throws IllegalArgumentException {
         if (!isEmpty(this.osmFile) & !allowWrites)
             throw new IllegalArgumentException("graph.allow_writes must be true if datareader.file is read");
