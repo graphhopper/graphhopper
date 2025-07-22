energy-measurement/ci-energy-e1-ci-skip

Improve ferry instructions (#3179)
* announce ferry in instructions

* extra instruction sign needed

* Update maps to 34acd30e

* fix

* ensure never NPE

* use mode==ferry for mapbox response

navigation/src/main/java/com/graphhopper/navigation/NavigateResponseConverter.java
@@ -17,7 +17,6 @@
  */
 package com.graphhopper.navigation;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -399,8 +398,7 @@ public class NavigateResponseConverter {
         // Does not include elevation
         instructionJson.put("geometry", ResponsePathSerializer.encodePolyline(pointList, false, 1e6));
 
-        // TODO: how about other modes?
-        instructionJson.put("mode", "driving");
+        instructionJson.put("mode", instruction.getSign() == Instruction.FERRY ? "ferry" : "driving");
 
         putManeuver(instruction, instructionJson, locale, translationMap, isFirstInstructionOfLeg);
 

graphhopper/graphhopper

navigation/src/main/java/com/graphhopper/navigation/NavigateResponseConverter.java
@@ -419,7 +419,7 @@ public class NavigateResponseConverter {
         // Does not include elevation
         stepJson.put("geometry", ResponsePathSerializer.encodePolyline(pointList, false, 1e6));
 
-        stepJson.put("mode", "driving");
+        stepJson.put("mode", instruction.getSign() == Instruction.FERRY ? "ferry" : "driving");
 
         putManeuver(instruction, stepJson, locale, translationMap, maneuverType);
 