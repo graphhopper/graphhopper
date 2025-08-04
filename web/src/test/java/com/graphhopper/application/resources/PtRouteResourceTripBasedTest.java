3b9d4ae2390f8096a6771c628f3af28f721e66ba
Michael Zilske <michael.zilske@graphhopper.com>
2025-08-04 19:26:37 +0200

    Merge branch 'master' into patterns
    # Conflicts:
    #	reader-gtfs/src/main/java/com/graphhopper/gtfs/GraphExplorer.java
    #	reader-gtfs/src/main/java/com/graphhopper/gtfs/RealtimeFeed.java
    #	reader-gtfs/src/test/java/com/graphhopper/AnotherAgencyIT.java
    #	reader-gtfs/src/test/java/com/graphhopper/GraphHopperGtfsIT.java
    #	reader-gtfs/src/test/java/com/graphhopper/GraphHopperMultimodalIT.java
    #	web-api/src/main/java/com/graphhopper/jackson/ResponsePathSerializer.java
    #	web-bundle/src/main/java/com/graphhopper/resources/PtRouteResource.java
    #	web/src/test/java/com/graphhopper/application/resources/PtRouteResourceTest.java

--- web/src/test/java/com/graphhopper/application/resources/PtRouteResourceTripBasedTest.java
+++ web/src/test/java/com/graphhopper/application/resources/PtRouteResourceTripBasedTest.java
@@ -24,6 +24,7 @@ import com.graphhopper.application.GraphHopperServerConfiguration;
 import com.graphhopper.application.util.GraphHopperServerTestConfiguration;
 import com.graphhopper.config.Profile;
 import com.graphhopper.resources.InfoResource;
+import com.graphhopper.routing.TestProfiles;
 import com.graphhopper.util.Helper;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
@@ -57,7 +58,8 @@ public class PtRouteResourceTripBasedTest {
                 putObject("import.osm.ignored_highways", "").
                 putObject("gtfs.trip_based", true).
                 putObject("gtfs.schedule_day", "2007-01-01").
-                setProfiles(Collections.singletonList(new Profile("foot")));
+                setProfiles(Collections.singletonList(TestProfiles.accessSpeedAndPriority("foot")))
+                .putObject("graph.encoded_values", "foot_access, foot_priority, foot_average_speed, car_access, car_average_speed");
         return config;
     }
 
