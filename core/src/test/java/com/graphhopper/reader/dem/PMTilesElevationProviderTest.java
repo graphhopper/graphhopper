0ca234b68eaa2e9939dad428a260545503be2b8a
Peter <graphhopper@gmx.de>
2026-02-16 13:30:28 +0100

    minor

--- core/src/test/java/com/graphhopper/reader/dem/PMTilesElevationProviderTest.java
+++ core/src/test/java/com/graphhopper/reader/dem/PMTilesElevationProviderTest.java
@@ -18,7 +18,6 @@
 package com.graphhopper.reader.dem;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
