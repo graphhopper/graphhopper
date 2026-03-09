7716530f91f3c352b8a13817ea9e6a64b01dc699
Peter <graphhopper@gmx.de>
2026-03-09 17:59:34 +0100

    sidewalk encoded value (#3305)

--- core/src/main/java/com/graphhopper/routing/ev/DefaultImportRegistry.java
+++ core/src/main/java/com/graphhopper/routing/ev/DefaultImportRegistry.java
@@ -162,6 +162,11 @@ public class DefaultImportRegistry implements ImportRegistry {
                     (lookup, props) -> new OSMFootwayParser(
                             lookup.getEnumEncodedValue(Footway.KEY, Footway.class))
             );
+        else if (Sidewalk.KEY.equals(name))
+            return ImportUnit.create(name, props -> Sidewalk.create(),
+                    (lookup, props) -> new OSMSidewalkParser(
+                            lookup.getEnumEncodedValue(Sidewalk.KEY, Sidewalk.class))
+            );
         else if (Cycleway.KEY.equals(name))
             return ImportUnit.create(name, props -> Cycleway.create(),
                     (lookup, props) -> new OSMCyclewayParser(
