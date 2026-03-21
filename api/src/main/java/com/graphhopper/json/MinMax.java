eb1c4c8e11635d6abd55a879dce29c5bf6f9869a
Cezary Sanecki <saneckicezary@gmail.com>
2026-03-21 22:17:53 +0100

    Rename module folder graphhopper-api to api

---
+++ api/src/main/java/com/graphhopper/json/MinMax.java
@@ -0,0 +1,11 @@
+package com.graphhopper.json;
+
+public class MinMax {
+    public double min;
+    public double max;
+
+    public MinMax(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+}
