b6a7b7b38aea9d46fd5c379d6b7be64d95668d04
Peter <graphhopper@gmx.de>
2026-03-25 22:04:58 +0100

    extract Curbside.AUTO resolving method

--- core/src/main/java/com/graphhopper/routing/ViaRouting.java
+++ core/src/main/java/com/graphhopper/routing/ViaRouting.java
@@ -18,7 +18,10 @@
 package com.graphhopper.routing;
 
 import com.carrotsearch.hppc.IntArrayList;
-import com.graphhopper.routing.ev.*;
+import com.graphhopper.routing.ev.EncodedValueLookup;
+import com.graphhopper.routing.ev.EnumEncodedValue;
+import com.graphhopper.routing.ev.RoadClass;
+import com.graphhopper.routing.ev.RoadEnvironment;
 import com.graphhopper.routing.querygraph.QueryGraph;
 import com.graphhopper.routing.querygraph.VirtualEdgeIteratorState;
 import com.graphhopper.routing.util.*;
@@ -35,7 +38,8 @@ import java.util.function.Function;
 
 import static com.graphhopper.util.EdgeIterator.ANY_EDGE;
 import static com.graphhopper.util.EdgeIterator.NO_EDGE;
-import static com.graphhopper.util.Parameters.Curbsides.*;
+import static com.graphhopper.util.Parameters.Curbsides.CURBSIDE_ANY;
+import static com.graphhopper.util.Parameters.Curbsides.CURBSIDE_AUTO;
 import static com.graphhopper.util.Parameters.Routing.CURBSIDE;
 
 /**
@@ -100,8 +104,7 @@ public class ViaRouting {
         if (!curbsides.isEmpty() && !headings.isEmpty())
             throw new IllegalArgumentException("You cannot use curbsides and headings or pass_through at the same time");
 
-        // Resolve AUTO curbsides based on road class, country and if one-way (later maybe lanes and max_speed)
-        Function<Snap, String> curbsideAutoFunction = createCurbsideAutoFunction(directedEdgeFilter, em);
+        Function<Snap, String> curbsideAutoFunction = CurbsideAutoHelper.createResolver(directedEdgeFilter, em);
 
         final int legs = snaps.size() - 1;
         Result result = new Result(legs);
@@ -163,33 +166,6 @@ public class ViaRouting {
         }
 
         return result;
-    }
-
-    private static Function<Snap, String> createCurbsideAutoFunction(final DirectedEdgeFilter edgeFilter, final EncodingManager em) {
-        EnumEncodedValue<RoadClass> roadClassEnc = em.getEnumEncodedValue(RoadClass.KEY, RoadClass.class);
-        EnumEncodedValue<Country> countryEnc = em.hasEncodedValue(Country.KEY) ? em.getEnumEncodedValue(Country.KEY, Country.class) : null;
-
-        return snap -> {
-            EdgeIteratorState edge = snap.getClosestEdge();
-
-            // do not force curbside for 'smaller roads' (for now not configurable)
-            RoadClass roadClass = edge.get(roadClassEnc);
-            if (roadClass != RoadClass.PRIMARY && roadClass != RoadClass.SECONDARY && roadClass != RoadClass.TRUNK)
-                return CURBSIDE_ANY;
-
-            // do not force curbside for one-ways
-            if (!edgeFilter.accept(edge, false) || !edgeFilter.accept(edge, true))
-                return CURBSIDE_ANY;
-
-            // do not force curbside for 'smaller roads' regarding lanes and max_speed
-            // note: lane count in OSM is for the entire road - not just for one direction
-            // TODO LATER: 'lanes' is 1 if OSM tag is missing, which might be rather misleading in this case
-//            if (lanesEnc != null && edge.get(lanesEnc) < 2 && maxSpeedEnc != null && edge.get(maxSpeedEnc) <= 50)
-//                return CURBSIDE_ANY;
-
-            // could be different per point
-            return countryEnc == null || edge.get(countryEnc).isRightHandTraffic() ? CURBSIDE_RIGHT : CURBSIDE_LEFT;
-        };
     }
 
     public static class Result {
