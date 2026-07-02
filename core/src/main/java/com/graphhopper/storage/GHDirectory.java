27d9b1e03df96eb27683bff1fef1a7d227a5220c
Peter <graphhopper@gmx.de>
2026-07-02 14:09:10 +0200

    Revert "see if import speed improves with RAM_INT_STORE"
    This reverts commit ca2f01dd275f6a7b521555e1661d9c0dcaa432bc.

--- core/src/main/java/com/graphhopper/storage/GHDirectory.java
+++ core/src/main/java/com/graphhopper/storage/GHDirectory.java
@@ -216,8 +216,8 @@ public class GHDirectory implements Directory {
      */
     public DAType getDefaultType(String dataAccess, boolean preferInts) {
         DAType type = getDefault(dataAccess, typeFallback);
-        if (preferInts && type.isInMemory())
-            return type.isStoring() ? RAM_INT_STORE : RAM_INT;
+//        if (preferInts && type.isInMemory())
+//            return type.isStoring() ? RAM_INT_STORE : RAM_INT;
         return type;
     }
 
